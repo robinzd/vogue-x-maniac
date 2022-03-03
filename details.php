1054953e9bff946d2232fa93dfcc880a630da9e5
robin <ibots.robin@gmail.com>
2022-03-03 19:24:19 +0530

    some changes in the pages

--- details.php
+++ details.php
@@ -59,8 +59,8 @@
                                 <div class="sizes mt-5">
                                     <h6 class="text-uppercase">Size</h6> <label class="radio"> <input type="radio" name="size" value="S" checked> <span>S</span> </label> <label class="radio"> <input type="radio" name="size" value="M"> <span>M</span> </label> <label class="radio"> <input type="radio" name="size" value="L"> <span>L</span> </label> <label class="radio"> <input type="radio" name="size" value="XL"> <span>XL</span> </label> <label class="radio"> <input type="radio" name="size" value="XXL"> <span>XXL</span> </label>
                                 </div>
-                                <div class="cart mt-4 align-items-center"> <button class="btn-success text-uppercase mr-2 px-4">Add to cart</button>
-                                    <button class="btn-success text-uppercase mr-2 px-4">Buy Now</button>
+                                <div class="cart mt-4 align-items-center"> <button class="btn-success mr-2 px-4">Add to cart</button>
+                                    <button class="btn-success mr-2 px-4">Buy Now</button>
                                 </div>
                             </div>
                         </div>
