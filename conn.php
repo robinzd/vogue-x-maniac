d4199c2d7bf258c5a60f05cff9621cd38fd710d7
robin <ibots.robin@gmail.com>
2022-03-09 13:21:35 +0530

    connecting database

--- conn.php
+++ conn.php
@@ -1,9 +1,9 @@
 <?php
 //Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["us-cdbr-east-05.cleardb.net"];
-$cleardb_username = $cleardb_url["bae780ccd132dd"];
-$cleardb_password = $cleardb_url["612c8a8d"];
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
