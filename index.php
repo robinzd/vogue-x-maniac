b991597f1c6e29675deaaf22f39ca27203b7cf58
robin <ibots.robin@gmail.com>
2022-03-09 13:19:03 +0530

    connecting database

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 
  <?php 
+
  include ("conn.php");
+
  ?> 
 
 
@@ -82,7 +84,7 @@
             
             <div class='item active'>
 
-            <img src='admin_area/slides_images/ $slider_image'>
+            <img src='admin_area/slides_images/$slider_image'>
 
             </div>
             
