75290657f465d6551e32b0ab3883e6623a31d7ef
robin <ibots.robin@gmail.com>
2022-03-10 18:21:43 +0530

    making the category section dynmaic

--- index.php
+++ index.php
@@ -265,9 +265,77 @@ include("./conn.php");
 
 
             </div>
+            <div class="gallery__column">
+                <a href="#" target="_blank" class="gallery__link">
+                    <?php
+
+                    $get_product_category = "select * from product_category LIMIT 4,2";
+
+                    $run_gallery_image = mysqli_query($conn, $get_product_category);
+
+
+
+                    while ($row_category_picture = mysqli_fetch_array($run_gallery_image)) {
+
+                        $category_image = $row_category_picture['category_image'];
+                        $category_name = $row_category_picture['category_name'];
+
+
+
+                        echo "<figure class='gallery__thumb'>
+    <img src='./categories_images/$category_image' alt='mens and womens shoes' class='gallery__image'>
+    <figcaption class='gallery__caption'>$category_name</figcaption>
+</figure>
+
+
+
+";
+                    }
+
+
+                    ?>
+                </a>
+
+
+
+            </div>
+            <div class="gallery__column">
+                <a href="#" target="_blank" class="gallery__link">
+                    <?php
+
+                    $get_product_category = "select * from product_category LIMIT 6,2";
+
+                    $run_gallery_image = mysqli_query($conn, $get_product_category);
+
+
+
+                    while ($row_category_picture = mysqli_fetch_array($run_gallery_image)) {
+
+                        $category_image = $row_category_picture['category_image'];
+                        $category_name = $row_category_picture['category_name'];
+
+
+
+                        echo "<figure class='gallery__thumb'>
+    <img src='./categories_images/$category_image' alt='mens and womens shoes' class='gallery__image'>
+    <figcaption class='gallery__caption'>$category_name</figcaption>
+</figure>
+
+
+
+";
+                    }
+
+
+                    ?>
+                </a>
+
+
+
+            </div>
 
           
-            </div>
+            
         </div>
 
     </section>
