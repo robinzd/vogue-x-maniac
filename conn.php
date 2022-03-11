d7578c158f71b3021b1013a39a14d93dd4883d20
robin <ibots.robin@gmail.com>
2022-03-12 01:26:22 +0530

    fixing bug for the table

--- conn.php
+++ conn.php
@@ -1,4 +1,6 @@
 <?php
+
+echo "Check from Data base";
 //Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $cleardb_server = $cleardb_url["host"];
@@ -9,4 +11,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+
+// Create connection
+// $conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 ?>
