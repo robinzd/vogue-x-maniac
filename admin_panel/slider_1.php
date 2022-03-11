c3ca8b6a3e1ce92e8bfa621637b50dd670247f43
robin <ibots.robin@gmail.com>
2022-03-11 18:57:58 +0530

    adding some pages into this files

--- admin_panel/slider_1.php
+++ admin_panel/slider_1.php
@@ -1,6 +1,9 @@
 <?php
 include './conn.php';
 ?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,15 +12,12 @@ include './conn.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Services Data</title>
-    <link rel="icon" type="image/x-icon" href="favicon.ico">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="./slider_1.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="./slider_1_ajax.js"></script>
+  
 </head>
 
 <body>
@@ -226,6 +226,11 @@ include './conn.php';
             <a class="text-white" href="#">Njoy Life</a>
         </div>
     </footer>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="./slider_1_ajax.js"></script>
 
 </body>
 
