44b46376c60d7f5872aa041e9b0b133abd4af002
robin <ibots.robin@gmail.com>
2022-03-17 14:51:57 +0530

    editing the table informations

--- admin_panel/slider_1/index.php
+++ admin_panel/slider_1/index.php
@@ -173,11 +173,11 @@ table.table td i {
             <div class="table-title">
                 <div class="row">
                     <div class="col-sm-5">
-                        <h2>User <b>Management</b></h2>
+                        <h2>Slider <b>Management</b></h2>
                     </div>
 
                     <div class="col-sm-7" align="right">
-                        <a href="insert.php" class="btn btn-secondary"><i class="material-icons">&#xE147;</i> <span>Add New Slider</span></a>
+                        <a href="insert.php" class="btn btn-success"><i class="material-icons">&#xE147;</i> <span>Add New Slider</span></a>
                     </div>
                     
                 </div>
