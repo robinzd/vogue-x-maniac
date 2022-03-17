db67796a6ca8ac9fb0f60f1e6e0c55860798a931
robin <ibots.robin@gmail.com>
2022-03-17 14:33:00 +0530

    editing the table informations

--- admin_panel/slider_1/edit.php
+++ admin_panel/slider_1/edit.php
@@ -150,10 +150,10 @@ while ($row=mysqli_fetch_array($ret)) {
 		<div class="form-group">
             <button type="submit" class="btn btn-success btn-lg btn-block" name="submit">Update</button>
 		</div>
-		<div class="cancel">
+</form>
+        <div class="cancel">
 		<button href="index.php" class="btn btn-danger btn-lg btn-block" name="submit">Cancel</button>
 		</div>
-    </form>
 
 </div>
 </body>
