e51b786356f105925a61352f966000a556563424
robin <ibots.robin@gmail.com>
2022-03-19 20:11:43 +0530

    fixing the bug in the admin panel card

--- admin_panel/admin_panel.php
+++ admin_panel/admin_panel.php
@@ -38,45 +38,42 @@
               </nav>
           </div>
           <!--Container Main start-->
-        
-              <h4>Main Components</h4>
-              <div class="container">
-                  <div class="row">
-                      <div class="col-md-3">
-                          <div class="card-counter primary">
-                              <i class="fa fa-code-fork"></i>
-                              <span class="count-numbers">12</span>
-                              <span class="count-name">Flowz</span>
-                          </div>
-                      </div>
 
-                      <div class="col-md-3">
-                          <div class="card-counter danger">
-                              <i class="fa fa-ticket"></i>
-                              <span class="count-numbers">599</span>
-                              <span class="count-name">Instances</span>
-                          </div>
+          <h4>Main Components</h4>
+          <div class="container">
+              <div class="row">
+                  <div class="col-md-3">
+                      <div class="card-counter primary">
+                          <i class="fa fa-code-fork"></i>
+                          <span class="count-name">Flowz</span>
                       </div>
+                  </div>
 
-                      <div class="col-md-3">
-                          <div class="card-counter success">
-                              <i class="fa fa-database"></i>
-                              <span class="count-numbers">6875</span>
-                              <span class="count-name">Data</span>
-                          </div>
+                  <div class="col-md-3">
+                      <div class="card-counter danger">
+                          <i class="fa fa-ticket"></i>
+                          <span class="count-name">Instances</span>
                       </div>
+                  </div>
 
-                      <div class="col-md-3">
-                          <div class="card-counter info">
-                              <i class="fa fa-users"></i>
-                              <span class="count-numbers">35</span>
-                              <span class="count-name">Users</span>
-                          </div>
+                  <div class="col-md-3">
+                      <div class="card-counter success">
+                          <i class="fa fa-database"></i>
+
+                          <span class="count-name">Data</span>
+                      </div>
+                  </div>
+
+                  <div class="col-md-3">
+                      <div class="card-counter info">
+                          <i class="fa fa-users"></i>
+                          <span class="count-name">Users</span>
                       </div>
                   </div>
               </div>
+          </div>
 
-          
+
           <!--Container Main end-->
 
 
