6de4512062aa4a55f7fb7402bd616b14b3c608e5
robin <ibots.robin@gmail.com>
2022-03-19 20:20:00 +0530

    editing some of the information of the admin card

--- admin_panel/admin_panel.php
+++ admin_panel/admin_panel.php
@@ -50,16 +50,15 @@
                   </div>
 
                   <div class="col-md-3">
-                      <div class="card-counter danger">
-                          <i class="fa fa-ticket"></i>
-                          <span class="count-name">Instances</span>
-                      </div>
+                  <a href="./slider_2/slider2.php"><div class="card-counter danger">
+                          <i class="fa fa-fork"></i>
+                          <span class="count-name">Slider 2</span>
+                      </div></a>
                   </div>
 
                   <div class="col-md-3">
                       <div class="card-counter success">
                           <i class="fa fa-database"></i>
-
                           <span class="count-name">Data</span>
                       </div>
                   </div>
