973f1006118e0f8f6b248a0af082b3afc0fe20b4
robin <ibots.robin@gmail.com>
2022-03-21 00:40:12 +0530

    adding search bar in the navbar

--- admin_panel/slider_1/slider1.php
+++ admin_panel/slider_1/slider1.php
@@ -232,6 +232,42 @@ if (isset($_GET['delid'])) {
         .text {
             margin-left: 10px;
         }
+
+        /* search box */
+        .height {
+    height: 100vh
+}
+
+.search {
+    position: relative;
+    box-shadow: 0 0 40px rgba(51, 51, 51, .1)
+}
+
+.search input {
+    height: 60px;
+    text-indent: 25px;
+    border: 2px solid #d6d4d4
+}
+
+.search input:focus {
+    box-shadow: none;
+    border: 2px solid blue
+}
+
+.search .fa-search {
+    position: absolute;
+    top: 20px;
+    left: 16px
+}
+
+.search button {
+    position: absolute;
+    top: 5px;
+    right: 5px;
+    height: 50px;
+    width: 110px;
+    background: blue
+}
     </style>
 </head>
 
@@ -244,7 +280,11 @@ if (isset($_GET['delid'])) {
             <div class="navbar-text">
                 <div class="icon"><a href="../admin_panel.php"><i class="fa fa-home"></i></a></div>
                 <div class="text">Home</div>
-                <input align="right" type="text" placeholder="search">
+                <div class="row height d-flex justify-content-center align-items-center">
+        <div class="col-md-8">
+            <div class="search"> <i class="fa fa-search"></i> <input type="text" class="form-control" placeholder="Have a question? Ask Now"> <button class="btn btn-primary">Search</button> </div>
+        </div>
+    </div>
             </div>
 
            
