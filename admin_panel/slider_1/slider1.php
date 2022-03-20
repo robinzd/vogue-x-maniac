3ea4e81c88c0bd1a511c80bdcb032d9a4a67e48a
robin <ibots.robin@gmail.com>
2022-03-21 00:54:00 +0530

    fixing some bug in the header in the slider1

--- admin_panel/slider_1/slider1.php
+++ admin_panel/slider_1/slider1.php
@@ -233,19 +233,7 @@ if (isset($_GET['delid'])) {
             margin-left: 10px;
         }
 
-        .form-inline .my-2 .my-lg-0{
-            display: inline-block;
-        }
-
-        .form-control .mr-sm-2{
-            display: inline;
-        }
-
-        .btn .btn-outline-success .my-2 .my-sm-0{
-            margin-left: 70px;
-        }
         
-
       
     </style>
 </head>
