1e07526df83e0fdcb06775b22b558d8c131bea68
robin <ibots.robin@gmail.com>
2022-03-20 20:47:29 +0530

    fixing some bug in the header in the brands

--- admin_panel/slider_1/slider1.php
+++ admin_panel/slider_1/slider1.php
@@ -245,6 +245,14 @@ if (isset($_GET['delid'])) {
                 <div class="icon"><a href="../admin_panel.php"><i class="fa fa-home"></i></a></div>
                 <div class="text">Home</div>
             </div>
+            <div class="input-group rounded right-aligned">
+  <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+  <span class="input-group-text border-0" id="search-addon">
+    <i class="fas fa-search"></i>
+  </span>
+</div>
+                
+            </div>
 
 
 
