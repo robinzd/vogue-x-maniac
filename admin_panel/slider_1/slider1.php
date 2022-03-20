4080edb67c9d80456c45f0e9691dcaa67ed0ed95
robin <ibots.robin@gmail.com>
2022-03-21 01:59:03 +0530

    adding footer to the admin panel section

--- admin_panel/slider_1/slider1.php
+++ admin_panel/slider_1/slider1.php
@@ -322,7 +322,7 @@ if (isset($_GET['delid'])) {
     </div>
     <footer class="bg-light text-center text-lg-start">
         <!-- Copyright -->
-        <div class="text-center p-3" style="background-color:black;">
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
             © 2022 Copyright:
             <a class="text-dark" href="">Vogue X Maniac</a>
         </div>
