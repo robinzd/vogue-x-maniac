4cfd3907ae63ed7a9777c3273282faae9722f407
robin <ibots.robin@gmail.com>
2022-03-20 16:54:37 +0530

    fixing the bug in the admil panel

--- admin_panel/admin_panel.php
+++ admin_panel/admin_panel.php
@@ -40,7 +40,7 @@
           <!--Container Main start-->
 
           <h4>Main Components</h4>
-          <div class="container-fluid">
+          <div class="container-fluid h-30">
               <div class="row align-middle">
                   <div class="col-md-6 col-lg-4 column">
                       <div class="card gr-1">
