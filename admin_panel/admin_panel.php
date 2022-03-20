7b0368af183fa0588ce69f7e9780a44f7017608f
robin <ibots.robin@gmail.com>
2022-03-20 18:34:50 +0530

    fixing the bug in the admin  panel

--- admin_panel/admin_panel.php
+++ admin_panel/admin_panel.php
@@ -41,7 +41,21 @@
 
           <h4>Main Components</h4>
           <div class="container-fluid h-100">
-              <div class="row align-middle">
+              <div class="row align-middle .d-none .d-xxl-block">
+                  <div class="col-md-6 col-lg-4 column">
+                      <div class="card gr-1">
+                          <div class="txt">
+                              <h1>BRANDING AND </br>
+                                  CORPORATE DESIGN</h1>
+                              <p>Visual communication and problem-solving</p>
+                          </div>
+                          <a href="#">more</a>
+                          <div class="ico-card">
+                              <i class="fa fa-rebel"></i>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="row align-middle-1.d-lg-none .d-xl-block">
                   <div class="col-md-6 col-lg-4 column">
                       <div class="card gr-1">
                           <div class="txt">
