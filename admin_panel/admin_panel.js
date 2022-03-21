9c2a4d74fed1847f208995e0b0b624ddb0ac8bb8
robin <ibots.robin@gmail.com>
2022-03-21 16:55:25 +0530

    changing the whole style of the admin panel

--- admin_panel/admin_panel.js
+++ admin_panel/admin_panel.js
@@ -36,66 +36,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     linkColor.forEach(l => l.addEventListener('click', colorLink))
 
 
-    // This is for able to see chart. We are using Apex Chart. U can check the documentation of Apex Charts too..
-    var options = {
-        series: [
-            {
-                name: "Net Profit",
-                data: [44, 55, 57, 56, 61, 58, 63, 60, 66],
-            },
-            {
-                name: "Revenue",
-                data: [76, 85, 101, 98, 87, 105, 91, 114, 94],
-            },
-            {
-                name: "Free Cash Flow",
-                data: [35, 41, 36, 26, 45, 48, 52, 53, 41],
-            },
-        ],
-        chart: {
-            type: "bar",
-            height: 250, // make this 250
-            sparkline: {
-                enabled: true, // make this true
-            },
-        },
-        plotOptions: {
-            bar: {
-                horizontal: false,
-                columnWidth: "55%",
-                endingShape: "rounded",
-            },
-        },
-        dataLabels: {
-            enabled: false,
-        },
-        stroke: {
-            show: true,
-            width: 2,
-            colors: ["transparent"],
-        },
-        xaxis: {
-            categories: ["Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"],
-        },
-        yaxis: {
-            title: {
-                text: "$ (thousands)",
-            },
-        },
-        fill: {
-            opacity: 1,
-        },
-        tooltip: {
-            y: {
-                formatter: function (val) {
-                    return "$ " + val + " thousands";
-                },
-            },
-        },
-    };
 
-    var chart = new ApexCharts(document.querySelector("#apex1"), options);
-    chart.render();
 
     //code to run since DOM is loaded and ready
 });
--- admin_panel/admin_panel.php
+++ admin_panel/admin_panel.php
@@ -33,8 +33,10 @@
                   </a>
                   <a href="#">
                       <img width="30" src="assets/avatar.svg" alt="" />
+                      <!-- <i class="fa fa-user-circle-o" aria-hidden="true"></i> -->
                   </a>
               </div>
+
           </header>
           <div class="l-navbar" id="nav-bar">
               <nav class="nav">
@@ -48,90 +50,124 @@
               </nav>
           </div>
           <!--Container Main start-->
-          <div class="main__cards">
-              <div class="card">
-                  <i class="fa fa-user-o fa-2x text-lightblue" aria-hidden="true"></i>
-                  <div class="card_inner">
-                      <p class="text-primary-p">Number of Subscribers</p>
-                      <span class="font-bold text-title">578</span>
-                  </div>
-              </div>
 
-              <div class="card">
-                  <i class="fa fa-calendar fa-2x text-red" aria-hidden="true"></i>
-                  <div class="card_inner">
-                      <p class="text-primary-p">Times of Watching</p>
-                      <span class="font-bold text-title">2467</span>
-                  </div>
-              </div>
-
-              <div class="card">
-                  <i class="fa fa-video-camera fa-2x text-yellow" aria-hidden="true"></i>
-                  <div class="card_inner">
-                      <p class="text-primary-p">Number of Videos</p>
-                      <span class="font-bold text-title">340</span>
-                  </div>
-              </div>
-
-              <div class="card">
-                  <i class="fa fa-thumbs-up fa-2x text-green" aria-hidden="true"></i>
-                  <div class="card_inner">
-                      <p class="text-primary-p">Number of Likes</p>
-                      <span class="font-bold text-title">645</span>
-                  </div>
-              </div>
-          </div>
-          <!-- MAIN CARDS ENDS HERE -->
-
-          <!-- CHARTS STARTS HERE -->
-          <div class="charts">
-              <div class="charts__left">
-                  <div class="charts__left__title">
-                      <div>
-                          <h1>Daily Reports</h1>
-                          <p>Cupertino, California, USA</p>
-                      </div>
-                      <i class="fa fa-usd" aria-hidden="true"></i>
-                  </div>
-                  <div id="apex1"></div>
-              </div>
-
-              <div class="charts__right">
-                  <div class="charts__right__title">
-                      <div>
-                          <h1>Stats Reports</h1>
-                          <p>Cupertino, California, USA</p>
-                      </div>
-                      <i class="fa fa-usd" aria-hidden="true"></i>
+          <h4>Main Components</h4>
+          <div class="container-fluid h-100 d-none d-lg-block">
+              <div class="row align-middle ">
+                  <div class="col-md-6 col-lg-4 column">
+                      <a href="./slider_1/slider1.php">
+                          <div class="card gr-1">
+                              <div class="txt">
+                                  <h1>Slider 1 </br>
+                                      Management</h1>
+                              </div>
+                              <div class="ico-card">
+                                  <i class="fa fa-gear"></i>
+                              </div>
+                          </div>
+                      </a>
                   </div>
 
-                  <div class="charts__right__cards">
-                      <div class="card1">
-                          <h1>Income</h1>
-                          <p>$75,300</p>
-                      </div>
-
-                      <div class="card2">
-                          <h1>Sales</h1>
-                          <p>$124,200</p>
-                      </div>
-
-                      <div class="card3">
-                          <h1>Users</h1>
-                          <p>3900</p>
-                      </div>
-
-                      <div class="card4">
-                          <h1>Orders</h1>
-                          <p>1881</p>
+                  <div class="col-md-6 col-lg-4 column">
+                      <a href="./brands/brand.php">
+                          <div class="card gr-2">
+                              <div class="txt">
+                                  <h1>Brands </br>
+                                      Management</h1>
+                              </div>
+                              <div class="ico-card">
+                                  <i class="fa fa-wrench"></i>
+                              </div>
+                          </div>
+                      </a>
+                  </div>
+                  <div class="col-md-6 col-lg-4 column">
+                      <a href="./slider_2/slider2.php">
+                          <div class="card gr-3">
+                              <div class="txt">
+                                  <h1>Slider 2 </br>
+                                      Management</h1>
+                              </div>
+                              <div class="ico-card">
+                                  <i class="fa fa-empire"></i>
+                              </div>
+                          </div>
+                      </a>
+                  </div>
+                  <div class="col-md-6 col-lg-4 column">
+                      <div class="card gr-4">
+                          <div class="txt">
+                              <h1>Latest Product </br>
+                                  Management</h1>
+                          </div>
+                          <div class="ico-card">
+                              <i class="fa fa-gears"></i>
+                          </div>
                       </div>
                   </div>
+
               </div>
           </div>
-          <!-- CHARTS ENDS HERE -->
-          </div>
-          </main>
 
+
+          <!--Container Main end-->
+
+          <div class="container-fluid h-100 d-lg-none">
+              <div class="row align-middle-1">
+                  <div class="col-md-6 col-lg-4 column">
+                      <a href="./slider_1/slider1.php">
+                          <div class="card gr-1">
+                              <div class="txt">
+                                  <h1>Slider 1 </br>
+                                      Management</h1>
+                              </div>
+                              <div class="ico-card">
+                                  <i class="fa fa-gear"></i>
+                              </div>
+                          </div>
+                      </a>
+                  </div>
+
+                  <div class="col-md-6 col-lg-4 column">
+                      <a href="./brands/brand.php">
+                          <div class="card gr-2">
+                              <div class="txt">
+                                  <h1>Brands </br>
+                                      Management</h1>
+                              </div>
+                              <div class="ico-card">
+                                  <i class="fa fa-wrench"></i>
+                              </div>
+                          </div>
+                      </a>
+                  </div>
+                  <div class="col-md-6 col-lg-4 column">
+                      <a href="./slider_2/slider2.php">
+                          <div class="card gr-3">
+                              <div class="txt">
+                                  <h1>Slider 2 </br>
+                                      Management</h1>
+                              </div>
+                              <div class="ico-card">
+                                  <i class="fa fa-empire"></i>
+                              </div>
+                          </div>
+                      </a>
+                  </div>
+                  <div class="col-md-6 col-lg-4 column">
+                      <div class="card gr-4">
+                          <div class="txt">
+                              <h1>Latest Product </br>
+                                  Management</h1>
+                          </div>
+                          <div class="ico-card">
+                              <i class="fa fa-gears"></i>
+                          </div>
+                      </div>
+                  </div>
+
+              </div>
+          </div>
 
 
 
