db9624badfc2982f75777a138f6a9c74755ed423
robin <ibots.robin@gmail.com>
2022-03-21 19:07:53 +0530

    adding the footer to the admin_panel.php

--- admin_panel/admin_panel.php
+++ admin_panel/admin_panel.php
@@ -175,7 +175,12 @@
           </div>
 
 
-
+          <footer class="bg-light text-center text-lg-start">
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color:ghostwhite;">
+            © 2022 Copyright:
+            <a class="text1" href="/index.php`">Vogue X Maniac</a>
+        </div>
 
 
 
