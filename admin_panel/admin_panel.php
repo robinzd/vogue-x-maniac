08fd825fe5c96230ee95dd45d7560ba44652d48d
robin <ibots.robin@gmail.com>
2022-03-21 15:52:22 +0530

    changing the whole style of the admin panel

--- admin_panel/admin_panel.php
+++ admin_panel/admin_panel.php
@@ -1,172 +1,224 @@
-  <!DOCTYPE html>
-  <html lang="en">
-
+<!DOCTYPE html>
+<html lang="en">
   <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Document</title>
-      <!-- fav icon -->
-      <link rel="icon" type="image/png" href="./favicon/vogue_x_maniac_png_K8m_icon.ico" />
-      <!-- bootsstrap cdn -->
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
-      <!-- font awesome cdn -->
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-      <!--box icon cdn -->
-      <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
-      <!-- link the external stylesheet -->
-      <link rel="stylesheet" type="text/css" href="admin_panel.css">
-
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+      integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="admin_panel.css" />
+    <title>CSS GRID DASHBOARD</title>
   </head>
+  <body id="body">
+    <div class="container">
+      <nav class="navbar">
+        <div class="nav_icon" onclick="toggleSidebar()">
+          <i class="fa fa-bars" aria-hidden="true"></i>
+        </div>
+        <div class="navbar__left">
+          <a href="#">Subscribers</a>
+          <a href="#">Video Management</a>
+          <a class="active_link" href="#">Admin</a>
+        </div>
+        <div class="navbar__right">
+          <a href="#">
+            <i class="fa fa-search" aria-hidden="true"></i>
+          </a>
+          <a href="#">
+            <i class="fa fa-clock-o" aria-hidden="true"></i>
+          </a>
+          <a href="#">
+            <img width="30" src="assets/avatar.svg" alt="" />
+            <!-- <i class="fa fa-user-circle-o" aria-hidden="true"></i> -->
+          </a>
+        </div>
+      </nav>
 
-  <body>
+      <main>
+        <div class="main__container">
+          <!-- MAIN TITLE STARTS HERE -->
 
-      <body id="body-pd">
-          <header class="header" id="header">
-              <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
-              <div class="header_img"> <img src="" alt=""> </div>
-          </header>
-          <div class="l-navbar" id="nav-bar">
-              <nav class="nav">
-                  <div> <a href="#" class="nav_logo"><i class="fa fa-user"></i><span class="nav_logo-name">Admin Panel</span> </a>
-                      <div class="nav_list"> <a href="#" class="nav_link active"> <i class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Dashboard</span> </a>
-                          <a href="#" class="nav_link"> <i class='bx bx-user nav_icon'></i> <span class="nav_name">Users</span> </a> <a href="#" class="nav_link"> <i class='bx bx-message-square-detail nav_icon'></i>
-                              <span class="nav_name">Messages</span> </a> <a href="#" class="nav_link"> <i class='bx bx-bookmark nav_icon'></i> <span class="nav_name">Bookmark</span> </a> <a href="#" class="nav_link">
-                              <i class='bx bx-folder nav_icon'></i> <span class="nav_name">Files</span> </a> <a href="#" class="nav_link"> <i class='bx bx-bar-chart-alt-2 nav_icon'></i> <span class="nav_name">Stats</span> </a>
-                      </div>
-                  </div> <a href="#" class="nav_link"> <i class='bx bx-log-out nav_icon'></i> <span class="nav_name">SignOut</span> </a>
-              </nav>
+          <div class="main__title">
+            <img src="./dashboard pics/undraw_Feeling_of_joy_re_cqko.png" alt="" />
+            <div class="main__greeting">
+              <h1>Hello Codersbite</h1>
+              <p>Welcome to your admin dashboard</p>
+            </div>
           </div>
-          <!--Container Main start-->
 
-          <h4>Main Components</h4>
-          <div class="container-fluid h-100 d-none d-lg-block">
-              <div class="row align-middle ">
-                  <div class="col-md-6 col-lg-4 column">
-                      <a href="./slider_1/slider1.php">
-                          <div class="card gr-1">
-                              <div class="txt">
-                                  <h1>Slider 1 </br>
-                                      Management</h1>
-                              </div>
-                              <div class="ico-card">
-                                  <i class="fa fa-gear"></i>
-                              </div>
-                          </div>
-                      </a>
-                  </div>
+          <!-- MAIN TITLE ENDS HERE -->
 
-                  <div class="col-md-6 col-lg-4 column">
-                  <a href="./brands/brand.php">
-                      <div class="card gr-2">
-                          <div class="txt">
-                              <h1>Brands </br>
-                                  Management</h1>
-                          </div>
-                          <div class="ico-card">
-                          <i class="fa fa-wrench"></i>
-                          </div>
-                      </div>
-                  </a>
-                  </div>
-                  <div class="col-md-6 col-lg-4 column">
-                  <a href="./slider_2/slider2.php">
-                      <div class="card gr-3">
-                          <div class="txt">
-                              <h1>Slider 2 </br>
-                                  Management</h1>
-                          </div>
-                          <div class="ico-card">
-                              <i class="fa fa-empire"></i>
-                          </div>
-                      </div>
-                  </a>
-                  </div>
-                  <div class="col-md-6 col-lg-4 column">
-                      <div class="card gr-4">
-                          <div class="txt">
-                              <h1>Latest Product </br>
-                                  Management</h1>
-                          </div>
-                          <div class="ico-card">
-                              <i class="fa fa-gears"></i>
-                          </div>
-                      </div>
-                  </div>
-
+          <!-- MAIN CARDS STARTS HERE -->
+          <div class="main__cards">
+            <div class="card">
+              <i
+                class="fa fa-user-o fa-2x text-lightblue"
+                aria-hidden="true"
+              ></i>
+              <div class="card_inner">
+                <p class="text-primary-p">Number of Subscribers</p>
+                <span class="font-bold text-title">578</span>
               </div>
-          </div>
+            </div>
 
-
-          <!--Container Main end-->
-
-          <div class="container-fluid h-100 d-lg-none">
-              <div class="row align-middle-1">
-                  <div class="col-md-6 col-lg-4 column">
-                      <a href="./slider_1/slider1.php">
-                          <div class="card gr-1">
-                              <div class="txt">
-                                  <h1>Slider 1 </br>
-                                      Management</h1>
-                              </div>
-                              <div class="ico-card">
-                                  <i class="fa fa-gear"></i>
-                              </div>
-                          </div>
-                      </a>
-                  </div>
-
-                  <div class="col-md-6 col-lg-4 column">
-                  <a href="./brands/brand.php">
-                      <div class="card gr-2">
-                          <div class="txt">
-                              <h1>Brands </br>
-                                  Management</h1>
-                          </div>
-                          <div class="ico-card">
-                          <i class="fa fa-wrench"></i>
-                          </div>
-                      </div>
-                  </a>
-                  </div>
-                  <div class="col-md-6 col-lg-4 column">
-                  <a href="./slider_2/slider2.php">
-                      <div class="card gr-3">
-                          <div class="txt">
-                              <h1>Slider 2 </br>
-                                  Management</h1>
-                          </div>
-                          <div class="ico-card">
-                              <i class="fa fa-empire"></i>
-                          </div>
-                      </div>
-                  </a>
-                  </div>
-                  <div class="col-md-6 col-lg-4 column">
-                      <div class="card gr-4">
-                          <div class="txt">
-                              <h1>Latest Product </br>
-                                  Management</h1>
-                          </div>
-                          <div class="ico-card">
-                              <i class="fa fa-gears"></i>
-                          </div>
-                      </div>
-                  </div>
-
+            <div class="card">
+              <i class="fa fa-calendar fa-2x text-red" aria-hidden="true"></i>
+              <div class="card_inner">
+                <p class="text-primary-p">Times of Watching</p>
+                <span class="font-bold text-title">2467</span>
               </div>
+            </div>
+
+            <div class="card">
+              <i
+                class="fa fa-video-camera fa-2x text-yellow"
+                aria-hidden="true"
+              ></i>
+              <div class="card_inner">
+                <p class="text-primary-p">Number of Videos</p>
+                <span class="font-bold text-title">340</span>
+              </div>
+            </div>
+
+            <div class="card">
+              <i
+                class="fa fa-thumbs-up fa-2x text-green"
+                aria-hidden="true"
+              ></i>
+              <div class="card_inner">
+                <p class="text-primary-p">Number of Likes</p>
+                <span class="font-bold text-title">645</span>
+              </div>
+            </div>
           </div>
+          <!-- MAIN CARDS ENDS HERE -->
 
+          <!-- CHARTS STARTS HERE -->
+          <div class="charts">
+            <div class="charts__left">
+              <div class="charts__left__title">
+                <div>
+                  <h1>Daily Reports</h1>
+                  <p>Cupertino, California, USA</p>
+                </div>
+                <i class="fa fa-usd" aria-hidden="true"></i>
+              </div>
+              <div id="apex1"></div>
+            </div>
 
+            <div class="charts__right">
+              <div class="charts__right__title">
+                <div>
+                  <h1>Stats Reports</h1>
+                  <p>Cupertino, California, USA</p>
+                </div>
+                <i class="fa fa-usd" aria-hidden="true"></i>
+              </div>
 
+              <div class="charts__right__cards">
+                <div class="card1">
+                  <h1>Income</h1>
+                  <p>$75,300</p>
+                </div>
 
+                <div class="card2">
+                  <h1>Sales</h1>
+                  <p>$124,200</p>
+                </div>
 
+                <div class="card3">
+                  <h1>Users</h1>
+                  <p>3900</p>
+                </div>
 
+                <div class="card4">
+                  <h1>Orders</h1>
+                  <p>1881</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- CHARTS ENDS HERE -->
+        </div>
+      </main>
 
-          <!-- j query -->
-          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
-          <script src="admin_panel.js"></script>
+      <div id="sidebar">
+        <div class="sidebar__title">
+          <div class="sidebar__img">
+            <img src="assets/logo.png" alt="logo" />
+            <h1>Codersbite</h1>
+          </div>
+          <i
+            onclick="closeSidebar()"
+            class="fa fa-times"
+            id="sidebarIcon"
+            aria-hidden="true"
+          ></i>
+        </div>
 
-      </body>
-
-  </html>
+        <div class="sidebar__menu">
+          <div class="sidebar__link active_menu_link">
+            <i class="fa fa-home"></i>
+            <a href="#">Dashboard</a>
+          </div>
+          <h2>MNG</h2>
+          <div class="sidebar__link">
+            <i class="fa fa-user-secret" aria-hidden="true"></i>
+            <a href="#">Admin Management</a>
+          </div>
+          <div class="sidebar__link">
+            <i class="fa fa-building-o"></i>
+            <a href="#">Company Management</a>
+          </div>
+          <div class="sidebar__link">
+            <i class="fa fa-wrench"></i>
+            <a href="#">Employee Management</a>
+          </div>
+          <div class="sidebar__link">
+            <i class="fa fa-archive"></i>
+            <a href="#">Warehouse</a>
+          </div>
+          <div class="sidebar__link">
+            <i class="fa fa-handshake-o"></i>
+            <a href="#">Contracts</a>
+          </div>
+          <h2>LEAVE</h2>
+          <div class="sidebar__link">
+            <i class="fa fa-question"></i>
+            <a href="#">Requests</a>
+          </div>
+          <div class="sidebar__link">
+            <i class="fa fa-sign-out"></i>
+            <a href="#">Leave Policy</a>
+          </div>
+          <div class="sidebar__link">
+            <i class="fa fa-calendar-check-o"></i>
+            <a href="#">Special Days</a>
+          </div>
+          <div class="sidebar__link">
+            <i class="fa fa-files-o"></i>
+            <a href="#">Apply for leave</a>
+          </div>
+          <h2>PAYROLL</h2>
+          <div class="sidebar__link">
+            <i class="fa fa-money"></i>
+            <a href="#">Payroll</a>
+          </div>
+          <div class="sidebar__link">
+            <i class="fa fa-briefcase"></i>
+            <a href="#">Paygrade</a>
+          </div>
+          <div class="sidebar__logout">
+            <i class="fa fa-power-off"></i>
+            <a href="#">Log out</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    <script src="admin_panel.js"></script>
+  </body>
+</html>
