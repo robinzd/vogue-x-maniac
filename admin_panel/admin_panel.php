6a1532aa4c725b6bf4828099217719be5c1c4bd5
robin <ibots.robin@gmail.com>
2022-03-21 23:22:11 +0530

    fixing some issuses in the footer

--- admin_panel/admin_panel.php
+++ admin_panel/admin_panel.php
@@ -218,7 +218,7 @@
               </div>
           </div>
 
-
+          
           <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
               © 2020 Copyright:
               <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
