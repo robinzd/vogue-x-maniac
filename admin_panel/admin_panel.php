c0754a49a6bbe94cc6416e9f5cc84cdb189d71d4
robin <ibots.robin@gmail.com>
2022-03-21 23:52:23 +0530

    fixing some issuses in the footer

--- admin_panel/admin_panel.php
+++ admin_panel/admin_panel.php
@@ -134,14 +134,15 @@
                   </div>
 
               </div>
-          </div>
-          <footer class="bg-light text-center text-lg-start d-none d-lg-block">
+              <footer class="bg-light text-center text-lg-start">
               <!-- Copyright -->
               <div class="text-center p-3" style="background-color:ghostwhite;">
                   © 2022 Copyright:
                   <a class="text1" href="/index.php`">Vogue X Maniac</a>
               </div>
 
+          </div>
+        
 
               <!--Container Main end-->
 
