4526686c5b905debb9454c8167ed0d25e4bcf2bb
robin <ibots.robin@gmail.com>
2022-03-21 23:18:55 +0530

    fixing some issuses in the footer

--- admin_panel/admin_panel.php
+++ admin_panel/admin_panel.php
@@ -219,12 +219,7 @@
           </div>
 
 
-          <footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50">
-    <div class=" text-center">
-      <small>Copyright &copy; Your Website</small>
-    </div>
-  </footer>
-
+         
 
 
 
