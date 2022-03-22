49955f26c59fe5fa485125c61c5a38dbb481ed93
robin <ibots.robin@gmail.com>
2022-03-22 15:50:03 +0530

    fixing the bug in the footer

--- admin_panel/admin_panel.php
+++ admin_panel/admin_panel.php
@@ -228,18 +228,13 @@
                       </div>
                   </a>
                   </div>
-                  <footer class="bg-light text-center text-lg-start">
-                      <!-- Copyright -->
-                      <div class="text-center p-3" style="background-color:ghostwhite;">
-                          © 2022 Copyright:
-                          <a class="text1" href="/index.php`">Vogue X Maniac</a>
-                      </div>
+                 
 
 
               </div>
 
           </div>
-
+         
 
 
 
@@ -259,6 +254,13 @@
                   <a class="text1" href="/index.php`">Vogue X Maniac</a>
               </div>
           </footer>
+          <footer class="bg-light text-center text-lg-start d-lg-none">
+                      <!-- Copyright -->
+                      <div class="text-center p-3" style="background-color:ghostwhite;">
+                          © 2022 Copyright:
+                          <a class="text1" href="/index.php`">Vogue X Maniac</a>
+                      </div>
+
 
       </body>
 
