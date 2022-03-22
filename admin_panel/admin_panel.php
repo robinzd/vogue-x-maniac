648a912e72552f6ef76f4dccee8ac9bfe34fb142
robin <ibots.robin@gmail.com>
2022-03-22 16:06:55 +0530

    fixing the bug in the footer

--- admin_panel/admin_panel.php
+++ admin_panel/admin_panel.php
@@ -35,7 +35,7 @@
               </div>
 
           </header>
-          <div class="l-navbar d-none d-lg-block" id="nav-bar">
+          <div class="l-navbar" id="nav-bar">
               <nav class="nav">
                   <div> <a href="#" class="nav_logo"><i class="fa fa-user"></i><span class="nav_logo-name">Admin Panel</span> </a>
                       <div class="nav_list"> <a href="#" class="nav_link active"> <i class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Dashboard</span> </a>
@@ -47,17 +47,7 @@
               </nav>
           </div><br><br>
 
-          <div class="l-navbar-1 d-lg-none" id="nav-bar">
-              <nav class="nav">
-                  <div> <a href="#" class="nav_logo"><i class="fa fa-user"></i><span class="nav_logo-name">Admin Panel</span> </a>
-                      <div class="nav_list"> <a href="#" class="nav_link active"> <i class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Dashboard</span> </a>
-                          <a href="#" class="nav_link"> <i class='bx bx-user nav_icon'></i> <span class="nav_name">Users</span> </a> <a href="#" class="nav_link"> <i class='bx bx-message-square-detail nav_icon'></i>
-                              <span class="nav_name">Messages</span> </a> <a href="#" class="nav_link"> <i class='bx bx-bookmark nav_icon'></i> <span class="nav_name">Bookmark</span> </a> <a href="#" class="nav_link">
-                              <i class='bx bx-folder nav_icon'></i> <span class="nav_name">Files</span> </a> <a href="#" class="nav_link"> <i class='bx bx-bar-chart-alt-2 nav_icon'></i> <span class="nav_name">Stats</span> </a>
-                      </div>
-                  </div> <a href="#" class="nav_link"> <i class='bx bx-log-out nav_icon'></i> <span class="nav_name">SignOut</span> </a>
-              </nav>
-          </div><br><br>
+        
 
           <!--Container Main start-->
 
