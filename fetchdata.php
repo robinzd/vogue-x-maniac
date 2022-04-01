49dbba47f5f76d584b67a3279e701f7db2abc70f
robin <ibots.robin@gmail.com>
2022-04-01 17:05:58 +0530

    adding the filter feauter into the shop page

---
+++ fetchdata.php
@@ -0,0 +1,89 @@
+<?php
+
+include("./db_conn.php");
+
+include("./conn.php");
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- link the external css sheet -->
+    <link rel="stylesheet" type="text/css" href="shop.css">
+    <title>Fetching Data</title>
+</head>
+
+<body>
+
+    <?php
+
+    //fetchdata.php
+
+   
+
+    if (isset($_POST["action"])) {
+        $query = "
+  SELECT * FROM shop_page WHERE product_status = '1'
+ ";
+        if (isset($_POST["minimum_price"], $_POST["maximum_price"]) && !empty($_POST["minimum_price"]) && !empty($_POST["maximum_price"])) {
+            $query .= "
+   AND product_price BETWEEN '" . $_POST["minimum_price"] . "' AND '" . $_POST["maximum_price"] . "'
+  ";
+        }
+        if (isset($_POST["brand"])) {
+            $brand_filter = implode("','", $_POST["brand"]);
+            $query .= "
+   AND product_brand IN('" . $brand_filter . "')
+  ";
+        }
+        if (isset($_POST["category"])) {
+            $category_filter = implode("','", $_POST["category"]);
+            $query .= "
+   AND product_category IN('" . $category_filter . "')
+  ";
+        }
+
+
+        $statement = $connect->prepare($query);
+        $statement->execute();
+        $result = $statement->fetchAll();
+        $total_row = $statement->rowCount();
+        $output = '';
+        if ($total_row > 0) {
+            foreach ($result as $row) {
+                $output .= '
+    <div class="col-sm-6 col-md-4">
+                            <div class="card bg-white">
+        <img class="card-img-top" src="./admin_area/product_images/' . $row['product_image'] . '" style="width:100%">
+        <div class="card-body">
+            <h5 class="card-title text-center">' . $row['product_title'] . '</h5>
+        <p class="card-text  text-center"><s>₹' . $row['product_strikeout_price'] . '</s>₹' . $row['product_price'] . '</p>
+            <div class="text-center">
+                <a href="details.php" class="btn btn-success">See Details</a>
+                <a href="#" class="btn btn-success">Add to Cart</a>
+            </div>
+        </div>
+    </div>
+    </div>
+   ';
+            }
+        } else {
+            $output = '<h3>No Data Found</h3>';
+        }
+        echo $output;
+    }
+    ?>
+
+</body>
+
+</html>
+
+
--- shop.php
+++ shop.php
@@ -279,10 +279,66 @@ include("./conn.php");
 
             <!-- j query -->
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-            <!-- owl carousel -->
             <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-            
 
+            <script>
+                $(document).ready(function() {
+
+                    filter_data();
+
+                    function filter_data() {
+                        $('.filter_data').html('<div id="loading" style=""></div>');
+                        var action = 'fetch_data';
+                        var minimum_price = $('#hidden_minimum_price').val();
+                        var maximum_price = $('#hidden_maximum_price').val();
+                        var brand = get_filter('brand');
+                        var category = get_filter('category');
+                       
+                        $.ajax({
+                            url: "fetchdata.php",
+                            method: "POST",
+                            data: {
+                                action: action,
+                                minimum_price: minimum_price,
+                                maximum_price: maximum_price,
+                                brand: brand,
+                                category: ram,
+                               
+                            },
+                            success: function(data) {
+                                $('.filter_data').html(data);
+                            }
+                        });
+                    }
+
+                    function get_filter(class_name) {
+                        var filter = [];
+                        $('.' + class_name + ':checked').each(function() {
+                            filter.push($(this).val());
+                        });
+                        return filter;
+                    }
+
+                    $('.common_selector').click(function() {
+                        filter_data();
+                    });
+
+                    $('#price_range').slider({
+                        range: true,
+                        min: 1000,
+                        max: 65000,
+                        values: [1000, 65000],
+                        step: 500,
+                        stop: function(event, ui) {
+                            $('#price_show').html(ui.values[0] + ' - ' + ui.values[1]);
+                            $('#hidden_minimum_price').val(ui.values[0]);
+                            $('#hidden_maximum_price').val(ui.values[1]);
+                            filter_data();
+                        }
+                    });
+
+                });
+            </script>
 
 </body>
 
