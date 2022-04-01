13e53ca33ef5dec95ff807f8e4298b540041a7bc
robin <ibots.robin@gmail.com>
2022-04-01 16:16:31 +0530

    adding the filter feauter into the shop page

--- shop.php
+++ shop.php
@@ -110,41 +110,46 @@ include("./conn.php");
                         ?>
                     </div>
 
+                    <div class="col-md-9">
+                        <br />
+                        <div class="row filter_data">
 
+                        </div>
+                    </div>
 
 
                 </form>
             </div>
 
-       
-
-
-
-        <div class="col-sm-8 col-md-9">
-            <!-- Filters -->
-            <div class="row">
-
-                <?php
-
-
-                $get_product_slider = "select * from shop_page";
-
-                $run_product_image = mysqli_query($conn, $get_product_slider);
-
-
-
-                while ($row_product_image = mysqli_fetch_array($run_product_image)) {
-
-                    $product_image = $row_product_image['product_image'];
-                    $product_title = $row_product_image['product_title'];
-                    $product_strikeout_price = $row_product_image['product_strikeout_price'];
-                    $product_price = $row_product_image['product_price'];
 
 
 
 
+            <div class="col-sm-8 col-md-9">
+                <!-- Filters -->
+                <div class="row">
 
-                    echo "<div class='col-sm-6 col-md-4'>
+                    <?php
+
+
+                    $get_product_slider = "select * from shop_page";
+
+                    $run_product_image = mysqli_query($conn, $get_product_slider);
+
+
+
+                    while ($row_product_image = mysqli_fetch_array($run_product_image)) {
+
+                        $product_image = $row_product_image['product_image'];
+                        $product_title = $row_product_image['product_title'];
+                        $product_strikeout_price = $row_product_image['product_strikeout_price'];
+                        $product_price = $row_product_image['product_price'];
+
+
+
+
+
+                        echo "<div class='col-sm-6 col-md-4'>
                             <div class='card bg-white'>
         <img class='card-img-top' src='./admin_area/product_images/$product_image' alt='' style='width:100%'>
         <div class='card-body'>
@@ -163,13 +168,10 @@ include("./conn.php");
 
 
 ";
-                }
+                    }
 
 
-                ?>
-
-
-
+                    ?>
 
 
 
@@ -177,98 +179,109 @@ include("./conn.php");
 
 
 
-            </div> <!-- / .row -->
-        </div>
-
-   
 
 
 
-    <!-- Footer -->
-
-    <footer class="text-center text-lg-start text-dark" style="background-color:lightgrey">
-        <!-- Grid container -->
-        <div class="container p-4 pb-0">
-            <!-- Section: Links -->
-            <section class="">
-                <!--Grid row-->
-                <div class="row">
-                    <!-- Grid column -->
-                    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                        <img class="footerimage" src="./header images/vogue x maniac png.png" alt="brand image">
-                        <p class="text-white">
-                            Here you can use rows and columns to organize your footer
-                            content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit.
-                        </p>
-                    </div>
-                    <!-- Grid column -->
-
-                    <hr class="w-100 clearfix d-md-none" />
-
-                    <!-- Grid column -->
-                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                        <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
-                        <p class="text-white">MDBootstrap</p>
-                        <p class="text-white">MDWordPress</p>
-                        <p class="text-white">BrandFlow</p>
-                        <p class="text-white">Angular</p>
-                    </div>
-                    <!-- Grid column -->
-
-                    <hr class="w-100 clearfix d-md-none" />
-
-                    <!-- Grid column -->
-                    <hr class="w-100 clearfix d-md-none" />
-
-                    <!-- Grid column -->
-                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-                        <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
-                        <p class="text-white"><i class="fas fa-home text-white mr-3"></i> New York, NY 10012, US</p>
-                        <p class="text-white" id="address"><i class="fas fa-envelope text-white mr-3"></i> inf0@text-white@gmail.com
-                        </p>
-                        <p class="text-white"><i class="fas fa-phone text-white mr-3"></i> + 01 234 567 88</p>
-                        <p class="text-white"><i class="fas fa-print text-white mr-3"></i> + 01 234 567 89</p>
-                    </div>
-                    <!-- Grid column -->
-
-                    <!-- Grid column -->
-                    <div class="col-md-3 col-lg-4 col-xl-4 mx-auto mt-3">
-                        <h6 class="text-uppercase mb-4 font-weight-bold">Follow us</h6>
-
-                        <!-- Facebook -->
-                        <a class="btn pmd-btn-fab pmd-ripple-effect btn-primary pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Facebook" role="button"><i class="fab fa-facebook"></i></a>
-
-                        <!-- Twitter -->
-                        <a class="btn pmd-btn-fab pmd-ripple-effect btn-info pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Twitter" role="button"><i class="fab fa-twitter"></i></a>
-
-                        <!-- youtube -->
-                        <a class="btn pmd-btn-fab pmd-ripple-effect btn-secondary pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="youtube" role="button"><i class="fab fa-youtube"></i></a>
+                </div> <!-- / .row -->
+            </div>
 
 
-                        <!-- Instagram -->
-                        <a class="btn pmd-btn-fab pmd-ripple-effect btn-danger pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Instagram" role="button"><i class="fab fa-instagram"></i></a>
 
-                    </div>
+
+
+            <!-- Footer -->
+
+            <footer class="text-center text-lg-start text-dark" style="background-color:lightgrey">
+                <!-- Grid container -->
+                <div class="container p-4 pb-0">
+                    <!-- Section: Links -->
+                    <section class="">
+                        <!--Grid row-->
+                        <div class="row">
+                            <!-- Grid column -->
+                            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                                <img class="footerimage" src="./header images/vogue x maniac png.png" alt="brand image">
+                                <p class="text-white">
+                                    Here you can use rows and columns to organize your footer
+                                    content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                                    elit.
+                                </p>
+                            </div>
+                            <!-- Grid column -->
+
+                            <hr class="w-100 clearfix d-md-none" />
+
+                            <!-- Grid column -->
+                            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                                <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+                                <p class="text-white">MDBootstrap</p>
+                                <p class="text-white">MDWordPress</p>
+                                <p class="text-white">BrandFlow</p>
+                                <p class="text-white">Angular</p>
+                            </div>
+                            <!-- Grid column -->
+
+                            <hr class="w-100 clearfix d-md-none" />
+
+                            <!-- Grid column -->
+                            <hr class="w-100 clearfix d-md-none" />
+
+                            <!-- Grid column -->
+                            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                                <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+                                <p class="text-white"><i class="fas fa-home text-white mr-3"></i> New York, NY 10012, US</p>
+                                <p class="text-white" id="address"><i class="fas fa-envelope text-white mr-3"></i> inf0@text-white@gmail.com
+                                </p>
+                                <p class="text-white"><i class="fas fa-phone text-white mr-3"></i> + 01 234 567 88</p>
+                                <p class="text-white"><i class="fas fa-print text-white mr-3"></i> + 01 234 567 89</p>
+                            </div>
+                            <!-- Grid column -->
+
+                            <!-- Grid column -->
+                            <div class="col-md-3 col-lg-4 col-xl-4 mx-auto mt-3">
+                                <h6 class="text-uppercase mb-4 font-weight-bold">Follow us</h6>
+
+                                <!-- Facebook -->
+                                <a class="btn pmd-btn-fab pmd-ripple-effect btn-primary pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Facebook" role="button"><i class="fab fa-facebook"></i></a>
+
+                                <!-- Twitter -->
+                                <a class="btn pmd-btn-fab pmd-ripple-effect btn-info pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Twitter" role="button"><i class="fab fa-twitter"></i></a>
+
+                                <!-- youtube -->
+                                <a class="btn pmd-btn-fab pmd-ripple-effect btn-secondary pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="youtube" role="button"><i class="fab fa-youtube"></i></a>
+
+
+                                <!-- Instagram -->
+                                <a class="btn pmd-btn-fab pmd-ripple-effect btn-danger pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Instagram" role="button"><i class="fab fa-instagram"></i></a>
+
+                            </div>
+                        </div>
+                        <!--Grid row-->
+                    </section>
+                    <!-- Section: Links -->
                 </div>
-                <!--Grid row-->
-            </section>
-            <!-- Section: Links -->
-        </div>
-        <!-- Grid container -->
+                <!-- Grid container -->
 
-        <!-- Copyright -->
-        <div class="text-center p-3" style="background-color: rgba(255, 255, 255, 0.096)">
+                <!-- Copyright -->
+                <div class="text-center p-3" style="background-color: rgba(255, 255, 255, 0.096)">
 
-            <p class="text-dark"> ©2022 Vogue X Maniac.All Rights Reserved
-            <p>
-                <img class="px-2" src="./payment_pics/2560px-MasterCard_Logo.svg-removebg-preview.png">
-                <img class="px-2" src="./payment_pics/1200px-Visa.svg-removebg-preview.png">
-                <img class="px-2" src="./payment_pics/paypal-logo-removebg-preview.png">
-        </div>
-        <!-- Copyright -->
-    </footer>
-    <!-- end of the footer -->
+                    <p class="text-dark"> ©2022 Vogue X Maniac.All Rights Reserved
+                    <p>
+                        <img class="px-2" src="./payment_pics/2560px-MasterCard_Logo.svg-removebg-preview.png">
+                        <img class="px-2" src="./payment_pics/1200px-Visa.svg-removebg-preview.png">
+                        <img class="px-2" src="./payment_pics/paypal-logo-removebg-preview.png">
+                </div>
+                <!-- Copyright -->
+            </footer>
+            <!-- end of the footer -->
+
+
+
+            <!-- j query -->
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+            <!-- owl carousel -->
+            <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+            
 
 
 </body>
