ff2bacc24c38d98692981b5ec3d2c5f04d86426a
robin <ibots.robin@gmail.com>
2022-04-02 22:55:24 +0530

    testing shop page

--- shop.php
+++ shop.php
@@ -16,6 +16,7 @@ include("./conn.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=shift_jis"> 
     <!-- bootstsrap cdn -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- fav icon -->
