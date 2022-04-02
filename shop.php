874b3e926eaaf5c721cc6457aa3760dc4f403b84
robin <ibots.robin@gmail.com>
2022-04-02 22:52:53 +0530

    testing shop page

--- shop.php
+++ shop.php
@@ -25,7 +25,7 @@ include("./conn.php");
     <!-- link the external css sheet -->
     <link rel="stylesheet" type="text/css" href="shop.css">
     <!-- jquery -->
-    <!-- <script src="./js/jquery-1.10.2.min.js"></script> -->
+    <script src="./js/jquery-1.10.2.min.js"></script>
     <!-- <script src="./js/jquery-ui.js"></script> -->
     <!-- jquery css -->
     <!-- <link href="./css/jquery-ui.css" rel="stylesheet"> -->
@@ -34,9 +34,9 @@ include("./conn.php");
     <!--js-bootsstrap -->
     <!-- <script src="./js/jquery-331.min.js"></script> -->
     <!-- ui jquery -->
-      <link href="https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" rel="stylesheet">  
-      <script src="https://code.jquery.com/jquery-1.10.2.js"></script>  
-      <script src="https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+    <link href="https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
     <title>Shop</title>
 </head>
 
@@ -289,61 +289,64 @@ include("./conn.php");
             <!-- end of the footer -->
 
 
-           
+
             <script>
-                $(document).ready(function() {
+                document.addEventListener("DOMContentLoaded", function(event) {
+                    $(document).ready(function() {
 
-                    filter_data();
+                        filter_data();
 
-                    function filter_data() {
-                        $('.filter_data').html('<div id="loading" style=""></div>');
-                        var action = 'fetch_data';
-                        var minimum_price = $('#hidden_minimum_price').val();
-                        var maximum_price = $('#hidden_maximum_price').val();
-                        var brand = get_filter('brand');
-                        var category = get_filter('category');
+                        function filter_data() {
+                            $('.filter_data').html('<div id="loading" style=""></div>');
+                            var action = 'fetch_data';
+                            var minimum_price = $('#hidden_minimum_price').val();
+                            var maximum_price = $('#hidden_maximum_price').val();
+                            var brand = get_filter('brand');
+                            var category = get_filter('category');
 
-                        $.ajax({
-                            url: "fetchdata.php",
-                            method: "POST",
-                            data: {
-                                action: action,
-                                minimum_price: minimum_price,
-                                maximum_price: maximum_price,
-                                brand: brand,
-                                category: category,
+                            $.ajax({
+                                url: "fetchdata.php",
+                                method: "POST",
+                                data: {
+                                    action: action,
+                                    minimum_price: minimum_price,
+                                    maximum_price: maximum_price,
+                                    brand: brand,
+                                    category: category,
 
-                            },
-                            success: function(data) {
-                                $('.filter_data').html(data);
+                                },
+                                success: function(data) {
+                                    $('.filter_data').html(data);
+                                }
+                            });
+                        }
+
+                        function get_filter(class_name) {
+                            var filter = [];
+                            $('.' + class_name + ':checked').each(function() {
+                                filter.push($(this).val());
+                            });
+                            return filter;
+                        }
+
+                        $('.form-check-input').click(function() {
+                            filter_data();
+                        });
+
+                        $('#price_range').slider({
+                            range: true,
+                            min: 1000,
+                            max: 65000,
+                            values: [1000, 65000],
+                            step: 500,
+                            stop: function(event, ui) {
+                                $('#price_show').html(ui.values[0] + ' - ' + ui.values[1]);
+                                $('#hidden_minimum_price').val(ui.values[0]);
+                                $('#hidden_maximum_price').val(ui.values[1]);
+                                filter_data();
                             }
                         });
-                    }
 
-                    function get_filter(class_name) {
-                        var filter = [];
-                        $('.' + class_name + ':checked').each(function() {
-                            filter.push($(this).val());
-                        });
-                        return filter;
-                    }
-
-                    $('.form-check-input').click(function() {
-                        filter_data();
-                    });
-
-                    $('#price_range').slider({
-                        range: true,
-                        min: 1000,
-                        max: 65000,
-                        values: [1000, 65000],
-                        step: 500,
-                        stop: function(event, ui) {
-                            $('#price_show').html(ui.values[0] + ' - ' + ui.values[1]);
-                            $('#hidden_minimum_price').val(ui.values[0]);
-                            $('#hidden_maximum_price').val(ui.values[1]);
-                            filter_data();
-                        }
                     });
 
                 });
