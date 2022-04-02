ada7fc8e934eb8ffc6f80b6848e40fc4e52f6d5b
robin <ibots.robin@gmail.com>
2022-04-02 16:34:25 +0530

    testing shop page

--- shop.php
+++ shop.php
@@ -26,11 +26,11 @@ include("./conn.php");
     <link rel="stylesheet" type="text/css" href="shop.css">
     <!-- jquery -->
     <script src="./js/jquery-1.10.2.min.js"></script>
-    <script src="./js/jquery-ui.js"></script>
+    <!-- <script src="./js/jquery-ui.js"></script> -->
     <!-- jquery css -->
-    <link href="./css/jquery-ui.css" rel="stylesheet">
+    <!-- <link href="./css/jquery-ui.css" rel="stylesheet"> -->
     <!-- js files -->
-    <script src="./js/bootstrap.min.js"></script>
+    <!-- <script src="./js/bootstrap.min.js"></script> -->
     <link href="https://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
     <script src="https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
