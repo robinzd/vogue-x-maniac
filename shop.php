49fcf5e92bac0ac703dcc73336621a17473d30ef
robin <ibots.robin@gmail.com>
2022-04-02 22:44:13 +0530

    testing shop page

--- shop.php
+++ shop.php
@@ -289,12 +289,7 @@ include("./conn.php");
             <!-- end of the footer -->
 
 
-            <!-- j query -->
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-            <!-- owl carousel -->
-            <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-
+           
             <script>
                 $(document).ready(function() {
 
