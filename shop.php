897eee787dd7ca3326e3f869ba7c0a40d135f709
robin <ibots.robin@gmail.com>
2022-04-02 16:13:13 +0530

    testing shop page

--- shop.php
+++ shop.php
@@ -28,7 +28,7 @@ include("./conn.php");
     <script src="js/jquery-1.10.2.min.js"></script>
     <script src="js/jquery-ui.js"></script>
     <!-- jquery css -->
-    <link href="css/jquery-ui.css" rel="stylesheet">
+    <link href="./css/jquery-ui.css" rel="stylesheet">
     <!-- js files -->
     <script src="js/bootstrap.min.js"></script>
     <title>Shop</title>
