ce62f04a0677a638d3e4e32f18dfdda8183c7678
robin <ibots.robin@gmail.com>
2022-04-06 00:23:57 +0530

    fixing bug in the categories section

--- index.php
+++ index.php
@@ -230,13 +230,11 @@ include("./conn.php");
         <div class="gallery">
 
 
-            <div class="gallery__column">
-
-                <a href="#" target="_blank" class="gallery__link">
-
+            
+                
                     <?php
 
-                    $get_product_category = "select * from product_category LIMIT 0,1";
+                    $get_product_category = "select * from product_category";
 
                     $run_gallery_image = mysqli_query($conn, $get_product_category);
 
@@ -249,10 +247,14 @@ include("./conn.php");
 
 
 
-                        echo "<figure class='gallery__thumb'>
+                        echo "<div class='gallery__column'>
+                        <a href='#' target='_blank' class='gallery__link'>
+                    <figure class='gallery__thumb'>
 <img src='./categories_images/$category_image' alt='mens and womens shoes' class='gallery__image'>
 <figcaption class='gallery__caption'>$category_name</figcaption>
 </figure>
+</a>
+</div>
 
 
 
@@ -267,7 +269,7 @@ include("./conn.php");
 
                     ?>
 
-                </a>
+                
 
 
 
@@ -276,47 +278,7 @@ include("./conn.php");
 
 
 
-
-                <a href="#" target="_blank" class="gallery__link">
-
-
-                    <?php
-
-                    $get_product_category = "select * from product_category LIMIT 1,1";
-
-                    $run_gallery_image = mysqli_query($conn, $get_product_category);
-
-
-
-                    while ($row_category_picture = mysqli_fetch_array($run_gallery_image)) {
-
-                        $category_image = $row_category_picture['category_image'];
-                        $category_name = $row_category_picture['category_name'];
-
-
-
-                        echo "<figure class='gallery__thumb'>
-    <img src='./categories_images/$category_image' alt='mens and womens shoes' class='gallery__image'>
-    <figcaption class='gallery__caption'>$category_name</figcaption>
-</figure>
-
-
-
-";
-                    }
-
-
-                    ?>
-
-
-
-
-
-                </a>
-
-
-
-            </div>
+            
 
             <div class="gallery__column">
                 <a href="#" target="_blank" class="gallery__link">
