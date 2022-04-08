052e7653b95af3f32c4b76dd3542212148a9fd86
robin <ibots.robin@gmail.com>
2022-04-08 20:56:15 +0530

    fixing bug in the shop.php

--- shop.php
+++ shop.php
@@ -62,8 +62,8 @@ include("./conn.php");
                 <form class="shop__filter">
                     <div class="list-group">
                         <h3>Price</h3>
-                        <input type="hidden" id="hidden_minimum_price" value="0" />
-                        <input type="hidden" id="hidden_maximum_price" value="65000" />
+                        <input type="range" id="hidden_minimum_price" value="0" />
+                        <input type="range" id="hidden_maximum_price" value="65000" />
                         <p id="price_show">1000 - 65000</p>
                         <div id="slider"></div>
                         <input id="rbSlider" type="range" class="span2" value="" data-slider-min="0" data-slider-max="1000" data-slider-step="5" data-slider-value="[0,1000]" />
