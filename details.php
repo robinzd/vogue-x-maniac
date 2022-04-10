723807aee1b81b99686852bd86590689edd10f71
robin <ibots.robin@gmail.com>
2022-04-11 02:43:35 +0530

    fixing some bug in the details.php

--- details.php
+++ details.php
@@ -89,7 +89,8 @@ include("./conn.php");
                                         <h6 class="text-uppercase">quantity</h6>
                                         <input type="button" value="-" class="minus"><input type="number" step="1" min="1" max="" name="quantity" value="1" title="Qty" class="input-text qty text" size="4" pattern="" inputmode=""><input type="button" value="+" class="plus">
                                     </div>
-
+                                  
+                                <h6 class='text-uppercase'>Size</h6>;
                                   
                                     <?php
 
@@ -104,7 +105,7 @@ include("./conn.php");
 
 
                                     if (!($sizeOfcheck == 1)) {
-                                        // echo "<h6 class='text-uppercase'>Size</h6>";
+                                      
                                         $x = 0;
                                         while ($x < $sizeOfcheck) {
                                             echo "<label class='radio'> <input type='radio' name='size' value='$check_array_result[$x]' checked> <span>$check_array_result[$x]</span>";
