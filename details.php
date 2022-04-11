84421786b3daf8a33e928719676d54d79437cb62
robin <ibots.robin@gmail.com>
2022-04-12 02:43:05 +0530

    adding database into the details page

--- details.php
+++ details.php
@@ -109,7 +109,11 @@ include("./conn.php");
 
                             print_r($row_products_images);
 
-                         
+                            $details_image = $row_products_images['details_image'];
+
+                            echo  $details_image;
+    
+                            $related_product = $row_products_images['related_product'];
 
 
                             echo "<div class='thumbnail text-center' id='thumbnailimage'>
