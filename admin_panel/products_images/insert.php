b3073b5c71f340b7994fb75c7d8bbafc85f29554
robin <ibots.robin@gmail.com>
2022-04-16 14:18:00 +0530

    adding one new card to the admin panel.php

--- admin_panel/products_images/insert.php
+++ admin_panel/products_images/insert.php
@@ -20,12 +20,25 @@ if (isset($_POST['submit'])) {
 		// Code for move image into directory
 		move_uploaded_file($_FILES["details_image"]["tmp_name"], "images/" . $imgnewfile);
 		// Query for data insertion
-		$get_product_slider = "select ID from products_details";
+		$get_product = "select ID from products_details";
 
-		$run_product_image = mysqli_query($conn, $get_product_slider);
+		$run_product= mysqli_query($conn, $get_product);
+
+		while ($row_product = mysqli_fetch_array($run_product)) {
+            
+			$product_id=$row_product['ID'];
+                 
+		}
 
 		$query = mysqli_query($conn, "insert into products_images(details_image,related_product,primary_image) value ('$imgnewfile','$relatedproduct','$primaryimage')");
-		if ($query == $run_product_image) {
+		
+		while ($row = mysqli_fetch_array($query)) {
+            
+			$id=$row_product['related_product'];
+                 
+		}
+
+		if ($product_id == $id) {
 			echo "<script>alert('You have successfully inserted the product image');</script>";
 			echo "<script type='text/javascript'> document.location ='productsimages.php'; </script>";
 		} else {
