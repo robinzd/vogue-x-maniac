c7c84726f62db5469a137d5b0e766bc52ae6224d
robin <ibots.robin@gmail.com>
2022-04-18 23:24:33 +0530

    fixing the bug

--- details.php
+++ details.php
@@ -471,12 +471,11 @@ include("./conn.php");
 
 
 
-
+   
 
 
     <!-- j query -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- owl carousel -->
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
