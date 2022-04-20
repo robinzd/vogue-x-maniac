7c512421427befe644bea9e1728f1309d5d74201
robin <ibots.robin@gmail.com>
2022-04-20 14:01:21 +0530

    fixing the bug

--- index.php
+++ index.php
@@ -189,7 +189,7 @@ include("./conn.php");
 
                 echo " <div class='card' style='background-color: $brand_color ;'>
              <div class='img'>
-                 <a href='productbrands.php?brand_name=$brand_name'><img src='./admin_panel/brands/brands_images/$image_source' alt=''></a>
+                 <a href='shop.php?brand_name=$brand_name'><img src='./admin_panel/brands/brands_images/$image_source' alt=''></a>
              </div>
             <div class='content'>
                 <div class='title'>
