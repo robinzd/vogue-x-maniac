27841d4e2dc4466bbedb0249f43af55273a2cf76
robin <ibots.robin@gmail.com>
2022-04-22 03:01:35 +0530

    creating add to cart in php

--- cart.php
+++ cart.php
@@ -6,14 +6,14 @@ include("./conn.php");
 
 <?php
 
- session_start();
+session_start();
 
 include("./conn.php");
 include("./function.php");
 
 $user_data = check_login($conn);
 
-$userid=$user_data['user_id'];
+$userid = $user_data['user_id'];
 
 ?>
 
@@ -48,19 +48,16 @@ if ($rowcount > 0) {
     }
 } else {
 
-$query = mysqli_query($conn,"insert into products_cart(product_id,user_id,product_quantity,product_size) value ('$productid','$userid','$productquantity','$productsize')");
-if ($query) {
-    echo "<script>alert('You have successfully inserted into the cart');</script>";
-    echo "<script type='text/javascript'> document.location ='details.php?id=$productid'; </script>";
-} else {
-    echo "<script>alert('Something Went Wrong. Please try again');</script>";
-}
+    $query = mysqli_query($conn, "insert into products_cart(product_id,user_id,product_quantity,product_size) value ('$productid','$userid','$productquantity','$productsize')");
+    if ($query) {
+        echo "<script>alert('You have successfully inserted into the cart');</script>";
+        echo "<script type='text/javascript'> document.location ='details.php?id=$productid'; </script>";
+    } else {
+        echo "<script>alert('Something Went Wrong. Please try again');</script>";
+    }
 };
 
 ?>
-
-
-
 
 
 
@@ -100,159 +97,191 @@ if ($query) {
     <!--back to top ends -->
 
 
-
-    <!-- Cart ---->
     <div id="shopping-cart">
         <div class="txt-heading">Shopping Cart</div>
 
-        <a id="btnEmpty" href="index.php?action=empty">Empty Cart</a>
-        <?php
-        if (isset($_SESSION["cart_item"])) {
-            $total_quantity = 0;
-            $total_price = 0;
-        ?>
-            <table class="tbl-cart" cellpadding="10" cellspacing="1">
-                <tbody>
-                    <tr>
-                        <th style="text-align:left;">Name</th>
-                        <th style="text-align:left;">Code</th>
-                        <th style="text-align:right;" width="5%">Quantity</th>
-                        <th style="text-align:right;" width="10%">Unit Price</th>
-                        <th style="text-align:right;" width="10%">Price</th>
-                        <th style="text-align:center;" width="5%">Remove</th>
-                    </tr>
-                    <?php
-                    foreach ($_SESSION["cart_item"] as $item) {
-                        $item_price = $item["quantity"] * $item["price"];
-                    ?>
-                        <tr>
-                            <td><img src="<?php echo $item["image"]; ?>" class="cart-item-image" /><?php echo $item["name"]; ?></td>
-                            <td><?php echo $item["code"]; ?></td>
-                            <td style="text-align:right;"><?php echo $item["quantity"]; ?></td>
-                            <td style="text-align:right;"><?php echo "$ " . $item["price"]; ?></td>
-                            <td style="text-align:right;"><?php echo "$ " . number_format($item_price, 2); ?></td>
-                            <td style="text-align:center;"><a href="index.php?action=remove&code=<?php echo $item["code"]; ?>" class="btnRemoveAction"><img src="icon-delete.png" alt="Remove Item" /></a></td>
-                        </tr>
-                    <?php
-                        $total_quantity += $item["quantity"];
-                        $total_price += ($item["price"] * $item["quantity"]);
+        <a id="btnEmpty" href="">Empty Cart</a>
+
+        <table class="tbl-cart" cellpadding="10" cellspacing="1">
+
+            <thead>
+                <tr>
+                    <th style="text-align:left;">S.no</th>
+                    <th style="text-align:left;">Product Name</th>
+                    <th style="text-align:left;">Product Quantity</th>
+                    <th style="text-align:right;">Product Size</th>
+                    <th style="text-align:right;" width="10%">Unit Price</th>
+                    <th style="text-align:right;" width="10%">Price</th>
+                    <th style="text-align:center;" width="5%">Remove</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+
+                $total_quantity = 0;
+                $total_price = 0;
+
+                $ret = mysqli_query($conn, "select * from products_cart where user_id='$userid'");
+                $cnt = 1;
+                $row = mysqli_num_rows($ret);
+                if ($row > 0) {
+                    while ($row = mysqli_fetch_array($ret)) {
+
+                        $product_id = $row['product_id'];
+
+                        $get_product = "select * from  products_details where ID='$product_id'";
+
+                        $run_product = mysqli_query($conn, $get_product);
+
+                        while ($row_product = mysqli_fetch_array($run_product)){
+
+                            $product_price = $row_product['product_price'];
+                            $product_title =  $row_product['product_title'];
+
+                            $unit_price = $row["product_quantity"] * $product_price;
+
+                            $get_image = "select details_image from  products_images where relative_product='$product_id' and primary_image=1";
+
+                            $run_image = mysqli_query($conn, $get_image);
+
+                            while ($row_image = mysqli_fetch_array($run_image)) {
+
+                                $product_image = $row_image['details_image'];
+                          ?>
+                                <tr>
+                                    <td><?php echo $cnt; ?></td>
+                                    <td><img src="<?php echo $product_image;?>" class="cart-item-image" /><?php echo $product_title ?></td>
+                                    <td style="text-align:right;"><?php echo $row["product_quantity"]; ?></td>
+                                    <td style="text-align:right;"><?php echo $row["product_size"]; ?></td>
+                                    <td style="text-align:right;"><?php echo $product_price; ?></td>
+                                    <td style="text-align:right;"><?php echo "₹" . number_format($unit_price , 2); ?></td>
+                                    <td style="text-align:center;"><a href="index.php?action=remove&code=<?php echo $item["code"]; ?>" class="btnRemoveAction"><img src="icon-delete.png" alt="Remove Item" /></a></td>
+                                </tr>
+                            <?php
+                                $total_quantity += $row["product_quantity"];
+                                $total_price += ($product_price * $row["product_quantity"]);
+                                $cnt = $cnt + 1;
+                            }
+
+                            ?>
+                            <tr>
+                                <td colspan="2" align="right">Total:</td>
+                                <td align="right"><?php echo $total_quantity; ?></td>
+                                <td align="right" colspan="2"><strong><?php echo "₹" . number_format($total_price, 2); ?></strong></td>
+                                <td></td>
+                            </tr>
+            </tbody>
+        </table>
+<?php
+                        }
                     }
-                    ?>
-
-                    <tr>
-                        <td colspan="2" align="right">Total:</td>
-                        <td align="right"><?php echo $total_quantity; ?></td>
-                        <td align="right" colspan="2"><strong><?php echo "$ " . number_format($total_price, 2); ?></strong></td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
-        <?php
-        } else {
-        ?>
-            <div class="no-records">Your Cart is Empty</div>
-        <?php
-        }
-        ?>
-    </div>
+                } 
+                else {
+?>
+<div class="no-records">Your Cart is Empty</div>
+<?php
+                }
+?>
 
 
-    <!-- Footer -->
-
-    <footer class="text-center text-lg-start text-dark" style="background-color:lightgrey">
-        <!-- Grid container -->
-        <div class="container p-4 pb-0">
-            <!-- Section: Links -->
-            <section class="">
-                <!--Grid row-->
-                <div class="row">
-                    <!-- Grid column -->
-                    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                        <img class="footerimage" src="./header images/vogue x maniac png.png" alt="brand image">
-                        <p class="text-white">
-                            Here you can use rows and columns to organize your footer
-                            content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit.
-                        </p>
-                    </div>
-                    <!-- Grid column -->
-
-                    <hr class="w-100 clearfix d-md-none" />
-
-                    <!-- Grid column -->
-                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                        <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
-                        <p class="text-white">MDBootstrap</p>
-                        <p class="text-white">MDWordPress</p>
-                        <p class="text-white">BrandFlow</p>
-                        <p class="text-white">Angular</p>
-                    </div>
-                    <!-- Grid column -->
-
-                    <hr class="w-100 clearfix d-md-none" />
-
-                    <!-- Grid column -->
-                    <hr class="w-100 clearfix d-md-none" />
-
-                    <!-- Grid column -->
-                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-                        <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
-                        <p class="text-white"><i class="fas fa-home text-white mr-3"></i> New York, NY 10012, US</p>
-                        <p class="text-white" id="address"><i class="fas fa-envelope text-white mr-3"></i> inf0@text-white@gmail.com
-                        </p>
-                        <p class="text-white"><i class="fas fa-phone text-white mr-3"></i> + 01 234 567 88</p>
-                        <p class="text-white"><i class="fas fa-print text-white mr-3"></i> + 01 234 567 89</p>
-                    </div>
-                    <!-- Grid column -->
-
-                    <!-- Grid column -->
-                    <div class="col-md-3 col-lg-4 col-xl-4 mx-auto mt-3">
-                        <h6 class="text-uppercase mb-4 font-weight-bold">Follow us</h6>
-
-                        <!-- Facebook -->
-                        <a class="btn pmd-btn-fab pmd-ripple-effect btn-primary pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Facebook" role="button"><i class="fab fa-facebook"></i></a>
-
-                        <!-- Twitter -->
-                        <a class="btn pmd-btn-fab pmd-ripple-effect btn-info pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Twitter" role="button"><i class="fab fa-twitter"></i></a>
-
-                        <!-- youtube -->
-                        <a class="btn pmd-btn-fab pmd-ripple-effect btn-secondary pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="youtube" role="button"><i class="fab fa-youtube"></i></a>
 
 
-                        <!-- Instagram -->
-                        <a class="btn pmd-btn-fab pmd-ripple-effect btn-danger pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Instagram" role="button"><i class="fab fa-instagram"></i></a>
+<!-- Footer -->
 
-                    </div>
+<footer class="text-center text-lg-start text-dark" style="background-color:lightgrey">
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+        <!-- Section: Links -->
+        <section class="">
+            <!--Grid row-->
+            <div class="row">
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                    <img class="footerimage" src="./header images/vogue x maniac png.png" alt="brand image">
+                    <p class="text-white">
+                        Here you can use rows and columns to organize your footer
+                        content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit.
+                    </p>
                 </div>
-                <!--Grid row-->
-            </section>
-            <!-- Section: Links -->
-        </div>
-        <!-- Grid container -->
+                <!-- Grid column -->
 
-        <!-- Copyright -->
-        <div class="text-center p-3" style="background-color: rgba(255, 255, 255, 0.096)">
+                <hr class="w-100 clearfix d-md-none" />
 
-            <p class="text-dark"> ©2022 Vogue X Maniac.All Rights Reserved
-            <p>
-                <img class="px-2" src="./payment_pics/2560px-MasterCard_Logo.svg-removebg-preview.png">
-                <img class="px-2" src="./payment_pics/1200px-Visa.svg-removebg-preview.png">
-                <img class="px-2" src="./payment_pics/paypal-logo-removebg-preview.png">
-        </div>
-        <!-- Copyright -->
-    </footer>
-    <!-- end of the footer -->
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                    <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+                    <p class="text-white">MDBootstrap</p>
+                    <p class="text-white">MDWordPress</p>
+                    <p class="text-white">BrandFlow</p>
+                    <p class="text-white">Angular</p>
+                </div>
+                <!-- Grid column -->
+
+                <hr class="w-100 clearfix d-md-none" />
+
+                <!-- Grid column -->
+                <hr class="w-100 clearfix d-md-none" />
+
+                <!-- Grid column -->
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                    <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+                    <p class="text-white"><i class="fas fa-home text-white mr-3"></i> New York, NY 10012, US</p>
+                    <p class="text-white" id="address"><i class="fas fa-envelope text-white mr-3"></i> inf0@text-white@gmail.com
+                    </p>
+                    <p class="text-white"><i class="fas fa-phone text-white mr-3"></i> + 01 234 567 88</p>
+                    <p class="text-white"><i class="fas fa-print text-white mr-3"></i> + 01 234 567 89</p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-4 col-xl-4 mx-auto mt-3">
+                    <h6 class="text-uppercase mb-4 font-weight-bold">Follow us</h6>
+
+                    <!-- Facebook -->
+                    <a class="btn pmd-btn-fab pmd-ripple-effect btn-primary pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Facebook" role="button"><i class="fab fa-facebook"></i></a>
+
+                    <!-- Twitter -->
+                    <a class="btn pmd-btn-fab pmd-ripple-effect btn-info pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Twitter" role="button"><i class="fab fa-twitter"></i></a>
+
+                    <!-- youtube -->
+                    <a class="btn pmd-btn-fab pmd-ripple-effect btn-secondary pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="youtube" role="button"><i class="fab fa-youtube"></i></a>
+
+
+                    <!-- Instagram -->
+                    <a class="btn pmd-btn-fab pmd-ripple-effect btn-danger pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Instagram" role="button"><i class="fab fa-instagram"></i></a>
+
+                </div>
+            </div>
+            <!--Grid row-->
+        </section>
+        <!-- Section: Links -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(255, 255, 255, 0.096)">
+
+        <p class="text-dark"> ©2022 Vogue X Maniac.All Rights Reserved
+        <p>
+            <img class="px-2" src="./payment_pics/2560px-MasterCard_Logo.svg-removebg-preview.png">
+            <img class="px-2" src="./payment_pics/1200px-Visa.svg-removebg-preview.png">
+            <img class="px-2" src="./payment_pics/paypal-logo-removebg-preview.png">
+    </div>
+    <!-- Copyright -->
+</footer>
+<!-- end of the footer -->
 
 
 
 
 
 
-    <!-- j query -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- owl carousel -->
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+<!-- j query -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- owl carousel -->
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
 </body>
 
 </html>
