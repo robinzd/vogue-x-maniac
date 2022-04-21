a45e590d3b460ca29b23d8777b827f483510e7c2
robin <ibots.robin@gmail.com>
2022-04-22 00:22:35 +0530

    creating add to cart in php

--- cart.php
+++ cart.php
@@ -26,10 +26,10 @@ $productquantity = isset($_POST['quantity']) ? $_POST['quantity'] : "";
 $productsize = isset($_POST['size']) ? $_POST['size'] : "";
 
 
-$query = mysqli_query($conn, "insert into products_cart(product_id,user_id,product_quantity,product_size) value ('$productid','$userid','$productquantity','$productsize')");
+$query = mysqli_query($conn,"insert into products_cart(product_id,user_id,product_quantity,product_size) value ('$productid','$userid','$productquantity','$productsize')");
 if ($query) {
     echo "<script>alert('You have successfully inserted into the cart');</script>";
-    echo "<script type='text/javascript'> document.location ='cart.php'; </script>";
+    echo "<script type='text/javascript'> document.location ='details.php'; </script>";
 } else {
     echo "<script>alert('Something Went Wrong. Please try again');</script>";
 }
