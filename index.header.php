6c0487d442a84d817de5a22b898625f909b0ba8b
robin <ibots.robin@gmail.com>
2022-04-22 07:53:46 +0530

    creating add to cart in php

--- index.header.php
+++ index.header.php
@@ -58,7 +58,7 @@ include("./conn.php");
 					<div class="social-media">
 						<p class="mb-0 d-flex">
 							<a href="./login_form/login.php" class="d-flex align-items-center justify-content-center"><span class="fa fa-user"><i class="sr-only">Facebook</i></span></a>
-							<a href="product_cart.php" class="d-flex align-items-center justify-content-center"><span class="fa fa-cart-shopping"><i class="sr-only">Twitter</i></span></a>
+							<a href="product_cart.php"  class="d-flex align-items-center justify-content-center"><span class="fa fa-cart-shopping"><i data-count="3" class="sr-only">Twitter</i></span></a>
 							<a href="./logout.php" class="d-flex align-items-center justify-content-center"><span class="fa fa-sign-out"><i class="sr-only">Instagram</i></span></a>
 							<a href="./admin_panel/admin_panel.php" class="d-flex align-items-center justify-content-center"><span class="fa fa-dribbble"><i class="sr-only">Dribbble</i></span></a>
 						</p>
