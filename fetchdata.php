2f2eb11497442c9258b6dec877fa7c08d70b4308
robin <ibots.robin@gmail.com>
2022-04-22 23:46:24 +0530

    some changes in the pages

--- fetchdata.php
+++ fetchdata.php
@@ -18,7 +18,7 @@ if (isset($_POST["action"])) {
     $query = "
   SELECT * FROM products_details WHERE product_status = '1'
  ";
-  
+
 
     if (isset($_POST["minimum_price"], $_POST["maximum_price"]) && !empty($_POST["minimum_price"]) && !empty($_POST["maximum_price"])) {
         $query .= "
@@ -46,28 +46,24 @@ if (isset($_POST["action"])) {
     $output = '';
     if ($total_row > 0) {
         foreach ($result as $row) {
-            
-            $product_id=$row['ID'];
-            
-            $get_products_image ="select details_image from products_images where primary_image=1 and related_product=$product_id";
 
-            $run_products_image = mysqli_query($conn,$get_products_image);
-            $details_image=null;
-            while ($row_products_image = mysqli_fetch_array( $run_products_image )){
-                $details_image=$row_products_image['details_image'];
-            } 
+            $product_id = $row['ID'];
+
+            $get_products_image = "select details_image from products_images where primary_image=1 and related_product=$product_id";
+
+            $run_products_image = mysqli_query($conn, $get_products_image);
+            $details_image = null;
+            while ($row_products_image = mysqli_fetch_array($run_products_image)) {
+                $details_image = $row_products_image['details_image'];
+            }
             $output .= '
     <div class="col-sm-6 col-md-4">
                             <div class="card bg-white">
-        <img class="card-img-top" src="./admin_panel/products_images/images/' . $details_image . '" style="width:100%">
+                            <a href="details.php?id=' . $row['ID'] . '"><img class="card-img-top" src="./admin_panel/products_images/images/' . $details_image . '" style="width:100%">
         <div class="card-body">
             <h5 class="card-title text-center">' . $row['product_title'] . '</h5>
         <p class="card-text  text-center"><s>₹' . $row['product_strikeout_price'] . '</s>₹' . $row['product_price'] . '</p>
-            <div class="text-center">
-                <a href="details.php?id='. $row['ID'] .'" class="btn btn-success">See Details</a>
-                <a href="#" class="btn btn-success">Add to Cart</a>
-            </div>
-        </div>
+        </div></a>
     </div>
     </div>
    ';
