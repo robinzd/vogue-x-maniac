bf93134218a9b14bd3b1fe0eade101ee8232b6a9
robin <ibots.robin@gmail.com>
2022-04-23 18:00:39 +0530

    fixing some bugs

--- Login_form/login.php
+++ Login_form/login.php
@@ -3,123 +3,115 @@
 session_start();
 
 
-  include("../conn.php");
-  include("../function.php");
- 
-      if($_SERVER['REQUEST_METHOD'] == "POST")
-       {
-        // something was posted
-        $email =$_POST["email"];
-        $password =$_POST["password"];
+include("../conn.php");
+include("../function.php");
 
-		
-       
-        if( !empty($email) && !empty($password) )
-        {
-            // read from database
-           
-            $query = "select * from users where user_email='$email' limit 1";
-
-		
-
-            $result=mysqli_query($conn, $query);
-
-			
-
-            if($result)
-            {    
-                if($result && mysqli_num_rows($result) > 0)
-                {   
-					
-                    $user_data = mysqli_fetch_assoc($result);
-                    if($user_data['user_password'] === $password)
-                    {  
-						
-                        $_SESSION['user_id'] = $user_data['user_id'];
-					
-                        header("location:../index.php");
-					    die;
-                    
-                    }
-                }
-            }
-            echo "<script>alert('Wrong Email or Password');</script>";
-          
-        }else
-        {
-            echo  "<script>alert('Please Enter Some Valid Information!');</script>";
-        }
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+	// something was posted
+	$email = $_POST["email"];
+	$password = $_POST["password"];
 
 
-       }
+
+	if (!empty($email) && !empty($password)) {
+		// read from database
+
+		$query = "select * from users where user_email='$email' limit 1";
 
 
-  
+
+		$result = mysqli_query($conn, $query);
 
 
-  ?>
+
+		if ($result) {
+			if ($result && mysqli_num_rows($result) > 0) {
+
+				$user_data = mysqli_fetch_assoc($result);
+				if ($user_data['user_password'] === $password) {
+
+					$_SESSION['user_id'] = $user_data['user_id'];
+
+					header("location:../index.php");
+					die;
+				}
+			}
+		}
+		echo "<script>alert('Wrong Email or Password');</script>";
+	} else {
+		echo  "<script>alert('Please Enter Some Valid Information!');</script>";
+	}
+}
+
+
+
+
+
+?>
 
 
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<title>Login</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="../favicon/vogue_x_maniac_png_K8m_icon.ico"/>
+	<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="../favicon/vogue_x_maniac_png_K8m_icon.ico" />
 
 	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->	
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="css/login1.css">
 	<link rel="stylesheet" type="text/css" href="css/login.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 </head>
+
 <body style="background-color: #666666;">
 
 
 
-	
+
 	<div class="limiter">
 		<div class="container-login100">
-			<div class="wrap-login100 d-block d-sm-none" style="margin-top:-60px;">
-				<form method="post" class="login100-form validate-form" >
+			<div class="wrap-login100 d-none d-sm-block" style="margin-top:-140px;">
+				<form method="post" class="login100-form validate-form">
 
-				<span class="login100-form-title p-b-43">
+					<span class="login100-form-title p-b-43">
 						<img src="../header images/vogue x maniac png.png">
-						
+
 					</span>
 
 
 					<span class="login100-form-title p-b-43">
 						Login Form
 					</span>
-					
-					
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+
+
+					<div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
 						<input class="input100" type="email" name="email">
 						<span class="focus-input100"></span>
 						<span class="label-input100">Email</span>
 					</div>
-					
-					
+
+
 					<div class="wrap-input100 validate-input" data-validate="Password is required">
 						<input class="input100" type="password" name="password">
 						<span class="focus-input100"></span>
@@ -128,12 +120,12 @@ session_start();
 
 					<div class="flex-sb-m w-full p-t-3 p-b-32">
 						<div class="contact100">
-							
+
 							<div>
-							<a href="../register_form/register.php" class="txt1">
-							Register Here! 
-							</a>
-                             </div>
+								<a href="../register_form/register.php" class="txt1">
+									Register Here!
+								</a>
+							</div>
 						</div>
 
 						<div>
@@ -142,22 +134,100 @@ session_start();
 							</a>
 						</div>
 					</div>
-			
+
 
 					<div class="container-login100-form-btn">
-						<button class="login100-form-btn"  type="submit" value="login">
+						<button class="login100-form-btn" type="submit" value="login">
 							Login
 						</button>
 					</div>
 
-					 <div class="text-center p-t-46 p-b-20">
+					<div class="text-center p-t-46 p-b-20">
 						<span class="txt2">
 							Don't Have an account?<a href="../register_form/register.php" class="txt1">Register</a>
 						</span>
 					</div>
 
-					
-					 <div class="text-center p-t-46 p-b-20">
+
+					<div class="text-center p-t-46 p-b-20">
+						<span class="txt2">
+							or sign up using
+						</span>
+					</div>
+
+					<!-- <div class="login100-form-social flex-c-m">
+						<a href="#" class="login100-form-social-item flex-c-m bg1 m-r-5">
+							<i class="fa fa-facebook-f" aria-hidden="true"></i>
+						</a>
+
+						<a href="#" class="login100-form-social-item flex-c-m bg2 m-r-5">
+							<i class="fa fa-google" aria-hidden="true"></i>
+						</a>
+					</div> -->
+				</form>
+
+				<div class="login100-more" style="background-image: url('images/login-pic.jpg');">
+				</div>
+			</div>
+			<div class="wrap-login100 d-block d-sm-none" style="margin-top:-60px;">
+				<form method="post" class="login100-form validate-form">
+
+					<span class="login100-form-title p-b-43">
+						<img src="../header images/vogue x maniac png.png">
+
+					</span>
+
+
+					<span class="login100-form-title p-b-43">
+						Login Form
+					</span>
+
+
+					<div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+						<input class="input100" type="email" name="email">
+						<span class="focus-input100"></span>
+						<span class="label-input100">Email</span>
+					</div>
+
+
+					<div class="wrap-input100 validate-input" data-validate="Password is required">
+						<input class="input100" type="password" name="password">
+						<span class="focus-input100"></span>
+						<span class="label-input100">Password</span>
+					</div>
+
+					<div class="flex-sb-m w-full p-t-3 p-b-32">
+						<div class="contact100">
+
+							<div>
+								<a href="../register_form/register.php" class="txt1">
+									Register Here!
+								</a>
+							</div>
+						</div>
+
+						<div>
+							<a href="#" class="txt1">
+								Forgot Password?
+							</a>
+						</div>
+					</div>
+
+
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn" type="submit" value="login">
+							Login
+						</button>
+					</div>
+
+					<div class="text-center p-t-46 p-b-20">
+						<span class="txt2">
+							Don't Have an account?<a href="../register_form/register.php" class="txt1">Register</a>
+						</span>
+					</div>
+
+
+					<div class="text-center p-t-46 p-b-20">
 						<span class="txt2">
 							or sign up using
 						</span>
@@ -181,27 +251,28 @@ session_start();
 	</div>
 
 
-	
-	
 
-	
-	
-<!--===============================================================================================-->
+
+
+
+
+	<!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/bootstrap/js/popper.js"></script>
 	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/daterangepicker/moment.min.js"></script>
 	<script src="vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="js/main.js"></script>
 
 </body>
+
 </html>
