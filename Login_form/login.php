1695737dafdf978cf48d1b771516053d3fb7abc9
robin <ibots.robin@gmail.com>
2022-04-23 18:42:11 +0530

    fixing some bugs

--- Login_form/login.php
+++ Login_form/login.php
@@ -100,7 +100,7 @@ session_start();
 	<div class="limiter">
 		<div class="container-login100">
 			<div class="wrap-login100" style="margin-top:-60px;">
-				<form method="post" class="login100-form validate-form" style="padding-bottom:0px;">
+				<form method="post" class="login100-form validate-form">
 
 				<span class="login100-form-title p-b-43">
 						<img src="../header images/vogue x maniac png.png">
