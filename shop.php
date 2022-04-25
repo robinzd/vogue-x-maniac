bc74a3c3c2c0b8460c80b529f48d44e4cb3822da
robin <ibots.robin@gmail.com>
2022-04-26 01:51:51 +0530

    activating the search bar

--- shop.php
+++ shop.php
@@ -126,10 +126,10 @@ $searchingquery = "SELECT * FROM products_details WHERE product_title LIKE '%$se
             <div class="col-sm-8 col-md-9">
                 <div class="dropdown">
                     <select class="form-select" aria-label="Default select example">
-                        <option selected>Sort by</option>
+                        <option selected>Sort by:featured</option>
                         <option value="1">Price:low to high</option>
                         <option value="2">Price:high to low</option>
-                        <option value="3">Featured:Latest Product</option>
+                        <option value="3">Latest Arraivals</option>
                     </select>
                 </div>
                 <!-- Filters -->
