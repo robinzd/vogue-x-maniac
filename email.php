98cb7b330b5f03dafb8c27f650160377abe02cd2
robin <ibots.robin@gmail.com>
2022-04-28 01:35:44 +0530

    adding email features

--- email.php
+++ email.php
@@ -32,7 +32,7 @@ try {
   // Mail Headers
   $phpmailer->SetFrom("ibots.robin@gmail.com", "Robinson Rajiv");
   // Change to recipient email. Make sure to use a real email address in your tests to avoid hard bounces and protect your reputation as a sender.
-  $phpmailer->AddAddress("rickmathews@gmail.com", "Robinson Rajiv");
+  $phpmailer->AddAddress("rickmathews2@gmail.com", "Robin");
   
   // Message
   $phpmailer->IsHTML(true);
