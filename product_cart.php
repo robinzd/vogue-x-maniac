f07222a410a98fa87780dba32dd94319e7aad1d3
robin <ibots.robin@gmail.com>
2022-05-12 17:35:58 +0530

    fixing bug

--- product_cart.php
+++ product_cart.php
@@ -13,7 +13,7 @@ $userid = $user_data['user_id'];
 
 if (isset($_GET['delid'])) {
     $rid = intval($_GET['delid']);
-    $sql = mysqli_query($conn, "delete from products_cart where ID='$rid'");
+    $sql = mysqli_query($conn, "delete * from products_cart where user_id='$rid'");
     echo "<script>alert('you have successfully deleted the item in the cart');</script>";
     echo "<script>window.location.href = 'product_cart.php'</script>";
 }
@@ -237,7 +237,7 @@ if (isset($_GET['delid'])) {
                                         <td style="text-align:center;"><?php echo $row["product_size"]; ?></td>
                                         <td style="text-align:center;"><?php echo $product_price; ?></td>
                                         <td style="text-align:center;"><?php echo "₹" . number_format($unit_price, 2); ?></td>
-                                        <td style="text-align:center;"><a href="product_cart.php?delid=<?php echo ($row['ID']); ?>" class="btnRemoveAction"><img src="icon-delete.png" alt="Remove Item" /></a></td>
+                                        <td style="text-align:center;"><a href="product_cart.php?delid=<?php echo ($row['user_id']); ?>" class="btnRemoveAction"><img src="icon-delete.png" alt="Remove Item" /></a></td>
                                     </tr>
                         <?php
                                     $total_quantity += $row["product_quantity"];
