7d1cd289edb466b0ad8ae80f945436ce434c1b58
robin <ibots.robin@gmail.com>
2022-05-13 16:52:58 +0530

    adding payment.php

--- payment.php
+++ payment.php
@@ -13,6 +13,8 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 	<!-- fav icon -->
 	<link rel="icon" type="image/png" href="./favicon/vogue_x_maniac_png_K8m_icon.ico" />
+	 <!-- owl carousel -->
+	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
 	<!-- Jquery -->
 	<link rel="stylesheet" href="https://jqueryvalidation.org/files/demo/site-demos.css">
 	<!-- font awesome cdn -->
