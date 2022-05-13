4e721f59f22a829ba31ed072569f2d4ad247df0f
robin <ibots.robin@gmail.com>
2022-05-13 16:50:53 +0530

    adding payment.php

--- payment.php
+++ payment.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<title>Payment Details</title>
@@ -8,36 +9,39 @@
 	<!-- Font-->
 	<link rel="stylesheet" type="text/css" href="css1/raleway-font.css">
 	<link rel="stylesheet" type="text/css" href="fonts1/material-design-iconic-font/css/material-design-iconic-font.min.css">
-	 <!-- fav icon -->
-	 <link rel="icon" type="image/png" href="./favicon/vogue_x_maniac_png_K8m_icon.ico" />
+	<!-- bootsstrap cdn -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+	<!-- fav icon -->
+	<link rel="icon" type="image/png" href="./favicon/vogue_x_maniac_png_K8m_icon.ico" />
 	<!-- Jquery -->
 	<link rel="stylesheet" href="https://jqueryvalidation.org/files/demo/site-demos.css">
-	 <!-- font awesome cdn -->
-	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<!-- font awesome cdn -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<!-- Main Style Css -->
-    <link rel="stylesheet" href="css1/style.css"/>
+	<link rel="stylesheet" href="css1/style.css" />
 </head>
+
 <body>
 	<!-- navbar starts -->
 
 
-    <?php include "./index.header.php"; ?>
+	<?php include "./index.header.php"; ?>
 
-    <!-- navbar  ends -->
+	<!-- navbar  ends -->
 
 	<div class="page-content" style="background-image: url('images/wizard-v1.jpg')">
 		<div class="wizard-v1-content">
 			<div class="wizard-form">
-		        <form class="form-register" id="form-register" action="#" method="post">
-		        	<div id="form-total">
-		        		<!-- SECTION 1 -->
-			            <h2>
-			            	<span class="step-icon"><i class="zmdi zmdi-account"></i></span>
-			            	<span class="step-number">Step 1</span>
-			            	<span class="step-text">Address Infomation</span>
-			            </h2>
-			            <section>
-			                <div class="inner">
+				<form class="form-register" id="form-register" action="#" method="post">
+					<div id="form-total">
+						<!-- SECTION 1 -->
+						<h2>
+							<span class="step-icon"><i class="zmdi zmdi-account"></i></span>
+							<span class="step-number">Step 1</span>
+							<span class="step-text">Address Infomation</span>
+						</h2>
+						<section>
+							<div class="inner">
 								<div class="form-row">
 									<div class="form-holder form-holder-2">
 										<label for="username">Username*</label>
@@ -53,7 +57,7 @@
 								<div class="form-row">
 									<div class="form-holder">
 										<label for="password">Password*</label>
-										<input type="password" placeholder="Password" class="form-control" id="password" name="password" required >
+										<input type="password" placeholder="Password" class="form-control" id="password" name="password" required>
 									</div>
 									<div class="form-holder">
 										<label for="confirm_password">Confirm Password*</label>
@@ -61,15 +65,15 @@
 									</div>
 								</div>
 							</div>
-			            </section>
+						</section>
 						<!-- SECTION 2 -->
-			            <h2>
-			            	<span class="step-icon"><i class="zmdi zmdi-card"></i></span>
-			            	<span class="step-number">Step 2</span>
-			            	<span class="step-text">Payment Infomation</span>
-			            </h2>
-			            <section>
-			                <div class="inner">
+						<h2>
+							<span class="step-icon"><i class="zmdi zmdi-card"></i></span>
+							<span class="step-number">Step 2</span>
+							<span class="step-text">Payment Infomation</span>
+						</h2>
+						<section>
+							<div class="inner">
 								<div class="form-row">
 									<div class="form-holder form-holder-2">
 										<label for="card-type">Card Type</label>
@@ -120,16 +124,16 @@
 									</div>
 								</div>
 							</div>
-			            </section>
-			            <!-- SECTION 3 -->
-			            <h2>
-			            	<span class="step-icon"><i class="zmdi zmdi-receipt"></i></span>
-			            	<span class="step-number">Step 3</span>
-			            	<span class="step-text">Confirm Your Details</span>
-			            </h2>
-			            <section>
-			                <div class="inner">
-			                	<h3>Comfirm Details</h3>
+						</section>
+						<!-- SECTION 3 -->
+						<h2>
+							<span class="step-icon"><i class="zmdi zmdi-receipt"></i></span>
+							<span class="step-number">Step 3</span>
+							<span class="step-text">Confirm Your Details</span>
+						</h2>
+						<section>
+							<div class="inner">
+								<h3>Comfirm Details</h3>
 								<div class="form-row table-responsive">
 									<table class="table">
 										<tbody>
@@ -165,108 +169,109 @@
 									</table>
 								</div>
 							</div>
-			            </section>
-		        	</div>
-		        </form>
+						</section>
+					</div>
+				</form>
 			</div>
 		</div>
 	</div>
 
 	<!-- Footer -->
 
-    <footer class="text-center text-lg-start text-dark" style="background-color:lightgrey">
-        <!-- Grid container -->
-        <div class="container p-4 pb-0">
-            <!-- Section: Links -->
-            <section class="">
-                <!--Grid row-->
-                <div class="row">
-                    <!-- Grid column -->
-                    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                        <img class="footerimage" src="./header images/vogue x maniac png.png" alt="brand image">
-                        <p class="text-white">
-                           vogue-x-maniac is the one of the leading ecommerce website
-                        </p>
-                    </div>
-                    <!-- Grid column -->
+	<footer class="text-center text-lg-start text-dark" style="background-color:lightgrey">
+		<!-- Grid container -->
+		<div class="container p-4 pb-0">
+			<!-- Section: Links -->
+			<section class="">
+				<!--Grid row-->
+				<div class="row">
+					<!-- Grid column -->
+					<div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+						<img class="footerimage" src="./header images/vogue x maniac png.png" alt="brand image">
+						<p class="text-white">
+							vogue-x-maniac is the one of the leading ecommerce website
+						</p>
+					</div>
+					<!-- Grid column -->
 
-                    <hr class="w-100 clearfix d-md-none" />
+					<hr class="w-100 clearfix d-md-none" />
 
-                    <!-- Grid column -->
-                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                        <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
-                        <p class="text-white">MDBootstrap</p>
-                        <p class="text-white">MDWordPress</p>
-                        <p class="text-white">BrandFlow</p>
-                        <p class="text-white">Angular</p>
-                    </div>
-                    <!-- Grid column -->
+					<!-- Grid column -->
+					<div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+						<h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+						<p class="text-white">MDBootstrap</p>
+						<p class="text-white">MDWordPress</p>
+						<p class="text-white">BrandFlow</p>
+						<p class="text-white">Angular</p>
+					</div>
+					<!-- Grid column -->
 
-                    <hr class="w-100 clearfix d-md-none" />
+					<hr class="w-100 clearfix d-md-none" />
 
-                    <!-- Grid column -->
-                    <hr class="w-100 clearfix d-md-none" />
+					<!-- Grid column -->
+					<hr class="w-100 clearfix d-md-none" />
 
-                    <!-- Grid column -->
-                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-                        <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
-                        <p class="text-white"><i class="fas fa-home text-white mr-3"></i> New York, NY 10012, US</p>
-                        <p class="text-white" id="address"><i class="fas fa-envelope text-white mr-3"></i> inf0@text-white@gmail.com
-                        </p>
-                        <p class="text-white"><i class="fas fa-phone text-white mr-3"></i> + 01 234 567 88</p>
-                        <p class="text-white"><i class="fas fa-print text-white mr-3"></i> + 01 234 567 89</p>
-                    </div>
-                    <!-- Grid column -->
+					<!-- Grid column -->
+					<div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+						<h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+						<p class="text-white"><i class="fas fa-home text-white mr-3"></i> New York, NY 10012, US</p>
+						<p class="text-white" id="address"><i class="fas fa-envelope text-white mr-3"></i> inf0@text-white@gmail.com
+						</p>
+						<p class="text-white"><i class="fas fa-phone text-white mr-3"></i> + 01 234 567 88</p>
+						<p class="text-white"><i class="fas fa-print text-white mr-3"></i> + 01 234 567 89</p>
+					</div>
+					<!-- Grid column -->
 
-                    <!-- Grid column -->
-                    <div class="col-md-3 col-lg-4 col-xl-4 mx-auto mt-3">
-                        <h6 class="text-uppercase mb-4 font-weight-bold">Follow us</h6>
+					<!-- Grid column -->
+					<div class="col-md-3 col-lg-4 col-xl-4 mx-auto mt-3">
+						<h6 class="text-uppercase mb-4 font-weight-bold">Follow us</h6>
 
-                        <!-- Facebook -->
-                        <a class="btn pmd-btn-fab pmd-ripple-effect btn-primary pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Facebook" role="button"><i class="fab fa-facebook"></i></a>
+						<!-- Facebook -->
+						<a class="btn pmd-btn-fab pmd-ripple-effect btn-primary pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Facebook" role="button"><i class="fab fa-facebook"></i></a>
 
-                        <!-- Twitter -->
-                        <a class="btn pmd-btn-fab pmd-ripple-effect btn-info pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Twitter" role="button"><i class="fab fa-twitter"></i></a>
+						<!-- Twitter -->
+						<a class="btn pmd-btn-fab pmd-ripple-effect btn-info pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Twitter" role="button"><i class="fab fa-twitter"></i></a>
 
-                        <!-- youtube -->
-                        <a class="btn pmd-btn-fab pmd-ripple-effect btn-secondary pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="youtube" role="button"><i class="fab fa-youtube"></i></a>
+						<!-- youtube -->
+						<a class="btn pmd-btn-fab pmd-ripple-effect btn-secondary pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="youtube" role="button"><i class="fab fa-youtube"></i></a>
 
 
-                        <!-- Instagram -->
-                        <a class="btn pmd-btn-fab pmd-ripple-effect btn-danger pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Instagram" role="button"><i class="fab fa-instagram"></i></a>
+						<!-- Instagram -->
+						<a class="btn pmd-btn-fab pmd-ripple-effect btn-danger pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Instagram" role="button"><i class="fab fa-instagram"></i></a>
 
-                    </div>
-                </div>
-                <!--Grid row-->
-            </section>
-            <!-- Section: Links -->
-        </div>
-        <!-- Grid container -->
+					</div>
+				</div>
+				<!--Grid row-->
+			</section>
+			<!-- Section: Links -->
+		</div>
+		<!-- Grid container -->
 
-        <!-- Copyright -->
-        <div class="text-center p-3" style="background-color: rgba(255, 255, 255, 0.096)">
+		<!-- Copyright -->
+		<div class="text-center p-3" style="background-color: rgba(255, 255, 255, 0.096)">
 
-            <p class="text-dark"> ©2022 Vogue X Maniac.All Rights Reserved
-            </p>
-            <img class="px-2" src="./payment_pics/2560px-MasterCard_Logo.svg-removebg-preview.png">
-            <img class="px-2" src="./payment_pics/1200px-Visa.svg-removebg-preview.png">
-            <img class="px-2" src="./payment_pics/paypal-logo-removebg-preview.png">
-        </div>
-        <!-- Copyright -->
-    </footer>
-    <!-- end of the footer -->
+			<p class="text-dark"> ©2022 Vogue X Maniac.All Rights Reserved
+			</p>
+			<img class="px-2" src="./payment_pics/2560px-MasterCard_Logo.svg-removebg-preview.png">
+			<img class="px-2" src="./payment_pics/1200px-Visa.svg-removebg-preview.png">
+			<img class="px-2" src="./payment_pics/paypal-logo-removebg-preview.png">
+		</div>
+		<!-- Copyright -->
+	</footer>
+	<!-- end of the footer -->
 
 
 	<script src="js1/jquery-3.3.1.min.js"></script>
 	<!--budle.js  -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 	<!-- bundle.js -->
-	 <!-- owl carousel -->
-	 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+	<!-- owl carousel -->
+	<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/additional-methods.min.js"></script>
 	<script src="js1/jquery.steps.js"></script>
 	<script src="js1/main.js"></script>
 </body>
+
 </html>
