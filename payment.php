244dd2dc1c544d0dfb9fd0fcca3d175b1ff8fad4
robin <ibots.robin@gmail.com>
2022-05-13 16:48:24 +0530

    adding payment.php

--- payment.php
+++ payment.php
@@ -261,6 +261,9 @@
 	<!--budle.js  -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 	<!-- bundle.js -->
+	 <!-- owl carousel -->
+	 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/additional-methods.min.js"></script>
 	<script src="js1/jquery.steps.js"></script>
