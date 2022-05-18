79d8b84cf2b8afa0691909a0f6012b4132f70a72
robin <ibots.robin@gmail.com>
2022-05-18 23:33:54 +0530

    adding address.php

--- address.php
+++ address.php
@@ -2,109 +2,196 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Address Details</title>
-    <!-- fav icon -->
-    <link rel="icon" type="image/png" href="./favicon/vogue_x_maniac_png_K8m_icon.ico" />
-    <!-- bootsstrap cdn -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- font awesome cdn -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- owl carousel -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
-    <!-- link the external stylesheet -->
-    <link rel="stylesheet" type="text/css" href="address.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Address</title>
+  <!-- fav icon -->
+  <link rel="icon" type="image/png" href="./favicon/vogue_x_maniac_png_K8m_icon.ico" />
+  <!-- bootsstrap cdn -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- font awesome cdn -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <!-- owl carousel -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+  <!-- link the external stylesheet -->
+  <link rel="stylesheet" type="text/css" href="address.css">
+  <!-- link the external stylesheet -->
+  <script src="./address.js"></script>
 </head>
 
 <body>
-<div class="row mt-3 mx-3" style="margin-top:25px ;">
-  <div class="col-md-3">
-    <div style="margin-top: 50px; margin-left: 10px;" class="text-center">
-      <i id="animationDemo" data-mdb-animation="slide-right" data-mdb-toggle="animation"
-        data-mdb-animation-reset="true" data-mdb-animation-start="onScroll"
-        data-mdb-animation-on-scroll="repeat" class="fa fa-3x fa-shipping-fast text-white"></i>
-      <h3 class="mt-3 text-white">Welcome</h3>
-      <p class="white-text">You are 30 seconds away from compleating your order!</p>
-    </div>
-    <div class="text-center">
-      <button type="submit" class="btn btn-white btn-rounded back-button">Go back</button>
-    </div>
+  <!-- navbar starts -->
 
 
-  </div>
-  <div class="col-md-9 justify-content-center">
-    <div class="card card-custom pb-4">
-      <div class="card-body mt-0 mx-5">
-        <div class="text-center mb-3 pb-2 mt-3">
-          <h4 style="color: #495057 ;">Delivery Details</h4>
+  <?php
+
+  include "./index.header.php";
+
+  ?>
+
+  <!-- navbar  ends -->
+
+  <div class="form-body">
+    <div class="row">
+      <div class="form-holder">
+        <div class="form-content">
+          <div class="form-items">
+            <h3>Register Today</h3>
+            <p>Fill in the data below.</p>
+            <form class="requires-validation" novalidate>
+
+              <div class="col-md-12">
+                <input class="form-control" type="text" name="name" placeholder="Full Name" required>
+                <div class="valid-feedback">Username field is valid!</div>
+                <div class="invalid-feedback">Username field cannot be blank!</div>
+              </div>
+
+              <div class="col-md-12">
+                <input class="form-control" type="email" name="email" placeholder="E-mail Address" required>
+                <div class="valid-feedback">Email field is valid!</div>
+                <div class="invalid-feedback">Email field cannot be blank!</div>
+              </div>
+
+              <div class="col-md-12">
+                <select class="form-select mt-3" required>
+                  <option selected disabled value="">Position</option>
+                  <option value="jweb">Junior Web Developer</option>
+                  <option value="sweb">Senior Web Developer</option>
+                  <option value="pmanager">Project Manager</option>
+                </select>
+                <div class="valid-feedback">You selected a position!</div>
+                <div class="invalid-feedback">Please select a position!</div>
+              </div>
+
+
+              <div class="col-md-12">
+                <input class="form-control" type="password" name="password" placeholder="Password" required>
+                <div class="valid-feedback">Password field is valid!</div>
+                <div class="invalid-feedback">Password field cannot be blank!</div>
+              </div>
+
+
+              <div class="col-md-12 mt-3">
+                <label class="mb-3 mr-1" for="gender">Gender: </label>
+
+                <input type="radio" class="btn-check" name="gender" id="male" autocomplete="off" required>
+                <label class="btn btn-sm btn-outline-secondary" for="male">Male</label>
+
+                <input type="radio" class="btn-check" name="gender" id="female" autocomplete="off" required>
+                <label class="btn btn-sm btn-outline-secondary" for="female">Female</label>
+
+                <input type="radio" class="btn-check" name="gender" id="secret" autocomplete="off" required>
+                <label class="btn btn-sm btn-outline-secondary" for="secret">Secret</label>
+                <div class="valid-feedback mv-up">You selected a gender!</div>
+                <div class="invalid-feedback mv-up">Please select a gender!</div>
+              </div>
+
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+                <label class="form-check-label">I confirm that all data are correct</label>
+                <div class="invalid-feedback">Please confirm that the entered data are all correct!</div>
+              </div>
+
+
+              <div class="form-button mt-3">
+                <button id="submit" type="submit" class="btn btn-primary">Register</button>
+              </div>
+            </form>
+          </div>
         </div>
-
-        <form class="mb-0">
-
-          <div class="row mb-4">
-            <div class="col">
-              <div class="form-outline">
-                <input type="text" id="form9Example1" class="form-control input-custom" />
-                <label class="form-label" for="form9Example1">First name</label>
-              </div>
-            </div>
-            <div class="col">
-              <div class="form-outline">
-                <input type="text" id="form9Example2" class="form-control input-custom" />
-                <label class="form-label" for="form9Example2">Last name</label>
-              </div>
-            </div>
-          </div>
-          <div class="row mb-4">
-            <div class="col">
-              <div class="form-outline">
-                <input type="text" id="form9Example3" class="form-control input-custom" />
-                <label class="form-label" for="form9Example3">City</label>
-              </div>
-            </div>
-            <div class="col">
-              <div class="form-outline">
-                <input type="text" id="form9Example4" class="form-control input-custom" />
-                <label class="form-label" for="form9Example4">Zip</label>
-              </div>
-            </div>
-          </div>
-          <div class="row mb-4">
-            <div class="col">
-              <div class="form-outline">
-                <input type="text" id="form9Example6" class="form-control input-custom" />
-                <label class="form-label" for="form9Example6">Address</label>
-              </div>
-            </div>
-            <div class="col">
-              <div class="form-outline">
-                <input type="email" id="typeEmail" class="form-control input-custom" />
-                <label class="form-label" for="typeEmail">Email</label>
-              </div>
-            </div>
-          </div>
-
-          <div class="float-end ">
-            <!-- Submit button -->
-            <button type="submit" class="btn btn-primary btn-rounded"
-              style="background-color: #0062CC ;">Place order</button>
-          </div>
-
-        </form>
       </div>
     </div>
   </div>
-</div>
+
+  <!-- Footer -->
+
+  <footer class="text-center text-lg-start text-dark" style="background-color:lightgrey">
+        <!-- Grid container -->
+        <div class="container p-4 pb-0">
+            <!-- Section: Links -->
+            <section class="">
+                <!--Grid row-->
+                <div class="row">
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                        <img class="footerimage" src="./header images/vogue x maniac png.png" alt="brand image">
+                        <p class="text-white">
+                           vogue-x-maniac is the one of the leading ecommerce website
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <hr class="w-100 clearfix d-md-none" />
+
+                    <!-- Grid column -->
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+                        <p class="text-white">MDBootstrap</p>
+                        <p class="text-white">MDWordPress</p>
+                        <p class="text-white">BrandFlow</p>
+                        <p class="text-white">Angular</p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <hr class="w-100 clearfix d-md-none" />
+
+                    <!-- Grid column -->
+                    <hr class="w-100 clearfix d-md-none" />
+
+                    <!-- Grid column -->
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+                        <p class="text-white"><i class="fas fa-home text-white mr-3"></i> New York, NY 10012, US</p>
+                        <p class="text-white" id="address"><i class="fas fa-envelope text-white mr-3"></i> inf0@text-white@gmail.com
+                        </p>
+                        <p class="text-white"><i class="fas fa-phone text-white mr-3"></i> + 01 234 567 88</p>
+                        <p class="text-white"><i class="fas fa-print text-white mr-3"></i> + 01 234 567 89</p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-4 col-xl-4 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold">Follow us</h6>
+
+                        <!-- Facebook -->
+                        <a class="btn pmd-btn-fab pmd-ripple-effect btn-primary pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Facebook" role="button"><i class="fab fa-facebook"></i></a>
+
+                        <!-- Twitter -->
+                        <a class="btn pmd-btn-fab pmd-ripple-effect btn-info pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Twitter" role="button"><i class="fab fa-twitter"></i></a>
+
+                        <!-- youtube -->
+                        <a class="btn pmd-btn-fab pmd-ripple-effect btn-secondary pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="youtube" role="button"><i class="fab fa-youtube"></i></a>
 
 
+                        <!-- Instagram -->
+                        <a class="btn pmd-btn-fab pmd-ripple-effect btn-danger pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Instagram" role="button"><i class="fab fa-instagram"></i></a>
 
-    <!-- j query -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- owl carousel -->
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+                    </div>
+                </div>
+                <!--Grid row-->
+            </section>
+            <!-- Section: Links -->
+        </div>
+        <!-- Grid container -->
+
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(255, 255, 255, 0.096)">
+
+            <p class="text-dark"> ©2022 Vogue X Maniac.All Rights Reserved
+            </p>
+            <img class="px-2" src="./payment_pics/2560px-MasterCard_Logo.svg-removebg-preview.png">
+            <img class="px-2" src="./payment_pics/1200px-Visa.svg-removebg-preview.png">
+            <img class="px-2" src="./payment_pics/paypal-logo-removebg-preview.png">
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <!-- end of the footer -->
+  <!-- j query -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- owl carousel -->
+  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
 </body>
 
 </html>
