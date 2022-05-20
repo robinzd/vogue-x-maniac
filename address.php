26f8e02e4547cd015a5af74b3146314a6b5dc846
robin <ibots.robin@gmail.com>
2022-05-20 23:47:44 +0530

    fixing bug in the address.php

--- address.php
+++ address.php
@@ -114,12 +114,12 @@
               <div class="col-md-12">
                 <input class="form-control" type="text" name="street" placeholder="Address" required>
                 <div class="invalid-feedback">Address field cannot be blank!</div>
-              </div><br>
+              </div>
 
               <div class="col-md-12">
                 <input class="form-control" type="text" name="landmark" placeholder="Landmark" required>
                 <div class="invalid-feedback">Landmark field cannot be blank!</div>
-              </div><br>
+              </div>
 
               <div class="col-md-12">
                 <input class="form-control" type="text" name="city" placeholder="City" required>
@@ -129,7 +129,7 @@
               <div class="col-md-12">
                 <input class="form-control" type="text" name="pincode" placeholder="Pincode" required>
                 <div class="invalid-feedback">Pincode field cannot be blank!</div>
-              </div><br>
+              </div>
 
 
               <div class="form-check">
