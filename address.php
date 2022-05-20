5e529d6c4fdc9eb8e25536ca300615ce6544b2d7
robin <ibots.robin@gmail.com>
2022-05-20 23:43:06 +0530

    fixing bug in the address.php

--- address.php
+++ address.php
@@ -80,7 +80,7 @@
 
 
               <div class="form-button mt-3">
-                <button id="submit" type="submit" class="btn btn-primary text-center">Submit</button>
+                <button id="submit" type="submit" class="btn btn-outline-success">Submit</button>
               </div>
             </form>
           </div>
@@ -140,7 +140,7 @@
 
 
               <div class="form-button mt-3">
-                <button id="submit" type="submit" class="btn btn-primary text-center">Submit</button>
+                <button id="submit" type="submit" class="btn btn-outline-success">Submit</button>
               </div>
             </form>
           </div>
