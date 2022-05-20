3b9abc359b8ef0e561dd0ebf16339f2b5360bfba
robin <ibots.robin@gmail.com>
2022-05-21 00:45:31 +0530

    fixing bug in the address.php

--- address.php
+++ address.php
@@ -38,7 +38,7 @@
           <div class="form-items">
             <h3>Address Details</h3>
             <p>Fill in the data below.</p>
-            <form class="requires-validation" novalidate method="Post">
+            <form class="requires-validation" novalidate>
 
               <div class="col col-md-12">
                 <input class="form-control" type="text" name="name" placeholder="Full Name" required>
