73fd40d0a085b7664e7574a2ebdebac0d5cafb22
robin <ibots.robin@gmail.com>
2022-05-27 00:59:26 +0530

    fixing bug in the address.php

--- bill.php
+++ bill.php
@@ -8,11 +8,26 @@ $user_data = check_login($conn);
 
 $userid = $user_data['user_id'];
 
+echo $userid;
+
+echo "<br>";
+
 $fullname = isset($_POST['name']) ? $_POST['name'] : "";
+
+echo $fullname;
+
+echo "<br>";
+
 
 $email = isset($_POST['email']) ? $_POST['email'] : "";
 
+echo $email;
+
+
+
 $street = isset($_POST['street']) ? $_POST['street'] : "";
+
+
 
 $landmark = isset($_POST['landmark']) ? $_POST['landmark'] : "";
 
