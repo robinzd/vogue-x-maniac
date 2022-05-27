15f97e68d27448e782cd211d439a1c325b50f0bf
robin <ibots.robin@gmail.com>
2022-05-28 00:21:09 +0530

    adding bill.php

--- bill.php
+++ bill.php
@@ -84,144 +84,269 @@ if (!empty($fullname) && !empty($email) && !empty($street) && !empty($landmark) 
 
 
                            
-<div class="receipt-content">
-    <div class="container bootstrap snippets bootdey">
-		<div class="row">
-			<div class="col-md-12">
-				<div class="invoice-wrapper">
-					<div class="intro">
-						Hi <strong>John McClane</strong>, 
-						<br>
-						This is the receipt for a payment of <strong>$312.00</strong> (USD) for your works
-					</div>
+<div class="container mt-5 mb-5">
 
-					<div class="payment-info">
-						<div class="row">
-							<div class="col-sm-6">
-								<span>Payment No.</span>
-								<strong>434334343</strong>
-							</div>
-							<div class="col-sm-6 text-right">
-								<span>Payment Date</span>
-								<strong>Jul 09, 2014 - 12:20 pm</strong>
-							</div>
-						</div>
-					</div>
+        <div class="row d-flex justify-content-center">
 
-					<div class="payment-details">
-						<div class="row">
-							<div class="col-sm-6">
-								<span>Client</span>
-								<strong>
-									Andres felipe posada
-								</strong>
-								<p>
-									989 5th Avenue <br>
-									City of monterrey <br>
-									55839 <br>
-									USA <br>
-									<a href="#">
-										jonnydeff@gmail.com
-									</a>
-								</p>
-							</div>
-							<div class="col-sm-6 text-right">
-								<span>Payment To</span>
-								<strong>
-									Juan fernando arias
-								</strong>
-								<p>
-									344 9th Avenue <br>
-									San Francisco <br>
-									99383 <br>
-									USA <br>
-									<a href="#">
-										juanfer@gmail.com
-									</a>
-								</p>
-							</div>
-						</div>
-					</div>
+            <div class="col-md-8">
 
-					<div class="line-items">
-						<div class="headers clearfix">
-							<div class="row">
-								<div class="col-xs-4">Description</div>
-								<div class="col-xs-3">Quantity</div>
-								<div class="col-xs-5 text-right">Amount</div>
-							</div>
-						</div>
-						<div class="items">
-							<div class="row item">
-								<div class="col-xs-4 desc">
-									Html theme
-								</div>
-								<div class="col-xs-3 qty">
-									3
-								</div>
-								<div class="col-xs-5 amount text-right">
-									$60.00
-								</div>
-							</div>
-							<div class="row item">
-								<div class="col-xs-4 desc">
-									Bootstrap snippet
-								</div>
-								<div class="col-xs-3 qty">
-									1
-								</div>
-								<div class="col-xs-5 amount text-right">
-									$20.00
-								</div>
-							</div>
-							<div class="row item">
-								<div class="col-xs-4 desc">
-									Snippets on bootdey 
-								</div>
-								<div class="col-xs-3 qty">
-									2
-								</div>
-								<div class="col-xs-5 amount text-right">
-									$18.00
-								</div>
-							</div>
-						</div>
-						<div class="total text-right">
-							<p class="extra-notes">
-								<strong>Extra Notes</strong>
-								Please send all items at the same time to shipping address by next week.
-								Thanks a lot.
-							</p>
-							<div class="field">
-								Subtotal <span>$379.00</span>
-							</div>
-							<div class="field">
-								Shipping <span>$0.00</span>
-							</div>
-							<div class="field">
-								Discount <span>4.5%</span>
-							</div>
-							<div class="field grand-total">
-								Total <span>$312.00</span>
-							</div>
-						</div>
+                <div class="card">
 
-						<div class="print">
-							<a href="#">
-								<i class="fa fa-print"></i>
-								Print this receipt
-							</a>
-						</div>
-					</div>
-				</div>
 
-				<div class="footer">
-					Copyright © 2014. company name
-				</div>
-			</div>
-		</div>
-	</div>
-</div>                    
+                        <div class="text-left logo p-2 px-5">
+
+                            <img src="https://i.imgur.com/2zDU056.png" width="50">
+                            
+
+                        </div>
+
+                        <div class="invoice p-5">
+
+                            <h5>Your order Confirmed!</h5>
+
+                            <span class="font-weight-bold d-block mt-4">Hello, Chris</span>
+                            <span>You order has been confirmed and will be shipped in next two days!</span>
+
+                            <div class="payment border-top mt-3 mb-3 border-bottom table-responsive">
+
+                                <table class="table table-borderless">
+                                    
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <div class="py-2">
+
+                                                    <span class="d-block text-muted">Order Date</span>
+                                                <span>12 Jan,2018</span>
+                                                    
+                                                </div>
+                                            </td>
+
+                                            <td>
+                                                <div class="py-2">
+
+                                                    <span class="d-block text-muted">Order No</span>
+                                                <span>MT12332345</span>
+                                                    
+                                                </div>
+                                            </td>
+
+                                            <td>
+                                                <div class="py-2">
+
+                                                    <span class="d-block text-muted">Payment</span>
+                                                <span><img src="https://img.icons8.com/color/48/000000/mastercard.png" width="20" /></span>
+                                                    
+                                                </div>
+                                            </td>
+
+                                            <td>
+                                                <div class="py-2">
+
+                                                    <span class="d-block text-muted">Shiping Address</span>
+                                                <span>414 Advert Avenue, NY,USA</span>
+                                                    
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+
+                                </table>
+
+
+
+
+                                
+                            </div>
+
+
+
+
+                                <div class="product border-bottom table-responsive">
+
+                                    <table class="table table-borderless">
+
+                                    <tbody>
+                                        <tr>
+                                            <td width="20%">
+                                            
+                                            <img src="https://i.imgur.com/u11K1qd.jpg" width="90">
+
+                                        </td>
+                                    
+                                        <td width="60%">
+                                            <span class="font-weight-bold">Men's Sports cap</span>
+                                            <div class="product-qty">
+                                                <span class="d-block">Quantity:1</span>
+                                                <span>Color:Dark</span>
+                                                
+                                            </div>
+                                        </td>
+                                        <td width="20%">
+                                            <div class="text-right">
+                                                <span class="font-weight-bold">$67.50</span>
+                                            </div>
+                                        </td>
+                                        </tr>
+
+
+                                        <tr>
+                                            <td width="20%">
+                                            
+                                            <img src="https://i.imgur.com/SmBOua9.jpg" width="70">
+
+                                        </td>
+                                    
+                                        <td width="60%">
+                                            <span class="font-weight-bold">Men's Collar T-shirt</span>
+                                            <div class="product-qty">
+                                                <span class="d-block">Quantity:1</span>
+                                                <span>Color:Orange</span>
+                                                
+                                            </div>
+                                        </td>
+                                        <td width="20%">
+                                            <div class="text-right">
+                                                <span class="font-weight-bold">$77.50</span>
+                                            </div>
+                                        </td>
+                                        </tr>
+                                    </tbody> 
+                                        
+                                    </table>
+                                    
+
+
+                                </div>
+
+
+
+                                <div class="row d-flex justify-content-end">
+
+                                    <div class="col-md-5">
+
+                                        <table class="table table-borderless">
+
+                                            <tbody class="totals">
+
+                                                <tr>
+                                                    <td>
+                                                        <div class="text-left">
+
+                                                            <span class="text-muted">Subtotal</span>
+                                                            
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="text-right">
+                                                            <span>$168.50</span>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+
+
+                                                 <tr>
+                                                    <td>
+                                                        <div class="text-left">
+
+                                                            <span class="text-muted">Shipping Fee</span>
+                                                            
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="text-right">
+                                                            <span>$22</span>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+
+
+                                                 <tr>
+                                                    <td>
+                                                        <div class="text-left">
+
+                                                            <span class="text-muted">Tax Fee</span>
+                                                            
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="text-right">
+                                                            <span>$7.65</span>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+
+
+                                                 <tr>
+                                                    <td>
+                                                        <div class="text-left">
+
+                                                            <span class="text-muted">Discount</span>
+                                                            
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="text-right">
+                                                            <span class="text-success">$168.50</span>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+
+
+                                                 <tr class="border-top border-bottom">
+                                                    <td>
+                                                        <div class="text-left">
+
+                                                            <span class="font-weight-bold">Subtotal</span>
+                                                            
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="text-right">
+                                                            <span class="font-weight-bold">$238.50</span>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                                
+                                            </tbody>
+                                            
+                                        </table>
+                                        
+                                    </div>
+                                    
+
+
+                                </div>
+
+
+                                <p>We will be sending shipping confirmation email when the item shipped successfully!</p>
+                                <p class="font-weight-bold mb-0">Thanks for shopping with us!</p>
+                                <span>Nike Team</span>
+
+
+
+                            
+
+                        </div>
+
+
+                        <div class="d-flex justify-content-between footer p-3">
+
+                            <span>Need Help? visit our <a href="#"> help center</a></span>
+                             <span>12 June, 2020</span>
+                            
+                        </div>
+
+
+
+            
+        </div>
+                
+            </div>
+            
+        </div>
+        
+    </div>    
 
 
 
