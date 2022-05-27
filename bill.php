f73d07bc27555b07785caa32cba4eee9e3510577
robin <ibots.robin@gmail.com>
2022-05-28 00:26:20 +0530

    adding bill.php

--- bill.php
+++ bill.php
@@ -93,12 +93,7 @@ if (!empty($fullname) && !empty($email) && !empty($street) && !empty($landmark) 
                 <div class="card">
 
 
-                        <div class="text-left logo p-2 px-5">
-
-                            <img src="https://i.imgur.com/2zDU056.png" width="50">
-                            
-
-                        </div>
+                      
 
                         <div class="invoice p-5">
 
@@ -131,14 +126,7 @@ if (!empty($fullname) && !empty($email) && !empty($street) && !empty($landmark) 
                                                 </div>
                                             </td>
 
-                                            <td>
-                                                <div class="py-2">
-
-                                                    <span class="d-block text-muted">Payment</span>
-                                                <span><img src="https://img.icons8.com/color/48/000000/mastercard.png" width="20" /></span>
-                                                    
-                                                </div>
-                                            </td>
+                                         
 
                                             <td>
                                                 <div class="py-2">
