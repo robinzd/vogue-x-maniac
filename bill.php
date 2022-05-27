521108773d1950bd502485f71a5380c19e80936e
robin <ibots.robin@gmail.com>
2022-05-28 00:11:36 +0530

    adding bill.php

--- bill.php
+++ bill.php
@@ -83,201 +83,145 @@ if (!empty($fullname) && !empty($email) && !empty($street) && !empty($landmark) 
 <body>
 
 
-    <div class="page-content container">
-        <div class="page-header text-blue-d2">
-            <h1 class="page-title text-secondary-d1">
-                Invoice
-                <small class="page-info">
-                    <i class="fa fa-angle-double-right text-80"></i>
-                    ID: #111-222
-                </small>
-            </h1>
+                           
+<div class="receipt-content">
+    <div class="container bootstrap snippets bootdey">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="invoice-wrapper">
+					<div class="intro">
+						Hi <strong>John McClane</strong>, 
+						<br>
+						This is the receipt for a payment of <strong>$312.00</strong> (USD) for your works
+					</div>
 
-            <div class="page-tools">
-                <div class="action-buttons">
-                    <a class="btn bg-white btn-light mx-1px text-95" href="#" data-title="Print">
-                        <i class="mr-1 fa fa-print text-primary-m1 text-120 w-2"></i>
-                        Print
-                    </a>
-                    <a class="btn bg-white btn-light mx-1px text-95" href="#" data-title="PDF">
-                        <i class="mr-1 fa fa-file-pdf-o text-danger-m1 text-120 w-2"></i>
-                        Export
-                    </a>
-                </div>
-            </div>
-        </div>
+					<div class="payment-info">
+						<div class="row">
+							<div class="col-sm-6">
+								<span>Payment No.</span>
+								<strong>434334343</strong>
+							</div>
+							<div class="col-sm-6 text-right">
+								<span>Payment Date</span>
+								<strong>Jul 09, 2014 - 12:20 pm</strong>
+							</div>
+						</div>
+					</div>
 
-        <div class="container px-0">
-            <div class="row mt-4">
-                <div class="col-12 col-lg-12">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="text-center text-150">
-                                <i class="fa fa-book fa-2x text-success-m2 mr-1"></i>
-                                <span class="text-default-d3">Bootdey.com</span>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- .row -->
+					<div class="payment-details">
+						<div class="row">
+							<div class="col-sm-6">
+								<span>Client</span>
+								<strong>
+									Andres felipe posada
+								</strong>
+								<p>
+									989 5th Avenue <br>
+									City of monterrey <br>
+									55839 <br>
+									USA <br>
+									<a href="#">
+										jonnydeff@gmail.com
+									</a>
+								</p>
+							</div>
+							<div class="col-sm-6 text-right">
+								<span>Payment To</span>
+								<strong>
+									Juan fernando arias
+								</strong>
+								<p>
+									344 9th Avenue <br>
+									San Francisco <br>
+									99383 <br>
+									USA <br>
+									<a href="#">
+										juanfer@gmail.com
+									</a>
+								</p>
+							</div>
+						</div>
+					</div>
 
-                    <hr class="row brc-default-l1 mx-n1 mb-4" />
+					<div class="line-items">
+						<div class="headers clearfix">
+							<div class="row">
+								<div class="col-xs-4">Description</div>
+								<div class="col-xs-3">Quantity</div>
+								<div class="col-xs-5 text-right">Amount</div>
+							</div>
+						</div>
+						<div class="items">
+							<div class="row item">
+								<div class="col-xs-4 desc">
+									Html theme
+								</div>
+								<div class="col-xs-3 qty">
+									3
+								</div>
+								<div class="col-xs-5 amount text-right">
+									$60.00
+								</div>
+							</div>
+							<div class="row item">
+								<div class="col-xs-4 desc">
+									Bootstrap snippet
+								</div>
+								<div class="col-xs-3 qty">
+									1
+								</div>
+								<div class="col-xs-5 amount text-right">
+									$20.00
+								</div>
+							</div>
+							<div class="row item">
+								<div class="col-xs-4 desc">
+									Snippets on bootdey 
+								</div>
+								<div class="col-xs-3 qty">
+									2
+								</div>
+								<div class="col-xs-5 amount text-right">
+									$18.00
+								</div>
+							</div>
+						</div>
+						<div class="total text-right">
+							<p class="extra-notes">
+								<strong>Extra Notes</strong>
+								Please send all items at the same time to shipping address by next week.
+								Thanks a lot.
+							</p>
+							<div class="field">
+								Subtotal <span>$379.00</span>
+							</div>
+							<div class="field">
+								Shipping <span>$0.00</span>
+							</div>
+							<div class="field">
+								Discount <span>4.5%</span>
+							</div>
+							<div class="field grand-total">
+								Total <span>$312.00</span>
+							</div>
+						</div>
 
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <div>
-                                <span class="text-sm text-grey-m2 align-middle">To:</span>
-                                <span class="text-600 text-110 text-blue align-middle">Alex Doe</span>
-                            </div>
-                            <div class="text-grey-m2">
-                                <div class="my-1">
-                                    Street, City
-                                </div>
-                                <div class="my-1">
-                                    State, Country
-                                </div>
-                                <div class="my-1"><i class="fa fa-phone fa-flip-horizontal text-secondary"></i> <b class="text-600">111-111-111</b></div>
-                            </div>
-                        </div>
-                        <!-- /.col -->
+						<div class="print">
+							<a href="#">
+								<i class="fa fa-print"></i>
+								Print this receipt
+							</a>
+						</div>
+					</div>
+				</div>
 
-                        <div class="text-95 col-sm-6 align-self-start d-sm-flex justify-content-end">
-                            <hr class="d-sm-none" />
-                            <div class="text-grey-m2">
-                                <div class="mt-1 mb-2 text-secondary-m1 text-600 text-125">
-                                    Invoice
-                                </div>
-
-                                <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">ID:</span> #111-222</div>
-
-                                <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">Issue Date:</span> Oct 12, 2019</div>
-
-                                <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">Status:</span> <span class="badge badge-warning badge-pill px-25">Unpaid</span></div>
-                            </div>
-                        </div>
-                        <!-- /.col -->
-                    </div>
-
-                    <div class="mt-4">
-                        <div class="row text-600 text-white bgc-default-tp1 py-25">
-                            <div class="d-none d-sm-block col-1">#</div>
-                            <div class="col-9 col-sm-5">Description</div>
-                            <div class="d-none d-sm-block col-4 col-sm-2">Qty</div>
-                            <div class="d-none d-sm-block col-sm-2">Unit Price</div>
-                            <div class="col-2">Amount</div>
-                        </div>
-
-                        <div class="text-95 text-secondary-d3">
-                            <div class="row mb-2 mb-sm-0 py-25">
-                                <div class="d-none d-sm-block col-1">1</div>
-                                <div class="col-9 col-sm-5">Domain registration</div>
-                                <div class="d-none d-sm-block col-2">2</div>
-                                <div class="d-none d-sm-block col-2 text-95">$10</div>
-                                <div class="col-2 text-secondary-d2">$20</div>
-                            </div>
-
-                            <div class="row mb-2 mb-sm-0 py-25 bgc-default-l4">
-                                <div class="d-none d-sm-block col-1">2</div>
-                                <div class="col-9 col-sm-5">Web hosting</div>
-                                <div class="d-none d-sm-block col-2">1</div>
-                                <div class="d-none d-sm-block col-2 text-95">$15</div>
-                                <div class="col-2 text-secondary-d2">$15</div>
-                            </div>
-
-                            <div class="row mb-2 mb-sm-0 py-25">
-                                <div class="d-none d-sm-block col-1">3</div>
-                                <div class="col-9 col-sm-5">Software development</div>
-                                <div class="d-none d-sm-block col-2">--</div>
-                                <div class="d-none d-sm-block col-2 text-95">$1,000</div>
-                                <div class="col-2 text-secondary-d2">$1,000</div>
-                            </div>
-
-                            <div class="row mb-2 mb-sm-0 py-25 bgc-default-l4">
-                                <div class="d-none d-sm-block col-1">4</div>
-                                <div class="col-9 col-sm-5">Consulting</div>
-                                <div class="d-none d-sm-block col-2">1 Year</div>
-                                <div class="d-none d-sm-block col-2 text-95">$500</div>
-                                <div class="col-2 text-secondary-d2">$500</div>
-                            </div>
-                        </div>
-
-                        <div class="row border-b-2 brc-default-l2"></div>
-
-                        <!-- or use a table instead -->
-                        <!--
-            <div class="table-responsive">
-                <table class="table table-striped table-borderless border-0 border-b-2 brc-default-l1">
-                    <thead class="bg-none bgc-default-tp1">
-                        <tr class="text-white">
-                            <th class="opacity-2">#</th>
-                            <th>Description</th>
-                            <th>Qty</th>
-                            <th>Unit Price</th>
-                            <th width="140">Amount</th>
-                        </tr>
-                    </thead>
-
-                    <tbody class="text-95 text-secondary-d3">
-                        <tr></tr>
-                        <tr>
-                            <td>1</td>
-                            <td>Domain registration</td>
-                            <td>2</td>
-                            <td class="text-95">$10</td>
-                            <td class="text-secondary-d2">$20</td>
-                        </tr> 
-                    </tbody>
-                </table>
-            </div>
-            -->
-
-                        <div class="row mt-3">
-                            <div class="col-12 col-sm-7 text-grey-d2 text-95 mt-2 mt-lg-0">
-                                Extra note such as company or payment information...
-                            </div>
-
-                            <div class="col-12 col-sm-5 text-grey text-90 order-first order-sm-last">
-                                <div class="row my-2">
-                                    <div class="col-7 text-right">
-                                        SubTotal
-                                    </div>
-                                    <div class="col-5">
-                                        <span class="text-120 text-secondary-d1">$2,250</span>
-                                    </div>
-                                </div>
-
-                                <div class="row my-2">
-                                    <div class="col-7 text-right">
-                                        Tax (10%)
-                                    </div>
-                                    <div class="col-5">
-                                        <span class="text-110 text-secondary-d1">$225</span>
-                                    </div>
-                                </div>
-
-                                <div class="row my-2 align-items-center bgc-primary-l3 p-2">
-                                    <div class="col-7 text-right">
-                                        Total Amount
-                                    </div>
-                                    <div class="col-5">
-                                        <span class="text-150 text-success-d3 opacity-2">$2,475</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <hr />
-
-                        <div>
-                            <span class="text-secondary-d1 text-105">Thank you for your business</span>
-                            <a href="#" class="btn btn-info btn-bold px-4 float-right mt-3 mt-lg-0">Pay Now</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
+				<div class="footer">
+					Copyright © 2014. company name
+				</div>
+			</div>
+		</div>
+	</div>
+</div>                    
 
 
 
