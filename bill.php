60d6d054c89853e1d943866107aa9697152b131f
robin <ibots.robin@gmail.com>
2022-05-27 20:37:55 +0530

    adding address.php in database

--- bill.php
+++ bill.php
@@ -3,6 +3,8 @@ session_start();
 
 include("./conn.php");
 include("./function.php");
+include("./orderid_generate.php");
+
 
 $user_data = check_login($conn);
 
@@ -35,21 +37,39 @@ if(!empty($fullname) && !empty($email ) && !empty($street) && !empty($landmark) 
     
     $query_address =mysqli_query ($conn,"INSERT INTO `users_address`( `user_id`, `user_fullname`, `user_email`, `user_address`, `user_landmark`,`user_city`,`user_pincode`) VALUES ('$userid ','$fullname','$email','$street','$landmark','$city','$pincode')");
   
-    if($query_address){
-    echo "<script>alert('added into the database!');</script>";
-  
-}
-  else{
-    
-    echo "<script>alert('mission failed!');</script>";
-  
-}
-
 }  
 
+$get_userscart = "select * from products_cart where user_id=$userid";
+
+$run_userscart = mysqli_query($conn,$get_userscart);
+
+
+
+while ($row_userscart = mysqli_fetch_array($run_userscart)) {
+
+    $product_id = $row_userscart['product_id'];
+    $product_quantity = $row_userscart['product_quantity'];
+    $product_size = $row_userscart['product_size'];
+
+}
+
+$order_id=random_num(10);
+
+$query_order = "select * from  users_order";
+
+$result_order = mysqli_query($conn, $query_order);
+
+$rowcount = mysqli_num_rows($result_order);
+
+if($rowcount == $rowcount){
+
+    $query_order =mysqli_query ($conn,"INSERT INTO `users_order`( `user_id`, `order_id`, `product_id`, `product_quantity`, `product_size`) VALUES ('$userid ','$order_id','$product_id','$product_quantity ','$product_size')");
+
+
+}
 
 else{
-  echo "<script>alert('Please Enter Some Valid Information!');</script>";
+  echo "<script>alert('Something Went Wrong!');</script>";
 }
 
 
