a00c951db0753c31b3f2c2983063ee866310bc43
robin <ibots.robin@gmail.com>
2022-05-27 18:33:36 +0530

    adding address.php in database

--- bill.php
+++ bill.php
@@ -33,8 +33,18 @@ echo "<br>";
 if(!empty($fullname) && !empty($email ) && !empty($street) && !empty($landmark) && !empty($city)&& !empty($pincode)){
     
     $query_address =mysqli_query ($conn,"INSERT INTO `users_address`( `user_id`, `user_fullname`, `user_email`, `user_address`, `user_landmark`,`user_city`,`user_pincode`) VALUES ('$userid ','$fullname','$email','$street','$city','$pincode')");
+  if($query_address){
+    echo "<script>alert('added into the database!');</script>";
   
 }
+  else{
+    
+    echo "<script>alert('mission failed!');</script>";
+  
+}
+
+}  
+
 
 else{
   echo "<script>alert('Please Enter Some Valid Information!');</script>";
