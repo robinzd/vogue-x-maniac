fe0e2a6aab469cb0212eda526fc104886f04e958
robin <ibots.robin@gmail.com>
2022-05-29 16:30:17 +0530

    some changes in the checking details

--- paytm_php_sample_app/payment-using-paytm/payment.php
+++ paytm_php_sample_app/payment-using-paytm/payment.php
@@ -57,7 +57,7 @@ $transactionURL = PAYTM_TXN_URL;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Check Details</title>
     <!-- fav icon -->
-    <link rel="icon" type="image/png" href="./favicon/vogue_x_maniac_png_K8m_icon.ico" />
+    <link rel="icon" type="image/png" href="./vogue_x_maniac_png_K8m_icon.ico" />
     <!-- bootsstrap cdn -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- font awesome cdn -->
