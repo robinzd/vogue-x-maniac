acf814fee5ead340c895a030ad8f5595051bc8b8
robin <ibots.robin@gmail.com>
2022-05-30 14:08:50 +0530

    fixing the bug

---
+++ buynow.php
@@ -0,0 +1,26 @@
+<?php
+
+$id = isset($_POST['productid']) ? $_POST['productid'] : "";
+
+echo $id;
+
+echo "<br>";
+
+$quantity = isset($_POST['quantity']) ? $_POST['quantity'] : "";
+
+echo $quantity;
+
+echo "<br>";
+
+$size = isset($_POST['size']) ? $_POST['size'] : "";
+
+echo $size;
+
+
+
+
+
+
+
+
+?>
