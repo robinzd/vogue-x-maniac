9b84b63f3237ae976621472d5f9abb71c858879e
robin <ibots.robin@gmail.com>
2022-05-31 18:55:05 +0530

    fixing the bug

--- admin_panel/shipping_charges/read.php
+++ admin_panel/shipping_charges/read.php
@@ -219,7 +219,7 @@ include('dbconnection.php');
 
 
         footer.bg-light.text-center.text-lg-start {
-            position: sticky;
+            position: fixed;
             left: 0;
             bottom: 0;
             width: 100%;
