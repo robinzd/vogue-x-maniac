d48c03afaebf09594f12a43ffed9d8a75b7d3794
robin <ibots.robin@gmail.com>
2022-06-01 01:44:46 +0530

    fixing the bug in the successfull.php

--- paytm_php_sample_app/payment-using-paytm/payment.php
+++ paytm_php_sample_app/payment-using-paytm/payment.php
@@ -6,6 +6,7 @@ $transaction_amount = $_POST['txn_no'];
 $customer_id = $_POST['cust_id'];
 $mobile_number = $_POST['mob_no'];
 $email = $_POST['email'];
+$order_id=$_POST['order_id'];
 
 
 // define("merchantMid", "dZlzzF17647713571019");
@@ -84,6 +85,7 @@ $transactionURL = PAYTM_TXN_URL;
                         </center>
                         <h1 class="text-center"><img src="./checking_details.png"></h1>
                         <form class="requires-validation" novalidatemethod='post' action='<?php echo $transactionURL; ?>' name='f1'>
+                            <input type="hidden" name="order_id" value="<?php echo $order_id; ?>">
                             <input type="hidden" name="ORDER_ID" value="<?php echo $orderId; ?>">
                             <input type="hidden" name="CUST_ID" value="<?php echo $custId; ?>" readonly>
                             <input type="hidden" name="MOBILE_NO" value="<?php echo $mobileNo; ?>" readonly>
