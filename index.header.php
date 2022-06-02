399db08ba7f691e70dddc00c8b93607f71c9358f
robin <ibots.robin@gmail.com>
2022-06-02 13:22:29 +0530

    adding dropdown in the index.php

--- index.header.php
+++ index.header.php
@@ -60,7 +60,7 @@ include("./conn.php");
 				<div class="col-md-4 d-flex">
 					<div class="social-media">
 						<p class="mb-0 d-flex">
-							<a href="./login_form/login.php" class="d-flex align-items-center justify-content-center" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="fa fa-user"><i class="sr-only">Facebook</i></span></a>
+							<a href="#" class="d-flex align-items-center justify-content-center" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="fa fa-user"><i class="sr-only">Facebook</i></span></a>
 
 						<div class="dropdown-menu dropdown-primary">
 							<a class="dropdown-item" href="#"><i class="fab fa-apple-pay"></i>&nbsp;&nbsp;Pay</a>
