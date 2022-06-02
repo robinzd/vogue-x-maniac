eab4c53baa0da2aed9a8d385e0cdafddc04ec796
robin <ibots.robin@gmail.com>
2022-06-02 13:06:34 +0530

    adding dropdown in the index.php

--- index.header.php
+++ index.header.php
@@ -158,6 +158,7 @@ include("./conn.php");
 	<script src="./js header/jquery.min.js"></script>
 	<script src="./js header/main.js"></script>
 	<script src="./js header/popper.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 
