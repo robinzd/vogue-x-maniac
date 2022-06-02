ae9ddadd94b930eb51818630f711e094c18d21c7
robin <ibots.robin@gmail.com>
2022-06-02 13:45:41 +0530

    adding dropdown in the index.php

--- user_dashboard.php
+++ user_dashboard.php
@@ -20,59 +20,19 @@
 
 <body>
 
-<div class="container mt-4">
+    <div class="container mt-4">
         <div class="row">
-            <div class="col-lg-3 my-lg-0 my-md-1">
-                <div id="sidebar" class="bg-purple">
-                    <div class="h4 text-white">Account</div>
-                    <ul>
-                        <li class="active">
-                            <a href="#" class="text-decoration-none d-flex align-items-start">
-                                <div class="fas fa-box pt-2 me-3"></div>
-                                <div class="d-flex flex-column">
-                                    <div class="link">My Account</div>
-                                    <div class="link-desc">View & Manage orders and returns</div>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="text-decoration-none d-flex align-items-start">
-                                <div class="fas fa-box-open pt-2 me-3"></div>
-                                <div class="d-flex flex-column">
-                                    <div class="link">My Orders</div>
-                                    <div class="link-desc">View & Manage orders and returns</div>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="text-decoration-none d-flex align-items-start">
-                                <div class="far fa-address-book pt-2 me-3"></div>
-                                <div class="d-flex flex-column">
-                                    <div class="link">Address Book</div>
-                                    <div class="link-desc">View & Manage Addresses</div>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="text-decoration-none d-flex align-items-start">
-                                <div class="far fa-user pt-2 me-3"></div>
-                                <div class="d-flex flex-column">
-                                    <div class="link">My Profile</div>
-                                    <div class="link-desc">Change your profile details & password</div>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" class="text-decoration-none d-flex align-items-start">
-                                <div class="fas fa-headset pt-2 me-3"></div>
-                                <div class="d-flex flex-column">
-                                    <div class="link">Help & Support</div>
-                                    <div class="link-desc">Contact Us for help and support</div>
-                                </div>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+            <div class="l-navbar" id="nav-bar">
+                <nav class="nav">
+                    <div> <a href="#" class="nav_logo"><i class="fa fa-user"></i><span class="nav_logo-name">Admin Panel</span> </a>
+                        <div class="nav_list"> <a href="./admin_panel.php" class="nav_link active"> <i class='bx bx-grid-alt nav_icon'></i> <span class="nav_name">Dashboard</span> </a>
+                            <a href="./user_table.php" class="nav_link"> <i class='bx bx-user nav_icon'></i> <span class="nav_name">Users Table</span> </a>
+                            <a href="./user_message.php" class="nav_link"> <i class='bx bx-message-square-detail nav_icon'></i> <span class="nav_name">Messages</span> </a>
+                            <!-- <a href="#" class="nav_link"> <i class='bx bx-bookmark nav_icon'></i> <span class="nav_name">Bookmark</span> </a> <a href="#" class="nav_link">
+                              <i class='bx bx-folder nav_icon'></i> <span class="nav_name">Files</span> </a> <a href="#" class="nav_link"> <i class='bx bx-bar-chart-alt-2 nav_icon'></i> <span class="nav_name">Stats</span> </a> -->
+                        </div>
+
+                </nav>
             </div>
             <div class="col-lg-9 my-lg-0 my-1">
                 <div id="main-content" class="bg-white border">
@@ -82,24 +42,21 @@
                     </div>
                     <div class="d-flex my-4 flex-wrap">
                         <div class="box me-4 my-1 bg-light">
-                            <img src="https://www.freepnglogos.com/uploads/box-png/cardboard-box-brown-vector-graphic-pixabay-2.png"
-                                alt="">
+                            <img src="https://www.freepnglogos.com/uploads/box-png/cardboard-box-brown-vector-graphic-pixabay-2.png" alt="">
                             <div class="d-flex align-items-center mt-2">
                                 <div class="tag">Orders placed</div>
                                 <div class="ms-auto number">10</div>
                             </div>
                         </div>
                         <div class="box me-4 my-1 bg-light">
-                            <img src="https://www.freepnglogos.com/uploads/shopping-cart-png/shopping-cart-campus-recreation-university-nebraska-lincoln-30.png"
-                                alt="">
+                            <img src="https://www.freepnglogos.com/uploads/shopping-cart-png/shopping-cart-campus-recreation-university-nebraska-lincoln-30.png" alt="">
                             <div class="d-flex align-items-center mt-2">
                                 <div class="tag">Items in Cart</div>
                                 <div class="ms-auto number">10</div>
                             </div>
                         </div>
                         <div class="box me-4 my-1 bg-light">
-                            <img src="https://www.freepnglogos.com/uploads/love-png/love-png-heart-symbol-wikipedia-11.png"
-                                alt="">
+                            <img src="https://www.freepnglogos.com/uploads/love-png/love-png-heart-symbol-wikipedia-11.png" alt="">
                             <div class="d-flex align-items-center mt-2">
                                 <div class="tag">Wishlist</div>
                                 <div class="ms-auto number">10</div>
@@ -118,8 +75,7 @@
                                     <div class="fs-8">Products #03</div>
                                     <div class="fs-8">22 August, 2020 | 12:05 PM</div>
                                     <div class="rating d-flex align-items-center pt-1">
-                                        <img src="https://www.freepnglogos.com/uploads/like-png/like-png-hand-thumb-sign-vector-graphic-pixabay-39.png"
-                                            alt=""><span class="px-2">Rating:</span>
+                                        <img src="https://www.freepnglogos.com/uploads/like-png/like-png-hand-thumb-sign-vector-graphic-pixabay-39.png" alt=""><span class="px-2">Rating:</span>
                                         <span class="fas fa-star"></span>
                                         <span class="fas fa-star"></span>
                                         <span class="fas fa-star"></span>
@@ -167,8 +123,7 @@
                                     <div class="fs-8">Products #03</div>
                                     <div class="fs-8">22 August, 2020 | 12:05 PM</div>
                                     <div class="rating d-flex align-items-center pt-1">
-                                        <img src="https://www.freepnglogos.com/uploads/like-png/like-png-hand-thumb-sign-vector-graphic-pixabay-39.png"
-                                            alt=""><span class="px-2">Rating:</span>
+                                        <img src="https://www.freepnglogos.com/uploads/like-png/like-png-hand-thumb-sign-vector-graphic-pixabay-39.png" alt=""><span class="px-2">Rating:</span>
                                         <span class="fas fa-star"></span>
                                         <span class="fas fa-star"></span>
                                         <span class="fas fa-star"></span>
