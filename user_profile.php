f0517612f3fbcb3c06ccfa9a82285dca9832655b
robin <ibots.robin@gmail.com>
2022-06-06 23:12:09 +0530

    adding new file into the app

--- user_profile.php
+++ user_profile.php
@@ -23,7 +23,10 @@
     <div class="container emp-profile">
         <form method="post">
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-md-4">
+                   
+                </div>
+                <div class="col-md-6">
                     <div class="profile-head">
                         <h5>
                             Kshiti Ghelani
@@ -40,12 +43,12 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col-md-12">
+                <div class="col-md-2">
                     <input type="submit" class="profile-edit-btn" name="btnAddMore" value="Edit Profile" />
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-md-8">
                     <div class="tab-content profile-tab" id="myTabContent">
                         <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
                             <div class="row">
@@ -91,15 +94,15 @@
                         </div>
                         <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
                             <div class="row">
-                                <div class="col-md-12">
+                                <div class="col-md-6">
                                     <label>Experience</label>
                                 </div>
-                                <div class="col-md-12">
+                                <div class="col-md-6">
                                     <p>Expert</p>
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="col-md-12">
+                                <div class="col-md-6">
                                     <label>Hourly Rate</label>
                                 </div>
                                 <div class="col-md-6">
