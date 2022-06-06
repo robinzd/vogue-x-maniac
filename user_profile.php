f912466f73ab342bc136c2033e6a9f721ced20b0
robin <ibots.robin@gmail.com>
2022-06-06 23:08:28 +0530

    adding new file into the app

--- user_profile.php
+++ user_profile.php
@@ -23,15 +23,6 @@
     <div class="container emp-profile">
         <form method="post">
             <div class="row">
-                <div class="col-md-4">
-                    <div class="profile-img">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog" alt="" />
-                        <div class="file btn btn-lg btn-primary">
-                            Change Photo
-                            <input type="file" name="file" />
-                        </div>
-                    </div>
-                </div>
                 <div class="col-md-6">
                     <div class="profile-head">
                         <h5>
