35cc2d43b009a0bc29a12935a06d07c710b6d756
robin <ibots.robin@gmail.com>
2022-06-07 14:25:34 +0530

    some changes in the app

--- user_profile.php
+++ user_profile.php
@@ -92,14 +92,14 @@
 
 
         <div class="d-grid gap-2 col-6 mx-auto">
-        <button class="btn btn-primary" type="button">Button</button>
-    </div>
+            <button class="btn btn-primary" type="button">Button</button>
+        </div>
 
 
     </div>
 
 
-    
+
 
 
 
