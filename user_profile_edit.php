bec3bbbf08a17c07635a5e26d38a7d42f4527bfd
robin <ibots.robin@gmail.com>
2022-06-08 00:27:49 +0530

    some changes in the app

--- user_profile_edit.php
+++ user_profile_edit.php
@@ -17,7 +17,7 @@ if (isset($_POST['submit'])) {
 
 	if ($query) {
 		echo "<script>alert('You have successfully update the profile details');</script>";
-		echo "<script type='text/javascript'> document.location ='shipping_details.php'; </script>";
+		echo "<script type='text/javascript'> document.location ='user_profile_edit'; </script>";
 	} else {
 		echo "<script>alert('Something Went Wrong. Please try again');</script>";
 	}
