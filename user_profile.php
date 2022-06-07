561195430e5846ad677d3a42b2cd1b9cd8b43736
robin <ibots.robin@gmail.com>
2022-06-07 16:05:33 +0530

    some changes in the app

--- user_profile.php
+++ user_profile.php
@@ -8,6 +8,7 @@
     <title>Document</title>
     <!-- fav icon -->
     <link rel="icon" type="image/png" href="./favicon/vogue_x_maniac_png_K8m_icon.ico" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <!-- bootsstrap cdn -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- font awesome cdn -->
