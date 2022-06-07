2fba42fb67b6e671f3b739506e1f8b7bf93f7a28
robin <ibots.robin@gmail.com>
2022-06-07 16:07:35 +0530

    some changes in the app

--- user_profile.php
+++ user_profile.php
@@ -8,7 +8,6 @@
     <title>Document</title>
     <!-- fav icon -->
     <link rel="icon" type="image/png" href="./favicon/vogue_x_maniac_png_K8m_icon.ico" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <!-- bootsstrap cdn -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- font awesome cdn -->
@@ -92,7 +91,7 @@
 
 
     <div class="d-grid gap-2 col-3 mx-auto">
-    <a href="edit.php?editid=<?php echo htmlentities($row['ID']); ?>"class="btn btn-primary edit" type="button"  title="Edit" data-toggle="tooltip"><i class="material-icons">&#xE254;</i>Edit</a>
+    <a href="edit.php?editid=<?php echo htmlentities($row['ID']); ?>"class="btn btn-primary edit" type="button"  title="Edit" data-toggle="tooltip">Edit</a>
     </div> <br>
 
 
