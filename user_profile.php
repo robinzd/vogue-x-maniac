32f09d08982fb25cd425477cde8aca1a6a97daff
robin <ibots.robin@gmail.com>
2022-06-07 16:10:38 +0530

    some changes in the app

--- user_profile.php
+++ user_profile.php
@@ -45,7 +45,7 @@
 
     <!--back to top ends -->
 
-    <h2>User Profile</h2>
+    <img src="./159-1595553_profile-blue-logo-png-removebg-preview.png"><h2>User Profile</h2>
 
 
     <div class="container height-100 d-flex justify-content-center align-items-center">
