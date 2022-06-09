315d788798922e44286ccb3c712ed72d4a91d73f
robin <ibots.robin@gmail.com>
2022-06-10 03:09:25 +0530

    some changes in the app

--- user_profile.php
+++ user_profile.php
@@ -58,7 +58,7 @@
 
     <!--back to top ends -->
     <div class="container">
-    <h1 class="heading">User Profile</h1><br>
+    <h1 class="heading">User Profile</h1><i class="fa fa-user"></i><br>
         <div class="cardcontainer">
             <div class="content">
                 <br>
