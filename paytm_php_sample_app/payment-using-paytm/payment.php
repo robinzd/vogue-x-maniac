e6199d0729b8a593aef1ff8484ad711311371d6d
robin <ibots.robin@gmail.com>
2022-06-17 01:55:09 +0530

    fixing bug in the paytm

--- paytm_php_sample_app/payment-using-paytm/payment.php
+++ paytm_php_sample_app/payment-using-paytm/payment.php
@@ -2,11 +2,12 @@
 require_once("PaytmKit/lib/config_paytm.php");
 require_once("PaytmKit/lib/encdec_paytm.php");
 
+$order_id = $_POST['order_id'];
 $transaction_amount = $_POST['txn_no'];
 $customer_id = $_POST['cust_id'];
 $mobile_number = $_POST['mob_no'];
 $email = $_POST['email'];
-$order_id = $_POST['order_id'];
+
 
 
 
