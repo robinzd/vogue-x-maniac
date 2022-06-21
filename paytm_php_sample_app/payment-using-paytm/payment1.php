898bb298e749c982a84f978f7b3f86b1443cc531
robin <ibots.robin@gmail.com>
2022-06-21 23:10:04 +0530

    cod my order backend logic

--- paytm_php_sample_app/payment-using-paytm/payment1.php
+++ paytm_php_sample_app/payment-using-paytm/payment1.php
@@ -14,7 +14,7 @@ $payondelivery_amount = $_POST['cod'];
 echo $payondelivery_amount;
 
 
-if ($payondelivery_amount == 250) {
+if (!empty($order_Id) && !empty($transaction_amount)) {
 
     $query_address = mysqli_query($conn, "INSERT INTO `cod_payment_info`( `order_id`, `transaction_amount`) VALUES ('$order_Id','$transaction_amount')");
 }
