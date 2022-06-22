3568a08b41afc5fe3f347068974ff307e773643e
robin <ibots.robin@gmail.com>
2022-06-22 23:14:36 +0530

    creating the admin panel

--- admin_panel/user_order_details.php
+++ admin_panel/user_order_details.php
@@ -158,14 +158,13 @@ include('dbconnection.php');
         <script src="admin_panel.js"></script>
 
 
-    <footer class="bg-light text-center text-lg-start">
-        <!-- Copyright -->
-        <div class="text-center p-3" style="background-color:#f5f5f5;">
-            © 2022 Copyright:
-            <a class="text1" href="/index.php`">Vogue X Maniac</a>
-        </div>
-        <!-- Copyright -->
-    </footer>
+        <footer class="bg-light text-center">
+            <!-- Copyright -->
+            <div class="text-center p-3" style="background-color:ghostwhite;">
+                © 2022 Copyright:
+                <a class="text1" href="../index.php">Vogue X Maniac</a>
+            </div>
+        </footer>
 </body>
 
 </html>
