e482e10e7e4d068efc6aa471cef6cf77a2d9b99e
robin <ibots.robin@gmail.com>
2022-06-22 14:55:22 +0530

    order info backend fixing bug

--- order_read.php
+++ order_read.php
@@ -63,9 +63,7 @@ $status = $_POST['status'];
 
     <div class="container mt-5 mb-5  d-none d-sm-block">
 
-        <div class="text-left"><a onclick="history.back()"><i class="fa fa-arrow-left"></i></a></div><br>
-
-        <div class="row d-flex justify-content-center">
+    <div class="row d-flex justify-content-center">
 
             <div class="col-md-8">
 
