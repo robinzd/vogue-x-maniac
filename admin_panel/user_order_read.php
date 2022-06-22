55d8ad4dd2810db762518dfa119f1f7b772d4f94
robin <ibots.robin@gmail.com>
2022-06-23 01:04:24 +0530

    fixing some bugs in the app

--- admin_panel/user_order_read.php
+++ admin_panel/user_order_read.php
@@ -24,7 +24,7 @@ $order_id_1 = $_GET['order_id'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Users Order Details </title>
     <!-- fav icon -->
-    <link rel="icon" type="image/png" href="./favicon/vogue_x_maniac_png_K8m_icon.ico" />
+    <link rel="icon" type="image/png" href="./favicon/icons8-view-details-48.png"/>
     <!-- bootsstrap cdn -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- font awesome cdn -->
