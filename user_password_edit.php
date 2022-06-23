1f9bf140ed6946c1925b39d82db98174c073b5b6
robin <ibots.robin@gmail.com>
2022-06-23 23:20:51 +0530

    bug fixed in the app

--- user_password_edit.php
+++ user_password_edit.php
@@ -1,6 +1,14 @@
 <?php
 //Database Connection
-include('./conn.php');
+session_start();
+
+include("./conn.php");
+include("./function.php");
+
+
+
+$user_data = check_login($conn);
+
 if (isset($_POST['submit'])) {
 	$eid = $_GET['editid'];
 	//Getting Post Values
@@ -35,8 +43,8 @@ if (isset($_POST['submit'])) {
 	<!-- font awesome cdn -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<!-- link the external stylesheet -->
-	 <link rel="stylesheet" type="text/css" href="./user_password_edit.css">
-	
+	<link rel="stylesheet" type="text/css" href="./user_password_edit.css">
+
 </head>
 
 <body>
@@ -95,97 +103,97 @@ if (isset($_POST['submit'])) {
 
 	<!-- Footer -->
 
-    <footer class="text-center text-lg-start text-dark" style="background-color:lightgrey">
-        <!-- Grid container -->
-        <div class="container p-4 pb-0">
-            <!-- Section: Links -->
-            <section class="">
-                <!--Grid row-->
-                <div class="row">
-                    <!-- Grid column -->
-                    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                        <img class="footerimage" src="./header images/vogue x maniac png.png" alt="brand image">
-                        <p class="text-white">
-                           vogue-x-maniac is the one of the leading ecommerce website
-                        </p>
-                    </div>
-                    <!-- Grid column -->
+	<footer class="text-center text-lg-start text-dark" style="background-color:lightgrey">
+		<!-- Grid container -->
+		<div class="container p-4 pb-0">
+			<!-- Section: Links -->
+			<section class="">
+				<!--Grid row-->
+				<div class="row">
+					<!-- Grid column -->
+					<div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+						<img class="footerimage" src="./header images/vogue x maniac png.png" alt="brand image">
+						<p class="text-white">
+							vogue-x-maniac is the one of the leading ecommerce website
+						</p>
+					</div>
+					<!-- Grid column -->
 
-                    <hr class="w-100 clearfix d-md-none" />
+					<hr class="w-100 clearfix d-md-none" />
 
-                    <!-- Grid column -->
-                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                        <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
-                        <p class="text-white">MDBootstrap</p>
-                        <p class="text-white">MDWordPress</p>
-                        <p class="text-white">BrandFlow</p>
-                        <p class="text-white">Angular</p>
-                    </div>
-                    <!-- Grid column -->
+					<!-- Grid column -->
+					<div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+						<h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+						<p class="text-white">MDBootstrap</p>
+						<p class="text-white">MDWordPress</p>
+						<p class="text-white">BrandFlow</p>
+						<p class="text-white">Angular</p>
+					</div>
+					<!-- Grid column -->
 
-                    <hr class="w-100 clearfix d-md-none" />
+					<hr class="w-100 clearfix d-md-none" />
 
-                    <!-- Grid column -->
-                    <hr class="w-100 clearfix d-md-none" />
+					<!-- Grid column -->
+					<hr class="w-100 clearfix d-md-none" />
 
-                    <!-- Grid column -->
-                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-                        <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
-                        <p class="text-white"><i class="fas fa-home text-white mr-3"></i> New York, NY 10012, US</p>
-                        <p class="text-white" id="address"><i class="fas fa-envelope text-white mr-3"></i> inf0@text-white@gmail.com
-                        </p>
-                        <p class="text-white"><i class="fas fa-phone text-white mr-3"></i> + 01 234 567 88</p>
-                        <p class="text-white"><i class="fas fa-print text-white mr-3"></i> + 01 234 567 89</p>
-                    </div>
-                    <!-- Grid column -->
+					<!-- Grid column -->
+					<div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+						<h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+						<p class="text-white"><i class="fas fa-home text-white mr-3"></i> New York, NY 10012, US</p>
+						<p class="text-white" id="address"><i class="fas fa-envelope text-white mr-3"></i> inf0@text-white@gmail.com
+						</p>
+						<p class="text-white"><i class="fas fa-phone text-white mr-3"></i> + 01 234 567 88</p>
+						<p class="text-white"><i class="fas fa-print text-white mr-3"></i> + 01 234 567 89</p>
+					</div>
+					<!-- Grid column -->
 
-                    <!-- Grid column -->
-                    <div class="col-md-3 col-lg-4 col-xl-4 mx-auto mt-3">
-                        <h6 class="text-uppercase mb-4 font-weight-bold">Follow us</h6>
+					<!-- Grid column -->
+					<div class="col-md-3 col-lg-4 col-xl-4 mx-auto mt-3">
+						<h6 class="text-uppercase mb-4 font-weight-bold">Follow us</h6>
 
-                        <!-- Facebook -->
-                        <a class="btn pmd-btn-fab pmd-ripple-effect btn-primary pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Facebook" role="button"><i class="fab fa-facebook"></i></a>
+						<!-- Facebook -->
+						<a class="btn pmd-btn-fab pmd-ripple-effect btn-primary pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Facebook" role="button"><i class="fab fa-facebook"></i></a>
 
-                        <!-- Twitter -->
-                        <a class="btn pmd-btn-fab pmd-ripple-effect btn-info pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Twitter" role="button"><i class="fab fa-twitter"></i></a>
+						<!-- Twitter -->
+						<a class="btn pmd-btn-fab pmd-ripple-effect btn-info pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Twitter" role="button"><i class="fab fa-twitter"></i></a>
 
-                        <!-- youtube -->
-                        <a class="btn pmd-btn-fab pmd-ripple-effect btn-secondary pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="youtube" role="button"><i class="fab fa-youtube"></i></a>
+						<!-- youtube -->
+						<a class="btn pmd-btn-fab pmd-ripple-effect btn-secondary pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="youtube" role="button"><i class="fab fa-youtube"></i></a>
 
 
-                        <!-- Instagram -->
-                        <a class="btn pmd-btn-fab pmd-ripple-effect btn-danger pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Instagram" role="button"><i class="fab fa-instagram"></i></a>
+						<!-- Instagram -->
+						<a class="btn pmd-btn-fab pmd-ripple-effect btn-danger pmd-btn-flat mx-1 my-2" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Instagram" role="button"><i class="fab fa-instagram"></i></a>
 
-                    </div>
-                </div>
-                <!--Grid row-->
-            </section>
-            <!-- Section: Links -->
-        </div>
-        <!-- Grid container -->
+					</div>
+				</div>
+				<!--Grid row-->
+			</section>
+			<!-- Section: Links -->
+		</div>
+		<!-- Grid container -->
 
-        <!-- Copyright -->
-        <div class="text-center p-3" style="background-color: rgba(255, 255, 255, 0.096)">
+		<!-- Copyright -->
+		<div class="text-center p-3" style="background-color: rgba(255, 255, 255, 0.096)">
 
-            <p class="text-dark"> ©2022 Vogue X Maniac.All Rights Reserved
-            </p>
-            <img class="px-2" src="./payment_pics/2560px-MasterCard_Logo.svg-removebg-preview.png">
-            <img class="px-2" src="./payment_pics/1200px-Visa.svg-removebg-preview.png">
-            <img class="px-2" src="./payment_pics/paypal-logo-removebg-preview.png">
-        </div>
-        <!-- Copyright -->
-    </footer>
-    <!-- end of the footer -->
-	
+			<p class="text-dark"> ©2022 Vogue X Maniac.All Rights Reserved
+			</p>
+			<img class="px-2" src="./payment_pics/2560px-MasterCard_Logo.svg-removebg-preview.png">
+			<img class="px-2" src="./payment_pics/1200px-Visa.svg-removebg-preview.png">
+			<img class="px-2" src="./payment_pics/paypal-logo-removebg-preview.png">
+		</div>
+		<!-- Copyright -->
+	</footer>
+	<!-- end of the footer -->
 
-	
+
+
 
 	<!-- add external js file -->
 	<script src="./user_password_edit.js"></script>
 	<!-- j query -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- owl carousel -->
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+	<!-- owl carousel -->
+	<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 </body>
 
 </html>
