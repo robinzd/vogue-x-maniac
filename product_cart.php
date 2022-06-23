df38477a35fec216b5e7fd251d41e88b677b1200
robin <ibots.robin@gmail.com>
2022-06-23 21:25:11 +0530

    creating the responsiveness in the cart

--- product_cart.php
+++ product_cart.php
@@ -57,7 +57,7 @@ if (isset($_GET['delid'])) {
     <!--back to top ends -->
 
     <!-- product_cart starts hidden only on xs -->
-    <div class="table-responsive d-none d-sm-block">
+    <div class="table-responsive d-none d-sm-block  d-sm-none d-md-block">
         <div id="shopping-cart">
 
 
