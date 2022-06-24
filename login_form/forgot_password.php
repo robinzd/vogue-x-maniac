f9c392831c824bd96091aa2705e2e418e9938b76
robin <ibots.robin@gmail.com>
2022-06-24 15:17:45 +0530

    fixing bug in the forgot password

--- login_form/forgot_password.php
+++ login_form/forgot_password.php
@@ -59,6 +59,12 @@
 						<span class="label-input100">Email</span>
 					</div>
 
+					<div class="wrap-input100 validate-input" data-validate="Password is required">
+						<input class="input100" type="password" name="password">
+						<span class="focus-input100"></span>
+						<span class="label-input100">Password</span>
+					</div>
+
 
 					<div class="container-login100-form-btn">
 						<button class="login100-form-btn" type="submit" value="login">
--- login_form/new_password.php
+++ login_form/new_password.php
@@ -3,6 +3,7 @@
 include("../conn.php");
 
 $email_id_1 = $_POST["email"];
+$new_password = $_POST['password'];
 
 echo $email_id_1;
 
@@ -25,9 +26,7 @@ while ($row_email_id = mysqli_fetch_array($run_email_id)) {
 	$email_id = $row_email['user_email'];
 }
 
-if (isset($_POST['submit'])) {
-	//Getting Post Values
-	$new_password = $_POST['password'];
+
 	//Query for data updation
 	if ($email_id_1 == $email_id) {
 		$query = mysqli_query($conn, "update users set user_password='$new_password' where user_email='$email_id_1'");
@@ -39,7 +38,7 @@ if (isset($_POST['submit'])) {
 			echo "<script>alert('Something Went Wrong. Please try again');</script>";
 		}
 	}
-}
+
 ?>
 
 
@@ -47,102 +46,3 @@ if (isset($_POST['submit'])) {
 
 
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-	<title>Forgot Password</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!--===============================================================================================-->
-	<link rel="icon" type="image/png" href="../favicon/vogue_x_maniac_png_K8m_icon.ico" />
-
-	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
-	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="css/login1.css">
-	<link rel="stylesheet" type="text/css" href="css/login.css">
-	<!--===============================================================================================-->
-</head>
-
-<body style="background-color: #666666;">
-
-
-
-
-	<div class="limiter">
-		<div class="container-login100">
-			<div class="wrap-login100">
-				<form method="post" class="login100-form validate-form">
-
-					<span class="login100-form-title p-b-43">
-						<img src="../header images/vogue x maniac png.png">
-
-					</span>
-
-
-					<span class="login100-form-title p-b-43">
-						New Password
-					</span>
-
-
-					<div class="wrap-input100 validate-input" data-validate="Valid email is required:">
-						<input class="input100" type="password" name="password">
-						<span class="focus-input100"></span>
-						<span class="label-input100">New Password</span>
-					</div>
-
-
-					<div class="container-login100-form-btn">
-						<button class="login100-form-btn" type="submit" name="submit" value="login">
-							Login
-						</button>
-					</div>
-				</form>
-
-				<div class="login100-more" style="background-image: url('images/login-pic.jpg');">
-				</div>
-			</div>
-		</div>
-	</div>
-
-
-
-
-
-
-
-	<!--===============================================================================================-->
-	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-	<!--===============================================================================================-->
-	<script src="vendor/animsition/js/animsition.min.js"></script>
-	<!--===============================================================================================-->
-	<script src="vendor/bootstrap/js/popper.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-	<!--===============================================================================================-->
-	<script src="vendor/select2/select2.min.js"></script>
-	<!--===============================================================================================-->
-	<script src="vendor/daterangepicker/moment.min.js"></script>
-	<script src="vendor/daterangepicker/daterangepicker.js"></script>
-	<!--===============================================================================================-->
-	<script src="vendor/countdowntime/countdowntime.js"></script>
-	<!--===============================================================================================-->
-	<script src="js/main.js"></script>
-
-</body>
-
-</html>
