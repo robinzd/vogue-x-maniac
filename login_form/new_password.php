143cb58c90361c7d46388ca3c577c290f03857e0
robin <ibots.robin@gmail.com>
2022-06-24 15:07:41 +0530

    fixing bug in the forgot password

--- login_form/new_password.php
+++ login_form/new_password.php
@@ -5,45 +5,44 @@ include("../conn.php");
 
 $email_id_1 = $_POST["email"];
 
-// echo $email_id_1;
+ echo $email_id_1;
 
-// echo "<br>";
+ echo "<br>";
 
-$get_email_id = "select user_email from users where user_email=$email_id_1";
+$get_email_id = "select user_email from users where user_email='$email_id_1'";
 
-// echo $get_email_id;
+echo $get_email_id;
 
-// echo "<br>";
+echo "<br>";
 
 $run_email_id = mysqli_query($conn,$get_email_id);
 
 
 
-// while ($row_email_id = mysqli_fetch_array($run_email_id )) {
+while ($row_email_id = mysqli_fetch_array($run_email_id )) {
 
 	
 
-// 	$email_id=$row_email['user_email'] ;
+$email_id=$row_email['user_email'];
 
 
 
-// }
+ }
 
 
-//    //Getting Post Values
-// 	$new_password = $_POST['password'];
+    //Getting Post Values
+	$new_password = $_POST['password'];
+ 	//Query for data updation
+	if($email_id_1 == $email_id ){
+	$query = mysqli_query($conn, "update users set user_password='$new_password' where user_email='$email_id_1'");
 
-// 	//Query for data updation
-// 	if($email_id_1 == $email_id ){
-// 	$query = mysqli_query($conn, "update users set user_password='$new_password' where user_email='$email_id_1'");
-
-// 	if ($query) {
-// 		echo "<script>alert('You have successfully changed the password');</script>";
-// 		echo "<script type='text/javascript'> document.location ='./login.php'; </script>";
-// 	} else {
-// 		echo "<script>alert('Something Went Wrong. Please try again');</script>";
-// 	}
-// }
+	if ($query) {
+	echo "<script>alert('You have successfully changed the password');</script>";
+		echo "<script type='text/javascript'> document.location ='./login.php'; </script>";
+	} else {
+		echo "<script>alert('Something Went Wrong. Please try again');</script>";
+	}
+ }
 
 ?>
 
