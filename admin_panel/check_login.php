1ff349f9ea4d676ec047081a1f267a13dbf64af3
robin <ibots.robin@gmail.com>
2022-07-06 17:29:48 +0530

    fixing bug in the app

--- admin_panel/check_login.php
+++ admin_panel/check_login.php
@@ -3,14 +3,12 @@
 function check_login($conn)
 {
 
-    if(isset($_SESSION['admin_id']))
-    {
+    if (isset($_SESSION['admin_id'])) {
         $id = $_SESSION['admin_id'];
-        $query ="select*from admin_login where admin_id='$id' limit 1";
+        $query = "select*from admin_login where admin_id='$id'";
 
-        $result = mysqli_query($conn,$query);
-        if($result && mysqli_num_rows($result) > 0)
-        {
+        $result = mysqli_query($conn, $query);
+        if ($result && mysqli_num_rows($result) > 0) {
             $admin_data = mysqli_fetch_assoc($result);
             return $admin_data;
         }
