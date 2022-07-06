d2930d6b67edbc3ec478169e4a0241011d3df52a
robin <ibots.robin@gmail.com>
2022-07-06 15:03:53 +0530

    creating the admin panel login

---
+++ admin_panel/admin_panel_login.php
@@ -0,0 +1,94 @@
+<?php
+
+include("../conn.php");
+
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    // something was posted
+    $email = $_POST["Username"];
+    $password = $_POST["password"];
+
+
+    $get_admin = "select * admin_login where email_admin='$email' and password_admin='$password'";
+
+    $run_admin = mysqli_query($conn, $get_admin);
+
+
+
+    while ($row_admin = mysqli_fetch_array($run_admin)) {
+
+        $admin_email = $row_admin['email_admin'];
+        $admin_password = $row_admin['password_admin'];
+    }
+
+    if ($admin_email == $email &&  $admin_password == $password) {
+        header("location:./admin_panel.php");
+        die;
+    }
+}
+else{
+    echo "<script>alert('Entered Email Or Password Wrong');</script>";
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel Login</title>
+    <!-- fav icon -->
+    <link rel="icon" type="image/png" href="./favicon/vogue_x_maniac_png_K8m_icon.ico" />
+    <!-- bootsstrap cdn -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- font awesome cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- owl carousel -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <!-- link the external stylesheet -->
+    <link rel="stylesheet" type="text/css" href="./admin_panel_login.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <h2 class="text-center text-dark mt-5">Login Form</h2>
+                <div class="text-center mb-5 text-dark">Made with bootstrap</div>
+                <div class="card my-5">
+
+                    <form class="card-body cardbody-color p-lg-5" method="POST">
+
+                        <div class="text-center">
+                            <img src="https://cdn.pixabay.com/photo/2016/03/31/19/56/avatar-1295397__340.png" class="img-fluid profile-image-pic img-thumbnail rounded-circle my-3" width="200px" alt="profile">
+                        </div>
+
+                        <div class="mb-3">
+                            <input type="text" class="form-control" id="Username" aria-describedby="emailHelp" placeholder="User Name" required="true">
+                        </div>
+                        <div class="mb-3">
+                            <input type="password" class="form-control" id="password" placeholder="password" required="true">
+                        </div>
+                        <div class="text-center"><button type="submit" class="btn btn-color px-5 mb-5 w-100">Login</button></div>
+                        <div id="emailHelp" class="form-text text-center mb-5 text-dark">Not
+                            Registered? <a href="#" class="text-dark fw-bold"> Create an
+                                Account</a>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- j query -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- owl carousel -->
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+</body>
+
+</html>
