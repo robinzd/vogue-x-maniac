610dd8e426bb73cbb5c0d863baf97baecef29745
robin <ibots.robin@gmail.com>
2022-07-07 17:39:52 +0530

    changing some designs in the app

--- admin_panel/admin_panel_login.php
+++ admin_panel/admin_panel_login.php
@@ -61,7 +61,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel Login</title>
     <!-- fav icon -->
-    <link rel="icon" type="image/png" href="./favicon/vogue_x_maniac_png_K8m_icon.ico" />
+    <link rel="icon" type="image/png" href="./favicon/icons8-admin-settings-male-48.png" />
     <!-- bootsstrap cdn -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- font awesome cdn -->
