0564f9c252aaa0f0174d9d9170756c9d27d4eca6
robin <ibots.robin@gmail.com>
2022-07-08 00:06:41 +0530

    creating search box in the admin panel.php

--- admin_panel/user_order_details.php
+++ admin_panel/user_order_details.php
@@ -79,8 +79,8 @@ check_login($conn);
         </div>
 
         <form class="form-inline d-flex justify-content-end md-form form-sm active-purple-2 mt-2">
-            <input class="form-control form-control-sm mr-3 w-75" type="text" placeholder="Search" aria-label="Search">
-            <i class="fas fa-search" aria-hidden="true"></i>
+            <input class="form-control form-control-sm mr-3" type="text" placeholder="Search" aria-label="Search">
+            <i class="fa fa-search" aria-hidden="true"></i>
         </form>
 
         <div class="container-xl">
