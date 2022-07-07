57068a960ce2a8947cb08b3a746932e3cd1ca9c6
robin <ibots.robin@gmail.com>
2022-07-07 16:37:28 +0530

    changing the design in the admin panel

--- admin_panel/admin_panel_login.php
+++ admin_panel/admin_panel_login.php
@@ -76,14 +76,13 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     <div class="container">
         <div class="row">
             <div class="col-md-6 offset-md-3">
-                <h2 class="text-center text-dark mt-5">Login Form</h2>
-                <div class="text-center mb-5 text-dark">Made with bootstrap</div>
+                <h2 class="text-center text-dark mt-5">Admin Login</h2>
                 <div class="card my-5">
 
                     <form class="card-body cardbody-color p-lg-5" method="Post">
 
                         <div class="text-center">
-                            <img src="https://cdn.pixabay.com/photo/2016/03/31/19/56/avatar-1295397__340.png" class="img-fluid profile-image-pic img-thumbnail rounded-circle my-3" width="200px" alt="profile">
+                            <img src="./images (1).png" class="img-fluid profile-image-pic img-thumbnail rounded-circle my-3" width="200px" alt="profile">
                         </div>
 
                         <div class="mb-3">
