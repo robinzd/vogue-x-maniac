9d5925d6efd6082c321822d8ce8688a0cdd24b8a
robin <ibots.robin@gmail.com>
2022-07-17 15:39:24 +0530

    cart delete logic after the product payment success

--- user_dashboard.php
+++ user_dashboard.php
@@ -8,39 +8,39 @@ $user_data = check_login($conn);
 
 $userid = $user_data['user_id'];
 
-$order_id = $_POST["order_id"];
-$status = $_POST["status"];
-$amount = $_POST["amount"];
-$dummy_data = "nothing";
-$realtime_status = "Ordered";
-$real_status_1 = "TXN_SUCCESS";
+// $order_id = $_POST["order_id"];
+// $status = $_POST["status"];
+// $amount = $_POST["amount"];
+// $dummy_data = "nothing";
+// $realtime_status = "Ordered";
+// $real_status_1 = "TXN_SUCCESS";
 
 
-$get_cart_delete = "select * from order_info where current_status='TXN_SUCCESS' AND order_id='$order_id'";
+// $get_cart_delete = "select * from order_info where current_status='TXN_SUCCESS' AND order_id='$order_id'";
 
-    $run_cart_delete = mysqli_query($conn, $get_cart_delete);
-
-
-
-    while ($row_cart_delete = mysqli_fetch_array($run_cart_delete)) {
-
-        $order_id_2 = $row_cart_delete['order_id'];
-    }
+//     $run_cart_delete = mysqli_query($conn, $get_cart_delete);
 
 
 
-if (!empty($userid) && !empty($order_id) && !empty($status) && !empty($amount)) {
+//     while ($row_cart_delete = mysqli_fetch_array($run_cart_delete)) {
 
-    $query_address = mysqli_query($conn, "INSERT INTO `order_info`( `user_id`, `order_id`, `current_status`,`transaction_amount`) VALUES ('$userid ','$order_id','$status','$amount')");
+//         $order_id_2 = $row_cart_delete['order_id'];
+//     }
 
-    if ($real_status_1 == $status) {
-        $query_address1 = mysqli_query($conn, "INSERT INTO status_info(`order_id`, `realtime_status`,`tracking_link`) VALUES ('$order_id','$realtime_status','$dummy_data')");
-    }
-    if ($query_address1) {
-        $cart_deletequery = mysqli_query($conn, "delete from products_cart where order_id= $order_id_2");
-    }
 
-}
+
+// if (!empty($userid) && !empty($order_id) && !empty($status) && !empty($amount)) {
+
+//     $query_address = mysqli_query($conn, "INSERT INTO `order_info`( `user_id`, `order_id`, `current_status`,`transaction_amount`) VALUES ('$userid ','$order_id','$status','$amount')");
+
+//     if ($real_status_1 == $status) {
+//         $query_address1 = mysqli_query($conn, "INSERT INTO status_info(`order_id`, `realtime_status`,`tracking_link`) VALUES ('$order_id','$realtime_status','$dummy_data')");
+//     }
+//     if ($query_address1) {
+//         $cart_deletequery = mysqli_query($conn, "delete from products_cart where order_id= $order_id_2");
+//     }
+
+// }
 
 
 
