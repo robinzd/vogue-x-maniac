03ff2af1a3cca04fc3f2da12681273dacd9522f1
robin <ibots.robin@gmail.com>
2022-07-20 17:18:24 +0530

    creating load more button logic

--- user_dashboard.php
+++ user_dashboard.php
@@ -371,7 +371,7 @@ if ($get_cart) {
 
 
     <!-- j query -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script> -->
     <!-- owl carousel -->
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
