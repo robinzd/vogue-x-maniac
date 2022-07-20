96934b8a98f1c5895d13033a2579f8cb2c82d1c4
robin <ibots.robin@gmail.com>
2022-07-20 18:42:23 +0530

    fixing some bug

--- user_dashboard.php
+++ user_dashboard.php
@@ -394,7 +394,7 @@ if ($get_cart) {
                     }
                     else{
                         $('#btnLoad').remove();
-                        $('#main-content').append('<h4>No More Data To Show</h4>');
+                        $('#main-content').append("<h4>No More Data To Show</h4>");
                     }
                   }
                });
