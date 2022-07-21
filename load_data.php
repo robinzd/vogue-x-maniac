cbdd8a1a5a2e4ab477b16384edc4c750055935bc
robin <ibots.robin@gmail.com>
2022-07-21 16:30:06 +0530

    changing some logics and designs

--- load_data.php
+++ load_data.php
@@ -138,7 +138,7 @@ if (isset($_POST['lastid']) && $_POST['lastid']!="") {
 
 else{
     echo '<div class="d-grid gap-2 col-6 mx-auto">
-    <button class="btn btn-primary" type="button" id="btnLoad" disabled>No More Data.</button>
+    <button class="btn btn-primary" type="button" id="btnLoad" disabled>No Orders..</button>
 </div>';
     
 }
