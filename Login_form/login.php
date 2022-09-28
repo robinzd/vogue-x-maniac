dfab23b68daf4b7edb10dd7b878d06550a569500
robin <ibots.robin@gmail.com>
2022-09-28 23:16:17 +0530

    working on google integration

--- Login_form/login.php
+++ Login_form/login.php
@@ -43,14 +43,68 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 	}
 }
 
+// google integration code starts//
+
+require './google-api-php-client-2.4.0/google-api-php-client-2.4.0/vendor/autoload.php';
+
+$clientId = "428003245396-63d10kjmatp8ubebi6qunbdj6sjvn1t9.apps.googleusercontent.com";
+$clientSecret = "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";
+$redirectURI = "https://vogue-x-maniac.herokuapp.com/login_form/login.php";
 
 
+$client = new Google_Client();
+$client->setClientId($clientId);
+$client->setClientSecret($clientSecret);
+$client->setRedirectUri($redirectURI);
+$client->addScope("email");
+$client->addScope("profile");
 
+if (isset($_GET["code"])){
+    $token = $client->fetchAccessTokenWithAuthCode($_GET["code"]);
+    $client->setAccessToken($token["access_token"]);
+
+    $obj = new Google_Service_Oauth2($client);
+    $data = $obj->userinfo->get();
+
+	$user_id = random_num(20);
+    $email = $_SESSION["email"] = $data->email;
+    echo $email;
+    $first_name = $_SESSION["givenName"] = $data->givenName;
+    $last_name = $_SESSION["familyName"] = $data->familyName;
+    $name = $_SESSION["name"] = $data->name;
+
+
+    // var_dump($data);
+
+	// user_id, first_name, last_name, user_email, user_password, user_mob_no, created_time, is_admin
+
+
+    $get_users = "select user_email from users where user_email='$email'";
+
+    echo $get_users;
+
+    $run_users = mysqli_query($conn, $get_users);
+
+    while ($row_users = mysqli_fetch_array($run_users)) {
+
+        $user_email = $row_users['user_email'];
+        echo $user_email;
+    }
+     if (!empty($data->email) && !empty($data->name) &&  $user_email !== $data->email) {
+            $query_address = mysqli_query($conn, "INSERT INTO `users`( `user_id`, `first_name`, `last_name`, `user_email`,`user_password`,`user_mob_no`,`is_admin`) VALUES ('$user_id','$first_name','$last_name','$email','NULL','NULL','0')");
+            if ($query_address) {
+                echo "<script>alert('hai');</script>";
+                header("location:home.php");
+            }
+    }else{
+        header("location:../index.php");
+    }
+}
+// echo "<a href='" . $client->createAuthUrl() . "'>Login With Google</a>";
+
+//google integration code ends//
 
 ?>
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -111,6 +165,8 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 						<span class="label-input100">Email</span>
 					</div>
 
+					
+
 
 					<div class="wrap-input100 validate-input" data-validate="Password is required">
 						<input class="input100" type="password" name="password">
@@ -147,22 +203,21 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 							Don't Have an account?<a href="../register_form/register.php" class="txt1">Register</a>
 						</span>
 					</div>
-
-										
-					 <div class="text-center p-t-46 p-b-20">
+                    <div class="text-center p-t-46 p-b-20">
 						<span class="txt2">
 							or sign up using
 						</span>
 					</div>
-
-					 <div class="login100-form-social flex-c-m">
+                    <div class="login100-form-social flex-c-m">
 						<!-- <a href="#" class="login100-form-social-item flex-c-m bg1 m-r-5">
 							<i class="fa fa-facebook-f" aria-hidden="true"></i>
 						</a> -->
-
-						<a href="#" class="login100-form-social-item flex-c-m bg2 m-r-5">
-							<i class="fa fa-google" aria-hidden="true"></i>
-						</a>
+						
+                    <?php 
+					echo "<a href='". $client->createAuthUrl() . "' class='login100-form-social-item flex-c-m bg2 m-r-5'>
+							<i class='fa fa-google' aria-hidden='true'></i>
+					</a>"
+					?>
 					</div> 
 				</form>
 
@@ -171,14 +226,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 			</div>
 		</div>
 	</div>
-
-
-
-
-
-
-
-	<!--===============================================================================================-->
+    <!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 	<!--===============================================================================================-->
 	<script src="vendor/animsition/js/animsition.min.js"></script>
