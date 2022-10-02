5450d903e456040564a54bc0c313975ed38cb302
robin <ibots.robin@gmail.com>
2022-10-02 17:10:24 +0530

    fixing some bugs

--- user_password.php
+++ user_password.php
@@ -62,7 +62,7 @@ $userid = $user_data['user_id'];
         <div class="row">
             <div class="form-holder">
                 <div class="form-content">
-                    <div class="form-items">
+                    <div class="form-items" id="form1">
                         <h3 class="text-center"><i class="fa fa-key"></i>User Password</h3>
                        
 
