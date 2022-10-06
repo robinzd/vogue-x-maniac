e752a93452d8aa12ef819b4b11f4ec9d9ca1a315
robin <ibots.robin@gmail.com>
2022-10-06 12:24:57 +0530

    changing the header design

--- index_header.js
+++ index_header.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     document.getElementById("mobile").style.padding = "30px 10px";
     document.getElementById("logo").style.fontSize = "25px";
   } else {
