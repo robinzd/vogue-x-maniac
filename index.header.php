8c16b827043b574bf42c7e4d7bfbee2b368c34e8
robin <ibots.robin@gmail.com>
2022-10-06 15:15:31 +0530

    changing the header design

--- index.header.php
+++ index.header.php
@@ -156,6 +156,7 @@ if ($get_user) {
 	<!-- The Navbar Design For The Mobile Version Starts -->
 	<nav class="navbar fixed-top bg-light d-block d-sm-none" id="mobile">
 		<div class="container-fluid">
+			<h1 id="heading1">hai</h1>
 			<div>
 			<a class="navbar-brand text-center" href="#" id="logo"><img src="./header images/vogue x maniac png.png" style="
     width: 30%; margin-top:-2%;"></a>
