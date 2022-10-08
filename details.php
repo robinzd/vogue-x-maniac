7e50d6fc5fef423524d78063be22f00f3c5d2c68
robin <ibots.robin@gmail.com>
2022-10-08 20:18:35 +0530

    changing the fotter content in all the pages

--- details.php
+++ details.php
@@ -139,7 +139,7 @@ include("./conn.php");
                                 </div>
                             </div>
 
-                           
+
 
                         </div>
 
@@ -301,12 +301,12 @@ include("./conn.php");
                                     ?>
 
                                 </div>
-                                
-                                <h1  class="text-muted" id="offerheading">Note*</h1>
-                                
+
+                                <h1 class="text-muted" id="offerheading">Note*</h1>
+
                                 <p class="text-muted" id="offer">Within Tamilnadu shipping charges free,for other states delivery charges applied!</p>
 
-                                
+
                                 <div class="cart mt-4 align-items-center">
                                     <button type="submit" class="btn-success mr-2 px-4" value="cart" formaction="cart.php">Add to cart</button>
                                     <button type="submit" class="btn-success mr-2 px-4" value="address" formaction="address1.php">Buy Now</button>
@@ -392,95 +392,88 @@ include("./conn.php");
     <!-- Footer -->
 
     <footer class="text-center text-lg-start text-dark">
-                <!-- Grid container -->
-                <div class="container p-4 pb-0">
-                    <!-- Section: Links -->
-                    <section class="">
-                        <!--Grid row-->
-                        <div class="row">
-                            <!-- Grid column -->
-                            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                                <img class="footerimage" src="./header images/vogue x maniac png.png" alt="brand image">
-                                <p class="text-white">
-                                    vogue-x-maniac is the one of the leading ecommerce website
-                                </p>
-                            </div>
-                            <!-- Grid column -->
+        <!-- Grid container -->
+        <div class="container p-4 pb-0">
+            <!-- Section: Links -->
+            <section class="">
+                <!--Grid row-->
+                <div class="row">
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                        <img class="footerimage" src="./header images/vogue x maniac png.png" alt="brand image">
+                        <p class="text-white">
+                            VogueXmaniac The Best place to buy your favorite products at affordable prices.
+                        </p>
+                    </div>
+                    <!-- Grid column -->
 
-                            <hr class="w-100 clearfix d-md-none" />
+                    <hr class="w-100 clearfix d-md-none" />
 
-                            <!-- Grid column -->
-                            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                                <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
-                                <p class="text-white">MDBootstrap</p>
-                                <p class="text-white">MDWordPress</p>
-                                <p class="text-white">BrandFlow</p>
-                                <p class="text-white">Angular</p>
-                            </div>
-                            <!-- Grid column -->
+                    <!-- Grid column -->
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+                        <p class="text-white">Mens Accessories</p>
+                        <p class="text-white">Womens Accessories</p>
+                        <p class="text-white">Smart Watches</p>
+                        <p class="text-white">Footwears</p>
+                        <p class="text-white">Analog Watches</p>
+                    </div>
+                    <!-- Grid column -->
 
-                            <hr class="w-100 clearfix d-md-none" />
+                    <hr class="w-100 clearfix d-md-none" />
 
-                            <!-- Grid column -->
-                            <hr class="w-100 clearfix d-md-none" />
+                    <!-- Grid column -->
+                    <hr class="w-100 clearfix d-md-none" />
 
-                            <!-- Grid column -->
-                            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-                                <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
-                                <p class="text-white"><i class="fas fa-home text-white mr-3"></i> New York, NY 10012, US</p>
-                                <p class="text-white" id="address"><i class="fas fa-envelope text-white mr-3"></i> inf0@text-white@gmail.com
-                                </p>
-                                <p class="text-white"><i class="fas fa-phone text-white mr-3"></i> + 01 234 567 88</p>
-                                <p class="text-white"><i class="fas fa-print text-white mr-3"></i> + 01 234 567 89</p>
-                            </div>
-                            <!-- Grid column -->
+                    <!-- Grid column -->
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+                        <p class="text-white"><i class="fas fa-home text-white mr-3"></i> Kajamalai,Trichy,620023.</p>
+                        <p class="text-white" id="address"><i class="fas fa-envelope text-white mr-3"></i>aslammr.9148@gmail.com </p>
+                        <p class="text-white"><i class="fas fa-phone text-white mr-3"></i>+91 8526359590</p>
+                        <p class="text-white"><i class="fas fa-phone text-white mr-3"></i>+91 7904860889</p>
+                        <p class="text-white"><i class="fas fa-phone text-white mr-3"></i> +91 6383457659</p>
+                    </div>
+                    <!-- Grid column -->
 
-                            <!-- Grid column -->
-                            <div class="col-md-3 col-lg-4 col-xl-4 mx-auto mt-3">
-                                <h6 class="text-uppercase mb-4 font-weight-bold">Follow us</h6>
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-4 col-xl-4 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold">Follow us</h6>
 
-                                <!-- Facebook -->
-                                <a class="mx-1 my-2" id="facebook" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Facebook" role="button"><i class="fab fa-facebook"></i></a>
-
-                                <!-- Twitter -->
-                                <a class=" mx-1 my-2" id="twitter" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Twitter" role="button"><i class="fab fa-twitter"></i></a>
-
-                                <!-- youtube -->
-                                <a class="mx-1 my-2" id="youtube" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="youtube" role="button"><i class="fab fa-youtube"></i></a>
+                        <!-- Facebook -->
+                        <a class="mx-1 my-2" id="facebook" href="https://www.facebook.com/voguexmaniac/" data-bs-toggle="tooltip" data-bs-placement="top" title="Facebook" role="button"><i class="fab fa-facebook"></i></a>
 
 
-                                <!-- Instagram -->
-                                <a class="mx-1 my-2" id="instagram" href="#!" data-bs-toggle="tooltip" data-bs-placement="top" title="Instagram" role="button"><i class="fab fa-instagram"></i></a>
+                        <!-- youtube -->
+                        <a class="mx-1 my-2" id="youtube" href="https://youtube.com/channel/UCvST9hfgXqtTiNJ-owE6DZQ" data-bs-toggle="tooltip" data-bs-placement="top" title="youtube" role="button"><i class="fab fa-youtube"></i></a>
 
-                            </div>
-                        </div>
-                        <!--Grid row-->
-                    </section>
-                    <!-- Section: Links -->
+
+                        <!-- Instagram -->
+                        <a class="mx-1 my-2" id="instagram" href="https://instagram.com/vogue_x_maniac_me?igshid=YmMyMTA2M2Y=" data-bs-toggle="tooltip" data-bs-placement="top" title="Instagram" role="button"><i class="fab fa-instagram"></i></a>
+
+                    </div>
                 </div>
-                <!-- Grid container -->
+                <!--Grid row-->
+            </section>
+            <!-- Section: Links -->
+        </div>
+        <!-- Grid container -->
 
-                <!-- Copyright -->
-                <div class="text-center p-3">
+        <!-- Copyright -->
+        <div class="text-center p-3">
 
-                    <p class="text-dark"> ©2022 Vogue X Maniac.All Rights Reserved
-                    </p>
-                    <img class="px-2" src="./payment_pics/2560px-MasterCard_Logo.svg-removebg-preview.png">
-                    <img class="px-2" src="./payment_pics/1200px-Visa.svg-removebg-preview.png">
-                    <img class="px-2" src="./payment_pics/paypal-logo-removebg-preview.png">
-                </div>
-                <!-- Copyright -->
-            </footer>
-            <!-- end of the footer -->
+            <p class="text-dark"> ©2022 Vogue X Maniac.All Rights Reserved
+            </p>
+            <img class="px-2" src="./payment_pics/2560px-MasterCard_Logo.svg-removebg-preview.png">
+            <img class="px-2" src="./payment_pics/1200px-Visa.svg-removebg-preview.png">
+            <img class="px-2" src="./payment_pics/paytm.png">
+            <img class="px-2" src="./payment_pics/upi.png">
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <!-- end of the footer -->
 
-
-
-
-
-
-
-
-    <!-- external js sheet -->
+  <!-- external js sheet -->
     <script src="details.js"></script>
     <!-- j query -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
@@ -510,10 +503,6 @@ include("./conn.php");
                     }
                 }
             });
-
-
-
-
         });
     </script>
 
