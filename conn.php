231c8de7e6df80f818a88647e0935c48f5981870
Adithian Arulmozhi <adithian@constellation4.com>
2022-10-24 23:17:52 +0530

    Changing the connection string

--- conn.php
+++ conn.php
@@ -2,7 +2,8 @@
 
 
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// // $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_url = parse_url("mysql:host=217.21.85.52;dbname=u291904117_kEVdC", "u291904117_usctU", "Robin@123");
 $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
--- db_conn.php
+++ db_conn.php
@@ -2,6 +2,6 @@
 
 //database_connection.php
 
-$connect = new PDO("mysql:host=us-cdbr-east-05.cleardb.net;dbname=heroku_d8dfd4489ae4b3b", "bae780ccd132dd", "612c8a8d");
+$connect = new PDO("mysql:host=217.21.85.52;dbname=u291904117_kEVdC", "u291904117_usctU", "Robin@123");
 
 ?>
