25525f4e6d78f3ca400b7d701d2571541dc4cf57
Adithian Arulmozhi <adithian@constellation4.com>
2022-10-25 01:12:23 +0530

    Connection file changes for hostinger database

--- paytm_php_sample_app/payment-using-paytm/conn1.php
+++ paytm_php_sample_app/payment-using-paytm/conn1.php
@@ -1,17 +1,15 @@
 <?php
 
 
-//Get Heroku ClearDB connection information
-// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_url = parse_url("mysql:host=217.21.85.52;dbname=u291904117_kEVdC", "u291904117_usctU", "Robin@123");
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$host="217.21.85.52";
+$port=3306;
+$socket="";
+$user="u291904117_usctU";
+$password="Robin@123";
+$dbname="u291904117_kEVdC";
+
+$conn = new mysqli($host, $user, $password, $dbname, $port, $socket)
+	or die ('Could not connect to the database server' . mysqli_connect_error());
 
 
 
@@ -19,6 +17,6 @@ $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $c
 
 // Check connection
 if (!$conn) {
-    die("Connection failed(Check): " . mysqli_connect_error()."$cleardb_server--$cleardb_username--$cleardb_password--$cleardb_db--$cleardb_url");
+    die("Connection failed(Check): " . mysqli_connect_error());
 }
 ?>
