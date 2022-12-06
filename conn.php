57319f7bbe82a6f366ea7d1b79e72d298ad1d8b0
robin <ibots.robin@gmail.com>
2022-12-06 10:12:28 +0530

    Changes In The Conn.php 06/12/2022

--- conn.php
+++ conn.php
@@ -16,16 +16,16 @@
 $host="217.21.85.52";
 $port=3306;
 $socket="";
-$user="u291904117_usctU";
+$user="u291904117_admin";
 $password="Robin@123";
-$dbname="u291904117_kEVdC";
+$dbname="u291904117_schema";
 // connect to db
 $conn = mysqli_connect($host, $user, $password, $dbname, $port, $socket)
 	or die ('Could not connect to the database server' . mysqli_connect_error());
 // Check connection
 if (!$conn) {
     die("Connection failed(Check): " . mysqli_connect_error());
+
 }
 ?>
-
 <!-- 217.21.85.52 -->
