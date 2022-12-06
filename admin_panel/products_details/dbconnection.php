869f613841ed26e68793d49bd4840e360d537dd0
robin <ibots.robin@gmail.com>
2022-12-06 12:09:33 +0530

    fixing some bug in the code on 06/12/2022

--- admin_panel/products_details/dbconnection.php
+++ admin_panel/products_details/dbconnection.php
@@ -1,19 +1,4 @@
 <?php
-// echo "Check from Data base";
-//Get Heroku ClearDB connection information
-// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-// $cleardb_server = $cleardb_url["host"];
-// $cleardb_username = $cleardb_url["user"];
-// $cleardb_password = $cleardb_url["pass"];
-// $cleardb_db = substr($cleardb_url["path"],1);
-// $active_group = 'default';
-// $query_builder = TRUE;
-// Connect to DB
-// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-// Check connection
-// if (!$conn) {
-//     die("Connection failed: " . mysqli_connect_error());
-// }
 $host="217.21.85.52";
 $port=3306;
 $socket="";
