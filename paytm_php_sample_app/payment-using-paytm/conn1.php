3fc65bfa761b3b4dc89016dbe8171f0f5e459bdc
robin <ibots.robin@gmail.com>
2022-12-13 21:48:21 +0530

    some changes in the code 13/12/2022

--- paytm_php_sample_app/payment-using-paytm/conn1.php
+++ paytm_php_sample_app/payment-using-paytm/conn1.php
@@ -1,20 +1,13 @@
 <?php
-
-
 $host="217.21.85.52";
 $port=3306;
 $socket="";
-$user="u291904117_usctU";
+$user="u291904117_admin";
 $password="Robin@123";
-$dbname="u291904117_kEVdC";
-
-$conn = new mysqli($host, $user, $password, $dbname, $port, $socket)
+$dbname="u291904117_schema";
+// connect to db
+$conn = mysqli_connect($host, $user, $password, $dbname, $port, $socket)
 	or die ('Could not connect to the database server' . mysqli_connect_error());
-
-
-
-
-
 // Check connection
 if (!$conn) {
     die("Connection failed(Check): " . mysqli_connect_error());
